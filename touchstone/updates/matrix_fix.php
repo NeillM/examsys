9339ba74184da5289357441b960a8aa7a28f0be1
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2011-05-27 14:22:35 +0000

    git-svn-id: svn://suivarro/touchstone/trunk@65 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- touchstone/updates/matrix_fix.php
+++ touchstone/updates/matrix_fix.php
@@ -34,7 +34,11 @@
     
     $answered = false;
     for ($i=0; $i<count($tmp_answer_parts); $i++) {
-      $new_answers[$tmp_order[$i]] = $tmp_answer_parts[$i];
+      if (isset($tmp_answer_parts[$i])) {
+        $new_answers[$tmp_order[$i]] = $tmp_answer_parts[$i];
+      } else {
+        $new_answers[$tmp_order[$i]] = 'u';
+      }
     }
     
     $new_answer = '';
