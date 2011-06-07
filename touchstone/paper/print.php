71a23ae41202662d26ffc32d9681aedeb3afab4b
Rob Ingram <Rob.Ingram@notitngham.ac.uk>
2011-06-07 11:00:43 +0000

    Fix display of question numbers
    git-svn-id: svn://suivarro/touchstone/trunk@144 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- touchstone/paper/print.php
+++ touchstone/paper/print.php
@@ -116,7 +116,8 @@ echo "<html>\n<head>\n<title>$paper_title</title>\n";
 <script language="JavaScript" src="../javascript/start.js"></script>
 <script language="JavaScript" src="../javascript/flash_include.js"></script>
 </head>
-<body onload="javascript:window.print()">
+<!--<body onload="javascript:window.print()">-->
+<body>
   <!--  Start outer table -->
   <table cellpadding="0" cellspacing="0" border="0" width="100%" height="100%">
   <tr><td valign="top">
@@ -146,6 +147,8 @@ echo "<html>\n<head>\n<title>$paper_title</title>\n";
 
     if ($question_no == 0) echo "<tr><td colspan=\"2\">&nbsp;</td></tr>\n";
     if ($old_q_id != $q_id) {          // New Question
+      $qn_set = false;
+      
       // Print the options of the previous question
       if ($old_leadin != '') {
         display_options($options_array, $old_q_id, $old_theme);
@@ -167,20 +170,22 @@ echo "<html>\n<head>\n<title>$paper_title</title>\n";
       if ($scenario != '' and $q_type != 'info' and $q_type != 'extmatch' and $q_type != 'matrix' and $q_type != 'likert' and $q_type != 'sct') {
         echo '<!-- Start q no. row  A --><tr><td class="question_no">' . ($question_no + $question_offset) . '.&nbsp;</td><td>' . make_para_if_not(trim($scenario)) . '<!-- End q no. row  A -->';
         echo "</td></tr><!-- Close question B -->\n";
+        $qn_set = true;
       }
       if ($q_media != '' and $q_media != NULL and $q_type != 'sct' and $q_type != 'hotspot' and $q_type != 'labelling' and $q_type != 'flash' and $q_type != 'extmatch' and $q_type != 'matrix') {
+        $q_no = (!$qn_set) ? ($question_no + $question_offset).'.' : '';
+        if ($li_set == 0) echo '<!-- Start q no. row  B --><tr><td class="question_no">' . $q_no . '.&nbsp;</td><td>';
+        $li_set = 1;
+        $qn_set = true;
         if (substr($q_media, -4) == '.gif' or substr($q_media, -4) == '.jpg' or substr($q_media, -4) == 'jpeg' or substr($q_media, -4) == '.png') {
-          if ($li_set == 0) echo '<!-- Start q no. row  B --><tr><td class="question_no">' . ($question_no + $question_offset) . '.&nbsp;</td><td>';
-          $li_set = 1;
           echo "<p align=\"center\">" . display_media($q_media,$q_media_width,$q_media_height,$question_no) . "</p>\n";
         } else {
-          if ($li_set == 0) echo '<!-- Start q no. row  C --><tr><td class="question_no">' . ($question_no + $question_offset) . '.&nbsp;</td><td>';
-          $li_set = 1;
           echo "<p>" . display_media($q_media,$q_media_width,$q_media_height,$question_no) . "</p>\n";
         }
       }
       if ($q_type != 'hotspot' and $q_type != 'likert' and $q_type != 'calculation' and $q_type != 'info' and $q_type != 'sct') {
-        if ($li_set == 0) echo '<!-- Start q no. row  D --><tr><td class="question_no">' . ($question_no + $question_offset) . '.&nbsp;</td><td>';
+        $q_no = (!$qn_set) ? ($question_no + $question_offset).'.' : '';
+        if ($li_set == 0) echo '<!-- Start q no. row  D --><tr><td class="question_no">' . $q_no . '&nbsp;</td><td>';
         $li_set = 1;
         echo make_para_if_not(trim($leadin));
       }
