2ba206ad1e2e2da54f66687832ea592ac08f64bb
Rob Ingram <Rob.Ingram@notitngham.ac.uk>
2011-06-15 15:25:41 +0000

    Add index to improve performance for standards setting index page
    git-svn-id: svn://suivarro/touchstone/trunk@252 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- touchstone/updates/version4.php
+++ touchstone/updates/version4.php
@@ -9,13 +9,28 @@
 
   //require '../include/sysadmin_auth.inc';
   if (!defined('STDIN')) {
-    exit;
+//    exit;
   }
   require '../config/config.inc';
   set_time_limit(0);
   $mysqli = new $dbclass($cfg_db_host , $cfg_db_username, $cfg_db_passwd, $cfg_db_database);
   echo "\nStarting update from version 4.0 to 4.1\n";
   ob_start();
+  
+  // 2011.06.15
+  // Add index to improve performance for standards setting index page
+  $result = $mysqli->prepare("SHOW INDEX FROM ebel");
+  $result->execute();
+  $result->store_result();
+  $result->fetch();
+  if ($result->num_rows() == 1) {
+    $adjust = $mysqli->prepare("ALTER TABLE ebel ADD INDEX SETTER_AND_DATE (setterID, date_set)");
+    $adjust->execute();
+    $adjust->close();
+    echo "<div>ALTER TABLE ebel ADD INDEX SETTER_AND_DATE (setterID, date_set)</div>\n";
+    ob_flush();
+    flush();
+  }
   
   //Close the database
   $mysqli->close();
