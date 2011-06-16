ab674ed1634b4cdd45eeb01e86fb0d5c44ce8dc7
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2011-06-16 13:58:27 +0000

    Remove unnecessary columns from log_late.
    Add additional data recording to custom errors.
    
    git-svn-id: svn://suivarro/touchstone/trunk@259 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- touchstone/updates/version4.php
+++ touchstone/updates/version4.php
@@ -7,7 +7,7 @@
 * @package
 */
 
-  //require '../include/sysadmin_auth.inc';
+  require '../include/sysadmin_auth.inc';
   if (!defined('STDIN')) {
 //    exit;
   }
@@ -17,7 +17,7 @@
   echo "\nStarting update from version 4.0 to 4.1\n";
   ob_start();
   
-  // 2011.06.15
+  // 15/06/2011
   // Add index to improve performance for standards setting index page
   $result = $mysqli->prepare("SHOW INDEX FROM ebel");
   $result->execute();
@@ -32,6 +32,60 @@
     flush();
   }
   
+  // 16/06/2011
+  $result = $mysqli->prepare("SELECT COLUMN_TYPE FROM information_schema.COLUMNS WHERE TABLE_NAME='log_late' AND TABLE_SCHEMA='$cfg_db_database' AND COLUMN_NAME='year'");
+  $result->execute();
+  $result->store_result();
+  $result->bind_result($column_type);
+  $result->fetch();
+  if ($result->num_rows() == 1) {
+    $adjust = $mysqli->prepare("ALTER TABLE log_late DROP COLUMN year");
+    $adjust->execute();
+    $adjust->close();
+    echo "<div>ALTER TABLE log_late DROP COLUMN year</div>\n";
+    
+    $adjust = $mysqli->prepare("ALTER TABLE log_late DROP COLUMN student_grade");
+    $adjust->execute();
+    $adjust->close();
+    echo "<div>ALTER TABLE log_late DROP COLUMN student_grade</div>\n";
+    
+    $adjust = $mysqli->prepare("ALTER TABLE log_late DROP COLUMN ipaddress");
+    $adjust->execute();
+    $adjust->close();
+    echo "<div>ALTER TABLE log_late DROP COLUMN ipaddress</div>\n";
+  }
+
+  // 16/06/2011
+  $result = $mysqli->prepare("SELECT COLUMN_TYPE FROM information_schema.COLUMNS WHERE TABLE_NAME='sys_errors' AND TABLE_SCHEMA='$cfg_db_database' AND COLUMN_NAME='fixed'");
+  $result->execute();
+  $result->store_result();
+  $result->bind_result($column_type);
+  $result->fetch();
+  if ($result->num_rows() == 0) {
+    $adjust = $mysqli->prepare("ALTER TABLE sys_errors ADD COLUMN fixed datetime");
+    $adjust->execute();
+    $adjust->close();
+    echo "<div>ALTER TABLE sys_errors ADD COLUMN fixed datetime</div>\n";
+    
+    $adjust = $mysqli->prepare("ALTER TABLE sys_errors ADD COLUMN php_self text");
+    $adjust->execute();
+    $adjust->close();
+    echo "<div>ALTER TABLE sys_errors ADD COLUMN php_self text</div>\n";
+    
+    $adjust = $mysqli->prepare("ALTER TABLE sys_errors ADD COLUMN query_string text");
+    $adjust->execute();
+    $adjust->close();
+    echo "<div>ALTER TABLE sys_errors ADD COLUMN query_string text</div>\n";
+    
+    $adjust = $mysqli->prepare("ALTER TABLE sys_errors ADD COLUMN request_method enum('GET', 'HEAD', 'POST', 'PUT', 'DELETE')");
+    $adjust->execute();
+    $adjust->close();
+    echo "<div>ALTER TABLE sys_errors ADD COLUMN request_method enum('GET', 'HEAD', 'POST', 'PUT', 'DELETE')</div>\n";
+    ob_flush();
+    flush();
+  }
+  $result->close();
+
   //Close the database
   $mysqli->close();
   ob_end_flush();
