f105b7e66ba1b57bbbac23b3b02791416772c008
Rob Ingram <Rob.Ingram@notitngham.ac.uk>
2011-06-30 15:43:14 +0000

    Rename existing reset password script. It will eventually probably make way for the new password reset functionality.
    git-svn-id: svn://suivarro/touchstone/trunk@342 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- touchstone/users/details.php
+++ touchstone/users/details.php
@@ -291,7 +291,7 @@ a.access:hover {color:white}
   }
   
   function resetPassword(username) {
-    editwin=window.open("reset_password.php?userID=<?php echo $_GET['userID']; ?>&username=" + username + "","editmodule","width=450,height=400,left="+(screen.width/2-200)+",top="+(screen.height/2-375)+",scrollbars=no,toolbar=no,location=no,directories=no,status=no,menubar=no,resizable");
+    editwin=window.open("reset_pwd.php?userID=<?php echo $_GET['userID']; ?>&username=" + username + "","editmodule","width=450,height=400,left="+(screen.width/2-200)+",top="+(screen.height/2-375)+",scrollbars=no,toolbar=no,location=no,directories=no,status=no,menubar=no,resizable");
     if (window.focus) {
       editwin.focus();
     }    
