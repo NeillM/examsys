e2000b73d3c39803cbccf27c2d692dfd0a192e5c
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2011-07-12 16:52:44 +0000

    Fixes for uploading students and modules manually.
    git-svn-id: svn://suivarro/touchstone/trunk@422 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- touchstone/admin/add_module.php
+++ touchstone/admin/add_module.php
@@ -115,7 +115,7 @@ if (isset($_POST['submit']) and $unique_moduleid == true) {
         }
         $tmp_userID = UserUtils::usernameExists($student->Username, $mysqli);
         if ($tmp_userID === false) {
-          $tmp_userID = UserUtils::createUser($student->Username, '', $student->Title, $student->Forename, $student->Surname, $student->Email, $student->CourseCode, $student->Gender, $sms->YearofStudy, 'Student', $student->StudentID, $mysqli);
+          $tmp_userID = UserUtils::createUser($student->Username, '', $student->Title, $student->Forename, $student->Surname, $student->Email, $student->CourseCode, $student->Gender, $student->YearofStudy, 'Student', $student->StudentID, $mysqli);
         }
         // Add student onto the module
         UserUtils::addUserToModule($tmp_userID, $module, $session, $mysqli);
