c9cff40cf9087a8f9d2ecb2ff2cb16f3fc2e09b2
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2011-07-12 16:39:15 +0000

    Testing a small change
    git-svn-id: svn://suivarro/touchstone/trunk@418 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- touchstone/admin/add_module.php
+++ touchstone/admin/add_module.php
@@ -161,7 +161,7 @@ if (isset($_POST['submit']) and $unique_moduleid == true) {
   <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
   <html>
   <head>
-  <title>2Create new Module<?php echo " $cfg_install_type"; ?></title>
+  <title>Create new Module<?php echo " $cfg_install_type"; ?></title>
   <link rel="stylesheet" type="text/css" href="../css/submenu.css" />
 
   <style>
