5ba4b81014782e1631534465ccd2a2c112c5c3dd
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2011-09-06 10:04:19 +0000

    Start of language pack extension to TouchStone.
    git-svn-id: svn://suivarro/touchstone/trunk@679 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

---
+++ touchstone/lang/en/touchstone/mapping/sessions_list.php
@@ -0,0 +1,19 @@
+<?php
+$string['sessions'] = '1Sessions';
+$string['createsession'] = '1Create new Session';
+$string['importnle'] = '1Import Sessions from NLE';
+$string['importfile'] = '1Import Sessions from File';
+$string['editsession'] = '1Edit Session';
+$string['deletesession'] = '1Delete Session';
+$string['copyyear'] = '1Copy Year';
+$string[''] = '1';
+$string[''] = '1';
+$string[''] = '1';
+$string[''] = '1';
+$string[''] = '1';
+$string[''] = '1';
+$string[''] = '1';
+$string[''] = '1';
+$string[''] = '1';
+$string[''] = '1';
+?>
