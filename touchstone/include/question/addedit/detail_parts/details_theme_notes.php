f02f30c963967107b92144210ca61f1aaa82e321
Rob Ingram <Rob.Ingram@notitngham.ac.uk>
2011-09-12 10:05:43 +0000

    Change 'Theme' input to be a text box not a textarea
    git-svn-id: svn://suivarro/touchstone/trunk@750 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- touchstone/include/question/addedit/detail_parts/details_theme_notes.php
+++ touchstone/include/question/addedit/detail_parts/details_theme_notes.php
@@ -4,7 +4,7 @@ $show_notes = (isset($show_notes)) ? $show_notes : true;
             <tr>
               <th><label for="theme"><?php echo $string['theme'] ?></label></th>
               <td>
-                <textarea id="theme" name="theme" cols="100" rows="5" class="form-large"><?php echo $question->get_theme() ?></textarea>
+                <input type="text" id="theme" name="theme" cols="100" rows="5" class="form-large" value="<?php echo $question->get_theme() ?>" />
               </td>
             </tr>
 <?php 
