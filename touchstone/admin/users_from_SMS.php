1b3d4ac8e140ea31b74b451fbcf58f46955f1f89
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2011-09-16 11:30:42 +0000

    git-svn-id: svn://suivarro/touchstone/trunk@783 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- touchstone/admin/users_from_SMS.php
+++ touchstone/admin/users_from_SMS.php
@@ -40,9 +40,6 @@
   
 	//require '../classes/dateutils.class.php';
   $mysqli = new $dbclass($cfg_db_host , $cfg_db_username, $cfg_db_passwd, $cfg_db_database);
-  
-  echo "Hi";
-  exit;
 
   // Calculate what the current academic session is.
   $session = (isset($_GET['session']) and $_GET['session'] != '') ? $_GET['session'] : DateUtils::get_current_academic_year();
