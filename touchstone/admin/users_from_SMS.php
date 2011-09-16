108380bef253ac00579a9d60f8d7123aede4aa51
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2011-09-16 13:58:21 +0000

    git-svn-id: svn://suivarro/touchstone/trunk@785 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- touchstone/admin/users_from_SMS.php
+++ touchstone/admin/users_from_SMS.php
@@ -84,8 +84,7 @@
   
     // Look up SMS
     $returned_data = file_get_contents($sms . "&code=$replaced_module&year=" . $session_parts[0]);
-    echo "hi";
-    echo $returned_data;
+    var_dump($replaced_module, $returned_data);
     exit;
     $xml = new SimpleXMLElement($returned_data);
     
