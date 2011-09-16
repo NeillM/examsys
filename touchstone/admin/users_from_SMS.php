00f7f2b92726a302d9705b39dca4234e09336ae7
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2011-09-16 11:30:27 +0000

    git-svn-id: svn://suivarro/touchstone/trunk@782 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- touchstone/admin/users_from_SMS.php
+++ touchstone/admin/users_from_SMS.php
@@ -38,7 +38,7 @@
   require_once $path . '/touchstone/config/config.inc.php';
   require $path . '/touchstone/classes/dateutils.class.php';
   
-	require '../classes/dateutils.class.php';
+	//require '../classes/dateutils.class.php';
   $mysqli = new $dbclass($cfg_db_host , $cfg_db_username, $cfg_db_passwd, $cfg_db_database);
   
   echo "Hi";
