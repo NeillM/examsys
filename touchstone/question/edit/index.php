9a88f021c79b0c34b0d83fddc1b592c787f55591
Rob Ingram <Rob.Ingram@notitngham.ac.uk>
2011-09-23 07:52:18 +0000

    Don't show changes and comments tabs when adding questions
    git-svn-id: svn://suivarro/touchstone/trunk@829 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- touchstone/question/edit/index.php
+++ touchstone/question/edit/index.php
@@ -353,7 +353,7 @@ endif;
 <script type="text/javascript">
 var lang = {
 <?php
-$langstrings = array('allowpartial', 'validationerror', 'enterleadin', 'showmore', 'hidemore');
+$langstrings = array('allowpartial', 'validationerror', 'enterleadin', 'enteroption', 'showmore', 'hidemore', 'mrqconvert');
 $first = true;
 foreach ($langstrings as $langstring) {
   if (!$first) {
@@ -392,8 +392,14 @@ if ($critical_error == '') {
         </p>
         <ol class="tabs">
           <li class="on"><a href="#" rel="editor"><?php echo $string['editor'] ?></a></li>
+<?php
+  if ($mode == $string['edit']):
+?>
           <li><a href="#" rel="changes"><?php echo $string['changes'] ?></a></li>
           <li><a href="#" rel="comments"><?php echo $string['comments'] ?></a></li>
+<?php
+  endif;
+?>
           <li<?php echo $mapping_enabled ?>><a href="#" rel="mapping"><?php echo $string['mapping'] ?></a></li>
         </ol>
       </div>
