ffa73428af41aaa656a83d89e4dcea05adb41fea
Anthony Brown <Anthony.Brown@nottingham.ac.uk>
2011-09-26 17:29:31 +0000

    Remove touchstone dir
    git-svn-id: svn://suivarro/touchstone/trunk@886 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- touchstone/include/question/addedit/media_upload.php
+++
@@ -1,8 +0,0 @@
-  <div id="media-upload-holder">
-    <div id="media-upload">
-      <h2><?php echo $string['uploadimage'] ?></h2>
-      <p><?php echo $string['uploadinstructions'] ?></p>
-      <p><label for="q_media" class="heavy"><?php echo $string['image'] ?></label> <input id="q_media" name="q_media" size="45" type="file" /></p>
-      <p class="align-centre"><input type="button" name="cancel" value="Cancel" onclick="javascript: history.back();" class="submit" /> <input type="submit" name="submit_media" value="<?php echo $string['next'] ?>" class="submit" /></p>
-    </div>
-  </div>
