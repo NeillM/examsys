8428a50c915b704e6858c423a13b547b3eb22a63
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2011-09-27 10:02:29 +0000

    TouchStone directory redesign
    git-svn-id: svn://suivarro/touchstone/trunk@911 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

---
+++ lang/en/qti/export.php
@@ -0,0 +1,28 @@
+<?php
+require '../lang/' . $language . '/include/paper_options.inc';
+
+$string['moreinformation'] = '1More Information';
+$string['viewdetails'] = '1View Details';
+$string['debuginformation'] = '1Debug Information';
+$string['loadingdebug'] = '1Loading Debug';
+$string['intermediateformatdebug'] = '1Intermediate Format Debug';
+$string['savingdebug'] = '1Saving Debug';
+$string['generaldebuginfo'] = '1General Debug Info';
+$string[''] = '1';
+$string[''] = '1';
+$string[''] = '1';
+$string[''] = '1';
+$string[''] = '1';
+$string[''] = '1';
+$string[''] = '1';
+$string[''] = '1';
+$string[''] = '1';
+$string[''] = '1';
+$string[''] = '1';
+$string[''] = '1';
+$string[''] = '1';
+$string[''] = '1';
+$string[''] = '1';
+$string[''] = '1';
+$string[''] = '1';
+?>
