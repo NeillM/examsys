50cf245c0628bac93bf6dc95e917589b3f8d9e16
Nikodem Miranowicz <Nikodem.Miranowicz@notitngham.ac.uk>
2011-10-11 14:40:59 +0000

    ldaplookup
    git-svn-id: svn://suivarro/touchstone/trunk@996 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- lang/en/users/ldaplookup.php
+++ lang/en/users/ldaplookup.php
@@ -1,6 +1,7 @@
 <?php
-//require '../lang/' . $language . '/include/user_search_options.inc';
-
-
-$string['sendwelcomeemail'] = '1Send welcome email to user';
+$string['username'] = '1Username';
+$string['surname'] = '1Surname';
+$string['lookup'] = '1Lookup';
+$string['cancel'] = '1Cancel';
+$string['nousersalert'] = '1No users found in LDAP lookup.';
 ?>
