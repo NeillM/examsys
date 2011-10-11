1e84d51bf43ff5da2c070cab462de4729279d67e
Nikodem Miranowicz <Nikodem.Miranowicz@notitngham.ac.uk>
2011-10-11 14:47:38 +0000

    ldaplookup
    git-svn-id: svn://suivarro/touchstone/trunk@1001 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- lang/en/users/ldaplookup.php
+++ lang/en/users/ldaplookup.php
@@ -2,6 +2,5 @@
 $string['username'] = '1Username';
 $string['surname'] = '1Surname';
 $string['lookup'] = '1Lookup';
-$string['cancel'] = '1Cancel';
 $string['nousersalert'] = '1No users found in LDAP lookup.';
 ?>
