7eebecb06c41de2d9e5408f60e9dc4cc59825a16
Nikodem Miranowicz <Nikodem.Miranowicz@notitngham.ac.uk>
2011-10-12 10:56:49 +0000

    git-svn-id: svn://suivarro/touchstone/trunk@1037 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- question/search.php
+++ question/search.php
@@ -106,6 +106,7 @@ if (isset($_POST['submit'])) {
   $error = '';
   if (!isset($_POST['theme']) {
     $error = 'theme?';
+  }
 
   if ($_POST['theme'] == '' and $_POST['scenario'] == '' and $_POST['leadin'] == '' and $_POST['options'] == '' and $_POST['keywords'] == '') {
     $error = 'You have not ticked any fields to search for.';
