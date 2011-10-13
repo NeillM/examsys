5ee5f15b09d4862e3b9600f7e264afb26c206181
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2011-10-13 17:02:33 +0000

    Fix for Ticket #281.
    git-svn-id: svn://suivarro/touchstone/trunk@1091 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- paper/finish.php
+++ paper/finish.php
@@ -1174,6 +1174,7 @@ table {font-size:100%}
           }
           break;
         case 'sct':
+          $paper[$question]['mark'] = 0;
           echo "<table cellpadding=\"3\" cellspacing=\"0\" border=\"0\" style=\"width:100%\"><tr><td colspan=\"3\" style=\"background-color:#E4EEFC; border-bottom:1px solid #B5C4DF; font-weight:bold\">Clinical Vignette</td></tr>\n<tr><td colspan=\"2\">" . $paper[$question]['scenario'] . "</td></tr>\n";
           if ($paper[$question]['q_media'] != '') echo "<tr><td colspan=\"3\"><p align=\"center\">" . display_media($paper[$question]['q_media'], $paper[$question]['q_media_width'], $paper[$question]['q_media_height'],$question_no) . "</p></td></tr>\n";
       
@@ -1224,7 +1225,7 @@ table {font-size:100%}
             if (isset($paper[$question]['user_answer']) and $tmp_part_id+1 == $paper[$question]['user_answer']) {
               if ($tmp_display_correct_answer == '1') {
                 echo '<td>';
-                if(isset($paper[$question]['std'][0])) echo display_std($paper[$question]['std'][0]);
+                if (isset($paper[$question]['std'][0])) echo display_std($paper[$question]['std'][0]);
                 echo '</td>';          
               } else {
                 echo '<td></td>';          
@@ -1232,10 +1233,13 @@ table {font-size:100%}
               if ($tmp_display_students_response == '1') {
                 if ($paper[$question]['correct'][$tmp_part_id] == $max) {
                   echo '<td><img src="../artwork/tick.gif" width="17" height="16" alt="Tick" /></td>';
+                  $paper[$question]['mark'] = 1;
                 } elseif ($paper[$question]['correct'][$tmp_part_id] > 0) {
                   echo '<td><img src="../artwork/tick_half.gif" width="17" height="16" alt="Half Right" /></td>';
+                  $paper[$question]['mark'] = $paper[$question]['correct'][$tmp_part_id] / $max;
                 } else {
                   echo '<td><img src="../artwork/cross.gif" width="17" height="16" alt="Cross" /></td>';
+                  $paper[$question]['mark'] = 0;
                 }
               } else {
                 echo '<td></td>';
