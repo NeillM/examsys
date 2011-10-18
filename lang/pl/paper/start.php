6fe16c6140ae2f856579fd6151e31559060a3717
Nikodem Miranowicz <Nikodem.Miranowicz@notitngham.ac.uk>
2011-10-18 10:27:10 +0000

    git-svn-id: svn://suivarro/touchstone/trunk@1143 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- lang/pl/paper/start.php
+++ lang/pl/paper/start.php
@@ -16,11 +16,11 @@ $string['abstain'] = 'Wstrzymany';
 $string['na'] = 'Nie dotyczy'; //N/A
 $string['unanswered'] = 'Brak odpowiedzi';
 $string['unansweredquestion'] = '= pytanie bez odpowiedzi';
-$string['negmarking'] = 'Ujemna punktacja';
-$string['bonusmark'] = 'dla prawidłowej opcji, plus 1 punkt dodatkowy za poprawna kolejność';
+$string['negmarking'] = 'ujemna punktacja';
+$string['bonusmark'] = 'dla prawidłowej opcji, plus 1 punkt dodatkowy za poprawną kolejność';
 $string['calculator'] = 'Kalkulator';
 $string['finishnote'] = '<strong>Uwaga:</strong> Należy wypełnić wszystkie odpowiedzi przed wybraniem &#145;Zakończ&#146; - powrót nie jest możliwy.';
-$string['gobackpink'] = 'Po powrocie, pytania, na które nie udzielono odpowiedzi, będą podświetlone na różowo.';
+$string['gobackpink'] = 'Po powrocie, pytania, na które nie udzielono odpowiedzi będą podświetlone na różowo.';
 $string['fireexit'] = 'Alarm pożarowy';
 $string['pleasecomplete'] = '<strong>Uwaga:</strong> Należy wypełnić wszystkie odpowiedzi przed wybraniem &#145;Ekranu %d &gt;&#146;, - powrót nie jest możliwy.';
 $string['javacheck1'] = 'Czy wypełniłeś wszystkie odpowiedzi na ekranie - powrót NIE będzie jest możliwy, czy na pewno chcesz kontynuować?';
