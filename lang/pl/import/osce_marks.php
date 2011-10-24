efd2e68e2ae751931a5485e4e7aedd392c0ff456
Nikodem Miranowicz <Nikodem.Miranowicz@notitngham.ac.uk>
2011-10-24 16:16:53 +0000

    lang.pl for osce_marks.php
    git-svn-id: svn://suivarro/touchstone/trunk@1331 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- lang/pl/import/osce_marks.php
+++ lang/pl/import/osce_marks.php
@@ -1,10 +1,10 @@
 <?php
 require '../lang/' . $language . '/include/paper_options.inc';
 
-$string['importoscemarks'] = '1Import OSCE Station Marks';
-$string['marksloaded'] = '1Marks loaded.';
-$string['csvfile'] = '1CSV File:';
-$string['topmsg'] = '1This area is for bulk uploading of marks from a paper-based OSCE. A CSV file in the following format should be used:';
-$string['headerrow'] = '1File contains header row';
-$string['import'] = '1Import';
+$string['importoscemarks'] = 'Importuj punkty OSCE';
+$string['marksloaded'] = 'Punkty/ów załadowano.';
+$string['csvfile'] = 'Plik CSV:';
+$string['topmsg'] = 'Ten obszar przeznaczony jest dla skomasowanego ładowania punktacji z arkuszy OSCE. Zastosowany powinien być plik CSV o następującym formacie:';
+$string['headerrow'] = 'Plik zawiera wiersz nagłówka';
+$string['import'] = 'Importuj';
 ?>
