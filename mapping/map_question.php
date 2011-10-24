76c0074f19b49dedcb56d77bdbc70676f00810e7
Anthony Brown <Anthony.Brown@nottingham.ac.uk>
2011-10-24 09:14:32 +0000

    fix for Ticket #367 error when selecting mapping icon
    git-svn-id: svn://suivarro/touchstone/trunk@1278 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- mapping/map_question.php
+++ mapping/map_question.php
@@ -26,17 +26,19 @@
   require '../include/question_types.inc';
   require '../include/mapping.inc';
   require '../include/display_functions.inc';
+  if (file_exists($cfg_web_root . "lang/$language/paper/start.php")) {
+    require $cfg_web_root . "lang/$language/paper/start.php";
+  }
   require '../include/media.inc';
   $paperID = $_GET['paperID'];
 
   function display_q($mysqlidb) {
     global $bgcolor;
-  
-    $question_data = $mysqlidb->prepare("SELECT q_type, q_id, score_method, marks, theme, scenario, leadin, correct, REPLACE(option_text,'\t','') AS option_text, q_media, q_media_width, q_media_height, o_media, o_media_width, o_media_height, notes FROM questions, options WHERE q_id=? AND questions.q_id=options.o_id ORDER BY id_num");
+    $question_data = $mysqlidb->prepare("SELECT q_type, q_id, score_method, display_method, marks_correct, marks_incorrect, marks_partial, theme, scenario, leadin, correct, REPLACE(option_text,'\t','') AS option_text, q_media, q_media_width, q_media_height, o_media, o_media_width, o_media_height, notes FROM questions, options WHERE q_id=? AND questions.q_id=options.o_id ORDER BY id_num");
     $question_data->bind_param('i', $_GET['q_id']);
     $question_data->execute();
     $question_data->store_result();
-    $question_data->bind_result($q_type, $q_id, $score_method, $marks, $theme, $scenario, $leadin, $correct, $option_text, $q_media, $q_media_width, $q_media_height, $o_media, $o_media_width, $o_media_height, $notes);
+    $question_data->bind_result($q_type, $q_id, $score_method, $display_method, $marks_correct, $marks_incorrect, $marks_partial, $theme, $scenario, $leadin, $correct, $option_text, $q_media, $q_media_width, $q_media_height, $o_media, $o_media_width, $o_media_height, $notes);
     $num_rows = $question_data->num_rows;
     echo "<table cellpadding=\"4\" cellspacing=\"0\" border=\"0\" width=\"100%\" style=\"table-layout:fixed\">\n";
     echo "<col width=\"40\"><col>\n";
@@ -50,12 +52,13 @@
         $question['q_type'] = $q_type;
         $question['q_id'] = $q_id;
         $question['score_method'] = $score_method;
+        $question['display_method'] = $display_method;
         $question['q_media'] = $q_media;
         $question['q_media_width'] = $q_media_width;
         $question['q_media_height'] = $q_media_height;
         $question['dismiss'] = '';
       }
-      $question['options'][] = array('correct'=>$correct, 'option_text'=>$option_text, 'o_media'=>$o_media, 'o_media_width'=>$o_media_width, 'o_media_height'=>$o_media_height, 'marks'=>$marks);
+      $question['options'][] = array('correct'=>$correct, 'option_text'=>$option_text, 'o_media'=>$o_media, 'o_media_width'=>$o_media_width, 'o_media_height'=>$o_media_height, 'marks_correct'=>$marks_correct, 'marks_incorrect'=>$marks_incorrect, 'marks_partial'=>$marks_partial);
     }
     $question_data->close();
     
