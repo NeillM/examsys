c4c69c43f74ad08504cb6303697c890ed6e077c8
Nikodem Miranowicz <Nikodem.Miranowicz@notitngham.ac.uk>
2011-10-25 15:47:32 +0000

    lang abstraction for print
    git-svn-id: svn://suivarro/touchstone/trunk@1365 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- lang/en/paper/print.php
+++ lang/en/paper/print.php
@@ -1,6 +1,15 @@
 <?php
 require '../lang/' . $language . '/question/sct_shared.php';
-$string['markingmethod'] = 'Metoda punktacji';
-$string['markperoption'] = 'Punktacja za opcję';
-$string['markperquestion'] = 'Punktacja za pytanie';
+$string['markingmethod'] = 'Marking Method';
+$string['markperoption'] = 'Mark per Option';
+$string['markperquestion'] = 'Mark per Question';
+$string['markingmethod'] = 'Marking Method';
+$string['markperoption'] = 'Mark per Option';
+$string['markperquestion'] = 'Mark per Question';
+$string['negativemarking'] = 'negative marking';
+$string['allwithnegative'] = 'all options items marked with negative marking';
+$string['correctplusbonus'] = 'marks for correct options, plus 1 bonus mark for fully correct order';
+$string['mark'] = 'mark';
+$string['halfmarksneighbours'] = 'half marks for neighbours';
+$string['exactorder'] = 'exact order';
 ?>
--- lang/pl/paper/print.php
+++ lang/pl/paper/print.php
@@ -1,6 +1,12 @@
 <?php
 require '../lang/' . $language . '/question/sct_shared.php';
-$string['markingmethod'] = 'Marking Method';
-$string['markperoption'] = 'Mark per Option';
-$string['markperquestion'] = 'Mark per Question';
+$string['markingmethod'] = 'Metoda punktacji';
+$string['markperoption'] = 'Punktacja za opcje';
+$string['markperquestion'] = 'Punktacja za pytanie';
+$string['negativemarking'] = 'ujemna punktacja';
+$string['allwithnegative'] = 'wszystkie elementy opcji punktowane z ujemną punktacją';
+$string['correctplusbonus'] = 'punkty za poprawne opcje, plus 1 punkt dodatkowy za poprawną kolejność';
+$string['mark'] = 'punkt';
+$string['halfmarksneighbours'] = 'punkty ułamkowe dla sąsiadów';
+$string['exactorder'] = 'poprawna kolejność';
 ?>
