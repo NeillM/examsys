81cf557f66859d18ca13926d6e416651e73a90db
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2011-10-25 14:58:34 +0000

    git-svn-id: svn://suivarro/touchstone/trunk@1362 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- import/osce_marks.php
+++ import/osce_marks.php
@@ -103,13 +103,13 @@ function marks_from_file($fileName, $mysqlidb) {
         $result->execute();
         $result->close();
 
-        echo "<li>$sid -&gt; " . $students[$sid]['username'] . "</li>";
+        echo "<li>$sid -&gt; " . $students[$sid]['username'] . ", $question_no</li>";
         
         // Record individual questions.
         $numeric_score = 0;
         $result = $mysqlidb->prepare("INSERT INTO log4 VALUES(NULL, ?, ?, ?, ?, ?, NULL)");
         for ($q=1; $q<=$question_no; $q++) {
-        $result->bind_param('isiis', $students[$sid]['id'], $paper_date, $_GET['paperID'], $paper[$q]['id'], $fields[$q]);
+          $result->bind_param('isiis', $students[$sid]['id'], $paper_date, $_GET['paperID'], $paper[$q]['id'], $fields[$q]);
           $fields[$q] = trim($fields[$q]);
           $result->execute();
           $numeric_score += trim($fields[$q]);
