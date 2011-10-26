4e87e442ed891ea04b0774202f64f36ef70c949c
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2011-10-26 14:18:30 +0000

    Fix for Ticket #391.
    git-svn-id: svn://suivarro/touchstone/trunk@1396 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- lang/pl/paper/print.php
+++ lang/pl/paper/print.php
@@ -1,12 +1,3 @@
 <?php
-require '../lang/' . $language . '/question/sct_shared.php';
-$string['markingmethod'] = 'Metoda punktacji';
-$string['markperoption'] = 'Punktacja za opcje';
-$string['markperquestion'] = 'Punktacja za pytanie';
-$string['negativemarking'] = 'ujemna punktacja';
-$string['allwithnegative'] = 'wszystkie elementy opcji punktowane z ujemną punktacją';
-$string['correctplusbonus'] = 'punkty za poprawne opcje, plus 1 punkt dodatkowy za poprawną kolejność';
-$string['mark'] = 'punkt';
-$string['halfmarksneighbours'] = 'punkty ułamkowe dla sąsiadów';
-$string['exactorder'] = 'poprawna kolejność';
+require '../lang/' . $language . '/paper/start.php';
 ?>
