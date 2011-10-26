b617310fc3be676fa8eb9f41601033a8034f4416
Nikodem Miranowicz <Nikodem.Miranowicz@notitngham.ac.uk>
2011-10-26 10:18:44 +0000

    lang in print.php
    git-svn-id: svn://suivarro/touchstone/trunk@1379 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- lang/en/paper/print.php
+++ lang/en/paper/print.php
@@ -3,8 +3,6 @@ require '../lang/' . $language . '/question/sct_shared.php';
 $string['markingmethod'] = 'Marking Method';
 $string['markperoption'] = 'Mark per Option';
 $string['markperquestion'] = 'Mark per Question';
-$string['markingmethod'] = 'Marking Method';
-$string['markperoption'] = 'Mark per Option';
 $string['negativemarking'] = 'negative marking';
 $string['allwithnegative'] = 'all options items marked with negative marking';
 $string['correctplusbonus'] = 'marks for correct options, plus 1 bonus mark for fully correct order';
