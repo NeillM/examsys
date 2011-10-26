cc774fc00755728df228812e76130faaa0e1fae4
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2011-10-26 08:56:16 +0000

    Fix for Ticket #389.
    git-svn-id: svn://suivarro/touchstone/trunk@1375 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- folder/all.php
+++ folder/all.php
@@ -28,7 +28,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <head>
-<title>Rog&#333;</title>
+<title>Rogō<?php echo " $cfg_install_type"; ?></title>
 <link rel="stylesheet" type="text/css" href="../css/submenu.css" />
 <style>
 .divider {padding-left:16px; padding-bottom:2px; font-weight:bold}
