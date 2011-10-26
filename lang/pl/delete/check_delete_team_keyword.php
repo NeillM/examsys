c5819846b1c4aa7145accd3ae97d1cc47d43616c
Nikodem Miranowicz <Nikodem.Miranowicz@notitngham.ac.uk>
2011-10-26 16:34:52 +0000

    lang string accessdeniedsysadmin for admin_auth.inc & sysadmin_auth.inc
    git-svn-id: svn://suivarro/touchstone/trunk@1406 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- lang/pl/delete/check_delete_team_keyword.php
+++ lang/pl/delete/check_delete_team_keyword.php
@@ -3,5 +3,4 @@ $string['recyclebin'] = 'Kosz';
 $string['deletekeyword'] = 'Usuń słowo kluczowe';
 $string['msg'] = "Czy na pewno usunąć słowo kluczowe '%s'?";
 $string['delete'] = 'Usuń';
-$string['accessdeniedmsg'] = "<strong>Access Denied</strong><br />you are attempting to access a system admin only screen.";
 ?>
