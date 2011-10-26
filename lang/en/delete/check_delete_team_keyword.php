24850012e52ca8f4bf150bda55aad2d7f02b656e
Nikodem Miranowicz <Nikodem.Miranowicz@notitngham.ac.uk>
2011-10-26 16:20:27 +0000

    lang additions to admin_auth.inc and check_delete_team_keyword.php
    git-svn-id: svn://suivarro/touchstone/trunk@1403 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- lang/en/delete/check_delete_team_keyword.php
+++ lang/en/delete/check_delete_team_keyword.php
@@ -3,4 +3,5 @@ $string['recyclebin'] = 'Recycle Bin';
 $string['deletekeyword'] = 'Delete Keyword';
 $string['msg'] = "Are you sure you wish to delete '%s' keyword?";
 $string['delete'] = 'Delete';
+$string['accessdeniedmsg'] = "<strong>Access Denied</strong><br />you are attempting to access a system admin only screen.";
 ?>
