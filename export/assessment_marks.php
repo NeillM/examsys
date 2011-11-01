db3324218514b4f0781e7ecdfc27b353a0a635bb
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2011-11-01 17:39:34 +0000

    Fixes to exporting marks to Excel.
    git-svn-id: svn://suivarro/touchstone/trunk@1524 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- export/assessment_marks.php
+++ export/assessment_marks.php
@@ -40,27 +40,30 @@
       }
       $q_no = 1;
       foreach ($paper_buffer as $q_id => $question) {
-        //$q_id = $question['id'];
-        if ($question['q_type'] == 'extmatch' or $question['q_type'] == 'matrix') {
+        if (array_key_exists($q_id,$excluded)) {
+          $tmp_exclude = $excluded[$q_id];
+        } else {
+          $tmp_exclude = '0000000000000000000000000000000000000000';
+        }
+        if ($question['q_type'] == 'extmatch' or $question['q_type'] == 'matrix' and $question['score_method'] == 'Mark per Option') {
           $sub_parts = 0;
           $paper_answers = explode("|",$question['correct'][0]);
-          if (array_key_exists($q_id,$excluded)) {
-            $tmp_exclude = $excluded[$q_id];
-          } else {
-            $tmp_exclude = '0000000000000000000000000000000000000000';
-          }
           for ($a=0; $a<count($paper_answers); $a++) {
             $sub_parts += substr_count($paper_answers[$a],'$');
           
             if ($paper_answers[$a] != '' and substr($tmp_exclude,$a+$sub_parts,1) == '0') echo ',Q' . $q_no . '.' . ($a+1);
           }
-        } elseif ($question['q_type'] == 'dichotomous' or $question['q_type'] == 'labelling' or $question['q_type'] == 'blank') {
-          if (array_key_exists($q_id,$excluded)) {
-            $tmp_exclude = $excluded[$q_id];
-          } else {
-            $tmp_exclude = '0000000000000000000000000000000000000000';
-          }
+        } elseif ($question['q_type'] == 'dichotomous' or $question['q_type'] == 'blank' and $question['score_method'] == 'Mark per Option') {
           for ($a=0; $a<count($question['correct']); $a++) {
+            if (substr($tmp_exclude,$a,1) == '0') echo ',Q' . $q_no . '.' . ($a+1);
+          }
+        } elseif ($question['q_type'] == 'labelling' and $question['score_method'] == 'Mark per Option') {
+          for ($a=0; $a<count($question['correct_labels']); $a++) {
+            if (substr($tmp_exclude,$a,1) == '0') echo ',Q' . $q_no . '.' . ($a+1);
+          }
+        } elseif ($question['q_type'] == 'hotspot' and $question['score_method'] == 'Mark per Option') {
+          $paper_answers = explode("|",$question['correct'][0]);
+          for ($a=0; $a<count($paper_answers); $a++) {
             if (substr($tmp_exclude,$a,1) == '0') echo ',Q' . $q_no . '.' . ($a+1);
           }
         } else {
@@ -70,6 +73,7 @@
       }
       echo "\n";
     }
+    
     // Write out the raw data.
     if ($individual['visible'] == 1) {
       if ($paper_type < 3) {
@@ -81,16 +85,15 @@
         if (isset($individual['mark_array'][$q_id])) {
           if (is_array($individual['mark_array'][$q_id])) {
             foreach ($individual['mark_array'][$q_id] as $tmp_mark) {
-              echo ',' . $tmp_mark;
+              echo "," . $tmp_mark;
             }
           } else {
-            echo ',' . $individual['mark_array'][$q_id];
+            echo "," . $individual['mark_array'][$q_id];
           }
         } else {
-          echo ',0';
+          echo ",0";
         }
       }
-      //foreach ($individual['mark_array'] as $individual_mark) echo ",$individual_mark";
       echo "\n";
     }
     $row_written++;
