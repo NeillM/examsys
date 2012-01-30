1a8448e9be3fb79bf002d9052bec3505a2bc216d
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2012-01-30 11:25:18 +0000

    Fix to handling image hotspots with carriage returns which was breaking frequency/discrimination reports.
    git-svn-id: svn://suivarro/touchstone/trunk@1811 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- reports/frequency_discrimination_analysis.php
+++ reports/frequency_discrimination_analysis.php
@@ -821,12 +821,15 @@
           }
           $coords = rtrim($coords, '|');
           
+          $tmp_correct = str_replace("'", "\'", trim($correct));
+          $tmp_correct = str_replace("&nbsp;", " ", $tmp_correct);
+          $tmp_correct = preg_replace('/\r\n/', '', $tmp_correct); 
           ?>
           <div align="center">
           <script language="JavaScript">
       			function swfLoaded<?php echo $q_no; ?>(message) {
       				var num = message.substring(5,message.length);
-      				setUpFlash(num, message, '<?php echo $language; ?>', '<?php echo $q_media; ?>', '<?php echo trim($correct); ?>', '<?php echo $coords; ?>','0');
+      				setUpFlash(num, message, '<?php echo $language; ?>', '<?php echo $q_media; ?>', '<?php echo $tmp_correct; ?>', '<?php echo $coords; ?>','0');
       			}
       			write_string('<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="https://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" id="flash<?php echo $q_no; ?>" width="<?php echo ($q_media_width + 302); ?>" height="<?php echo ($q_media_height + 25); ?>" align="middle">');
       			write_string('<param name="allowScriptAccess" value="always" />');
