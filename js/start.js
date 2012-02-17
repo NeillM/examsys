a375abed93c160f81bda2648426385dbe9cac63e
Rob Ingram <Rob.Ingram@notitngham.ac.uk>
2012-02-17 16:38:31 +0000

    For calculation questions only allow numeric input (plus .).  Still allows multiple full stops but better than allowing any input whatsoever
    git-svn-id: svn://suivarro/touchstone/trunk@1921 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- js/start.js
+++ js/start.js
@@ -1,5 +1,7 @@
 $(function () {
   $('.rankselect').change(rankCheck);
+
+  $(".calc-answer").keydown(filterKeypress);
 });
 
 function refreshparent() {
@@ -141,3 +143,21 @@ function saveMath() {
   }
 }
 
+function filterKeypress(event) {
+  // Allow: backspace, delete, tab and escape
+  if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 190 || event.keyCode == 9 || event.keyCode == 27 ||
+  // Allow: Ctrl+A
+  (event.keyCode == 65 && event.ctrlKey === true) ||
+  // Allow: home, end, left, right
+  (event.keyCode >= 35 && event.keyCode <= 39)) {
+    // let it happen, don't do anything
+    return;
+  }
+  else {
+    // Ensure that it is a number and stop the keypress
+    if (((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 ))
+            || event.shiftKey === true) {
+      event.preventDefault();
+    }
+  }
+}
