f559efa3827ff324449745a8dd497cd9a718dbf3
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2012-02-28 17:47:30 +0000

    Improving Chrome compatibility with CSS.
    git-svn-id: svn://suivarro/touchstone/trunk@1999 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- admin/clear_guest_users.php
+++ admin/clear_guest_users.php
@@ -28,6 +28,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta http-equiv="content-type" content="text/html;charset=<?php echo $cfg_page_charset ?>" />
 <title>Clear Guest Users</title>
 <link rel="stylesheet" type="text/css" href="../css/submenu.css" />
@@ -48,7 +49,7 @@
 <div id="content" class="content" style="font-size:80%">
 <table class="header">
 <tr><th><div class="breadcrumb"><a href="../staff/index.php"><?php echo $string['home']; ?></a>&nbsp;&nbsp;<img src="../artwork/breadcrumb_arrow.png" width="4" height="7" alt="-" />&nbsp;&nbsp;<a href="./index.php"><?php echo $string['administrativetools']; ?></a></div><div style="font-size:200%; margin-left:10px; font-weight:bold"><?php echo $string['clearguestaccounts']; ?></div></th><th style="text-align:right; vertical-align:top; padding-top:2px; padding-right:6px"><a href="#" onclick="launchHelp(243); return false;"><img src="../artwork/small_help_icon.gif" width="16" height="16" alt="<?php echo $string['help']; ?>" border="0" /></a></th></tr>
-<tr><th colspan="2" class="bevel"><img src="../artwork/header_horizontal_line.gif" width="100%" height="3" alt="Line" /></th></tr>
+<tr><th colspan="2" class="bevel"></th></tr>
 </table>
 <br />
 
