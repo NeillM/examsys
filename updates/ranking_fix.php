97f23354940b381df8e9151c7311925df648a9e9
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2012-02-29 13:24:02 +0000

    Alterations to fixing scripts to handle logs 0, 1 and 2.
    git-svn-id: svn://suivarro/touchstone/trunk@2007 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- updates/ranking_fix.php
+++ updates/ranking_fix.php
@@ -34,9 +34,9 @@ while ($result->fetch()) {
   }
   $result2->close();
   
-  for ($log_id=0; $log_id<3; $log_id++) {
+  for ($log_no=0; $log_no<3; $log_no++) {
     // Get student answers for the question
-    $result2 = $mysqli->prepare("SELECT id, user_answer, mark FROM log$log_id WHERE q_id=$q_id");
+    $result2 = $mysqli->prepare("SELECT id, user_answer, mark FROM log$log_no WHERE q_id=$q_id");
     $result2->execute();
     $result2->store_result();
     $result2->bind_result($log_id, $user_answer, $original_mark);
@@ -87,9 +87,9 @@ while ($result->fetch()) {
         }
       }
       
-      echo "<div>$score_method, UPDATE log$log_id SET mark=$mark, totalpos=$totalpos WHERE id=$log_id</div>";
+      echo "<div>$score_method, UPDATE log$log_no SET mark=$mark, totalpos=$totalpos WHERE id=$log_id</div>";
 
-      $adjust = $mysqli->prepare("UPDATE log$log_id SET mark=?, totalpos=? WHERE id=?");
+      $adjust = $mysqli->prepare("UPDATE log$log_no SET mark=?, totalpos=? WHERE id=?");
       $adjust->bind_param('dii', $mark, $totalpos, $log_id);
       $adjust->execute();
       $adjust->close();
