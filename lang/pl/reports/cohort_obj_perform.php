a3d31ac3950d76d09a61cc693de6907b013e1391
Nikodem Miranowicz <Nikodem.Miranowicz@notitngham.ac.uk>
2012-03-09 14:21:04 +0000

    lang corrections
    git-svn-id: svn://suivarro/touchstone/trunk@2104 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- lang/pl/reports/cohort_obj_perform.php
+++ lang/pl/reports/cohort_obj_perform.php
@@ -5,7 +5,7 @@ $string['totalcandidate'] = 'Całkowita liczba kandydatów';
 $string['key1'] = 'Wypełnienie 80-100% celów szczegółowych';
 $string['key2'] = 'Wypełnienie 50-79% celów szczegółowych';
 $string['key3'] = 'Wypełnienie 0-49% celów szczegółowych';
-$string['key4'] = '<a href="" onclick="return false;">przejście</a> do sekcji VLE w poszukiwaniu dalszych szczegółów';
+$string['key4'] = '<a href="" onclick="return false;" style="cursor: text;">hiperłącze</a> - przejście do sekcji VLE w poszukiwaniu dalszych szczegółów';
 $string['msg'] = 'Poniżej znajduje się lista %d specyficznych celów kształcenia weryfikowanych przez ten arkusz. Ponieważ pytania złożone mogą testować te same cele, może zdarzyć się częściowe wypełnianie celu. Lista ta jest uporządkowana malejąco wg. procentu grupy wypełniającej dany cel.';
 $string['uppersize'] = 'rozmiar górnej grupy';
 $string['lowersize'] = 'rozmiar dolnej grupy';
