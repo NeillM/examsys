48c52516bb7ea927aed260487f66bceede69c87f
Nikodem Miranowicz <Nikodem.Miranowicz@notitngham.ac.uk>
2012-03-12 16:20:54 +0000

    lang abstraction in addedit/media_upload/media_upload_hotspot.php
    git-svn-id: svn://suivarro/touchstone/trunk@2151 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- include/question/addedit/media_upload/media_upload_hotspot.php
+++ include/question/addedit/media_upload/media_upload_hotspot.php
@@ -27,6 +27,6 @@
       <h2><?php echo $string['uploadimage'] ?></h2>
       <p><?php echo $string['uploadinstructions'] ?></p>
       <p><label for="q_media" class="heavy"><?php echo $string['image'] ?></label> <input id="q_media" name="q_media" size="45" type="file" /></p>
-      <p class="align-centre"><input type="button" name="cancel" value="Cancel" onclick="javascript: history.back();" class="submit cancel" /> <input type="submit" name="submit_media" value="<?php echo $string['next'] ?>" class="submit cancel" /></p>
+      <p class="align-centre"><input type="button" name="cancel" value="<?php echo $string['cancel'] ?>" onclick="javascript: history.back();" class="submit cancel" /> <input type="submit" name="submit_media" value="<?php echo $string['next'] ?>" class="submit cancel" /></p>
     </div>
   </div>
