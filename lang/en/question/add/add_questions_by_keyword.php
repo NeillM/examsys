fc4f4ca5fe1b97c3a6afd31f9feabc3c5b97d3b1
Rob Ingram <Rob.Ingram@notitngham.ac.uk>
2012-03-12 15:23:56 +0000

    Ticket #637: Fix undefined index in add questions to paper by Keywords
    git-svn-id: svn://suivarro/touchstone/trunk@2139 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- lang/en/question/add/add_questions_by_keyword.php
+++ lang/en/question/add/add_questions_by_keyword.php
@@ -1,4 +1,6 @@
 <?php
+require '../../lang/' . $language . '/include/question_types.inc';
+
 $string['bykeywords'] = 'By Keyword';
 $string['question'] = 'Question';
 $string['type'] = 'Type';
--- lang/pl/question/add/add_questions_by_keyword.php
+++ lang/pl/question/add/add_questions_by_keyword.php
@@ -1,4 +1,6 @@
 <?php
+require '../../lang/' . $language . '/include/question_types.inc';
+
 $string['bykeywords'] = 'Wg. słów kluczowych';
 $string['question'] = 'Pytanie';
 $string['type'] = 'Typ';
