a729a0c95ae6d0bd7e7ced59bdd4d3155dc446a5
Nikodem Miranowicz <Nikodem.Miranowicz@notitngham.ac.uk>
2012-03-12 16:13:13 +0000

    lang abstraction in addedit/media_upload/media_upload_labelling.php
    git-svn-id: svn://suivarro/touchstone/trunk@2149 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- include/question/addedit/media_upload/media_upload_labelling.php
+++ include/question/addedit/media_upload/media_upload_labelling.php
@@ -27,9 +27,9 @@
       <h2><?php echo $string['uploadimage'] ?></h2>
       <p><?php echo $string['uploadinstructions'] ?></p>
       <p><label for="q_media" class="heavy"><?php echo $string['image'] ?></label> <input id="q_media" name="q_media" size="45" type="file" /></p>
-      <p class="compact"><a href="#" id="media-labels-link">Upload images for labels</a></p>
+      <p class="compact"><a href="#" id="media-labels-link"><?php echo $string['upload_images'] ?></a></p>
       <div id="media-label-upload">
-        <p>Maximum size 200 pixels square.</p>
+        <p><?php echo $string['maximum_size'] ?></p>
         <p><label for="label_media1" class="heavy"><?php echo $string['image'] ?> 1</label> <input id="label_media1" name="label_media1" size="45" type="file" /></p>
         <p><label for="label_media2" class="heavy"><?php echo $string['image'] ?> 2</label> <input id="label_media2" name="label_media2" size="45" type="file" /></p>
         <p><label for="label_media3" class="heavy"><?php echo $string['image'] ?> 3</label> <input id="label_media3" name="label_media3" size="45" type="file" /></p>
@@ -37,6 +37,6 @@
         <p><label for="label_media5" class="heavy"><?php echo $string['image'] ?> 5</label> <input id="label_media5" name="label_media5" size="45" type="file" /></p>
         <p class="compact"><label for="label_media6" class="heavy"><?php echo $string['image'] ?> 6</label> <input id="label_media6" name="label_media6" size="45" type="file" /></p>
       </div>
-      <p class="align-centre"><input type="button" name="cancel" value="Cancel" onclick="javascript: history.back();" class="submit cancel" /> <input type="submit" name="submit_media" value="<?php echo $string['next'] ?>" class="submit cancel" /></p>
+      <p class="align-centre"><input type="button" name="cancel" value="<?php echo $string['cancel'] ?>" onclick="javascript: history.back();" class="submit cancel" /> <input type="submit" name="submit_media" value="<?php echo $string['next'] ?>" class="submit cancel" /></p>
     </div>
   </div>
