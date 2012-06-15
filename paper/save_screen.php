d6d20af5c6daa308e7e04cb3f0f0b0b5667908b7
Anthony Brown <Anthony.Brown@nottingham.ac.uk>
2012-06-15 15:50:02 +0000

    Ajax save bug fix for header output before session_start()
    git-svn-id: svn://suivarro/touchstone/trunk@2565 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- paper/save_screen.php
+++ paper/save_screen.php
@@ -1,40 +1,34 @@
-﻿<?php
+<?php
 // This file is part of Rogō
 //
-// Rogō is free software: you can redistribute it and/or modify
+// Rog? is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// Rogō is distributed in the hope that it will be useful,
+// Rog? is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Rogō.  If not, see <http://www.gnu.org/licenses/>.
+// along with Rog?.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
-* 
-* This script can only be called from a paper in 'summative' mode from one of the four green fire exit icons displayed in 'start.php'.
-*  It does three main things: 
-*        1) record the current screen data to the 'log' table, 
-*        2) blank the screen to prevent plagiarism among evacuating examinees, and 
-*        3) has a 'continue' button at the bottom of the screen with passes the correct parameters back to 'start.php' if the 
-*           examinees are allowed to re-enter the building.
-* 
+*
+* This script can only be called from start.php for ajax saving'.
 * @author Anthony Brown
 * @version 1.0
 * @copyright Copyright (c) 2012 The University of Nottingham
 * @package
 */
 
-require '../include/staff_student_auth.inc';
-require '../include/marking_functions.inc';
-require '../include/errors.inc';
-require '../include/paper_security.inc';
-
+require_once '../include/staff_student_auth.inc';
+require_once '../include/marking_functions.inc';
+require_once '../include/errors.inc';
+require_once '../include/paper_security.inc';
 $displayDebug = false; //ajax call so debug info messes up the output
+
 check_var('id', 'GET', true, false);
 
 $stmt = $mysqli->prepare("SELECT property_id, paper_type, labs, UNIX_TIMESTAMP(start_date), UNIX_TIMESTAMP(end_date), moduleID, calendar_year, password FROM properties WHERE crypt_name=?");
@@ -45,16 +39,16 @@ $stmt->bind_result($property_id, $paper_type, $labs, $start_date, $end_date, $mo
 while ($stmt->fetch()) {
   $attempt = 1; //default attempt to 1 overwritten if the student is resit candidate
   if (strpos($userroles,'Student') !== false) {
-  
+
     // Check for additional password on the paper
     check_paper_password($password);
-    
+
     // Check time security
     check_datetime($start_date, $end_date);
 
     //Check room security
     $low_bandwidth = check_labs($paper_type, $labs, $mysqli);
-      
+
     //get modules if the user is a student and the paper is not formative
     $attempt = check_modules($userID, $moduleID, $calendar_year, $mysqli);
 
@@ -70,5 +64,5 @@ $stmt->free_result();
 $stmt->close();
 //TODO we need to add some error checking in here. maby wrap this whole function in a transaction ??
 $ret = record_marks($property_id, $mysqli, $userID, $paper_type, $grade, $year, $attempt, $userroles);
-echo $_POST['randomPageID'];
+echo $_POST['randumPageID'];
 ?>
