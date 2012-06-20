257603bebfdded6f6da32daf89d67af6c74fd2cf
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2012-06-20 17:10:23 +0000

    Addition of question and objective-based feedback to the student landing page.
    git-svn-id: svn://suivarro/touchstone/trunk@2570 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- lang/en/students/index.php
+++ lang/en/students/index.php
@@ -2,4 +2,8 @@
 require_once '../lang/' . $language . '/folder/details.php';
 
 $string['nopapers'] = 'You have no papers available at this time.';
+$string['to'] = 'to';
+$string['feedbackonassessment'] = 'Feedback on assessment at';
+$string['screen'] = 'screen';
+$string['screens'] = 'screens';
 ?>
