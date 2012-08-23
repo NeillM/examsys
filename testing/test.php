46b722b5cc97f85539199fe988512c13879a19fc
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2012-08-23 14:16:25 +0000

    Just a test.
    git-svn-id: svn://suivarro/touchstone/trunk@2853 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- testing/test.php
+++ testing/test.php
@@ -1,5 +1,5 @@
 <?php
-  $returned_data = file_get_contents("http://saturn-exports.nottingham.ac.uk/touchstone.ashx?campus=uk&code=A14ACE&year=2012");
+  $returned_data = file_get_contents("http://saturn-exports.nottingham.ac.uk/touchstone.ashx?campus=uk&code=A14ACE&year=2011");
   echo($returned_data)
 
 ?>
