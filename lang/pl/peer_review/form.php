df93c553cbd159009748155fd674bd8388c15dee
Nikodem Miranowicz <Nikodem.Miranowicz@notitngham.ac.uk>
2012-08-23 13:15:19 +0000

    fixing lang translations
    git-svn-id: svn://suivarro/touchstone/trunk@2847 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- lang/pl/peer_review/form.php
+++ lang/pl/peer_review/form.php
@@ -1,5 +1,5 @@
 <?php
-$string['peerreview'] = 'Recenzja wewnetrzna';  
+$string['peerreview'] = 'Recenzja wewnętrzna';  
 $string['save'] = 'Zachowaj';
 $string['error_time'] = 'Arkusz który próbujesz otworzyć jest dostępny tylko pomiędzy %s i %s';
 
