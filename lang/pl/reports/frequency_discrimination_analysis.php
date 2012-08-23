0340f1fca3db98d7c7863f167e98add1b58c69a6
Nikodem Miranowicz <Nikodem.Miranowicz@notitngham.ac.uk>
2012-08-23 13:55:53 +0000

    fixing lang translations
    git-svn-id: svn://suivarro/touchstone/trunk@2850 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- lang/pl/reports/frequency_discrimination_analysis.php
+++ lang/pl/reports/frequency_discrimination_analysis.php
@@ -6,14 +6,14 @@ $string['groupsizes'] = 'Górne/dolne rozmiary grup';
 $string['pergroup'] = 'na grupę';
 $string['boldstems'] = 'pogrubiona opcja';//1Bold stems??
 $string['correctanswers'] = 'reprezentuje poprawną odpowiedź (odpowiedzi)';
-$string['p_definition'] = 'trudność elementu (proporocje studentów odpowiadających poprawnie)';
+$string['p_definition'] = 'trudność elementu (proporcje studentów odpowiadających poprawnie)';
 $string['d_definition'] = 'moc różnicująca';
 $string['t_definition'] = 'procent <strong>całości</strong> grupy odpowiadającej';
 $string['u_definition'] = 'procent <strong>górnej</strong> części grupy odpowiadającej';
 $string['l_definition'] = 'procent <strong>dolnej</strong> części grupy odpowiadającej';
 $string['warning'] = 'Ostrzeżenie';
 $string['p_warning'] = '<strong>p < 0.2</strong> (tj. bardzo trudne)';
-$string['d_warning'] = "<strong>d < 0.15</strong> (tj. niski)<br />Rozważ czerwone flagi i jeśli uznasz, że element jest słaby wyklucz go wybierając ikonę <img src=\"../artwork/exclude_off.gif\" style=\"cursor:pointer\" width=\"23\" height=\"22\" border=\"0\" alt=\"Exclude\" /> a następnie wybierając 'Zapisz'";
+$string['d_warning'] = "<strong>d < 0.15</strong> (tj. małe rożnicowanie)<br />Przeanalizuj każdy element z czerwoną flagą i jeśli uznasz, że jest on słaby wyklucz go wybierając ikonę <img src=\"../artwork/exclude_off.gif\" style=\"cursor:pointer\" width=\"23\" height=\"22\" border=\"0\" alt=\"Exclude\" />, a następnie wybierając 'Zapisz'";
 $string['summary'] = 'Podsumowanie';
 $string['msg'] = 'Liczba elementów może być większa od liczby pytań, jako że pytania rozdzielające, etykietujące oraz o rozszerzonym dopasowaniu tworzone są przez łączenie kilku elementów każdego z indywidualnymi wartościami p i d.';
 $string['difficulty'] = 'Trudność';
