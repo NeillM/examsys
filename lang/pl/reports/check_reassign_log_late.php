7ae0f3d996ae6ad148a1359ab8353337ea033799
Nikodem Miranowicz <Nikodem.Miranowicz@notitngham.ac.uk>
2012-08-24 15:05:38 +0000

    fixing lang translations
    git-svn-id: svn://suivarro/touchstone/trunk@2871 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- lang/pl/reports/check_reassign_log_late.php
+++ lang/pl/reports/check_reassign_log_late.php
@@ -8,7 +8,7 @@ $string['reject'] = 'Odrzucono';
 $string['accept'] = 'Przyjęto';
 $string['warning'] = 'Ostrzeżenie';
 $string['msg1'] = '(podaj powód, dla którego wpisy te odrzucono lub przyjęto)';
-$string['msg2'] = 'Skrypty egzaminacyjne nie mogą być ponownie przypisane w trakcie trwania egzaminu.<br />Należy poczekać do zakończenia egzaminu.';
+$string['msg2'] = 'Zapisy egzaminów nie mogą być ponownie przypisane w trakcie trwania egzaminu.<br />Należy poczekać do zakończenia egzaminu.';
 $string['msg3'] = 'Czy na pewno przyjąć te spóźnione odpowiedzi?';
 $string['msg4'] = 'Czy na pewno odrzucić te spóźnione odpowiedzi?';
 ?>
