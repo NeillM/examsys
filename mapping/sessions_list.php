9802aa29812dc2cbb61a1c5ff5385e0db3234f5b
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2012-08-28 08:45:23 +0000

    Fix for Ticket #803.
    git-svn-id: svn://suivarro/touchstone/trunk@2880 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- mapping/sessions_list.php
+++ mapping/sessions_list.php
@@ -138,6 +138,7 @@
       } else {
         $identifier = '';
       }
+      var_dump($session['VLE']);
       if ($session['VLE'] != '') {
         echo "<tr style=\"cursor:pointer\" id=\"$id\" onmouseover=\"highlight($id)\" onmouseout=\"unhighlight($id)\" onclick=\"selSession('$id','$identifier','" . $session['calendar_year'] . "','" . $session['VLE'] . "',event);\" ondblclick=\"editVLESession('" . $session['calendar_year'] . "');\">";
       } else {
