33a7502ae34c37dc4819e65191908a917fad0d50
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2012-08-29 09:54:46 +0000

    Fix for Ticket #795.
    git-svn-id: svn://suivarro/touchstone/trunk@2906 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- admin/add_module.php
+++ admin/add_module.php
@@ -106,6 +106,9 @@ if (isset($_POST['submit']) and $unique_moduleid == true) {
       $xml = new SimpleXMLElement($returned_data);
       $enrolement_details = '';
       
+      var_dump($xml);
+      exit;
+      
       if (isset($xml->Module->Membership->Student)) {
         foreach ($xml->Module->Membership->Student as $student) {
           $student->Title = trim($student->Title);
