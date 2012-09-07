8d9e47f5e5e0569493ebcf72cfac18c230391de0
Nikodem Miranowicz <Nikodem.Miranowicz@notitngham.ac.uk>
2012-09-07 12:49:27 +0000

    css for load_session_from_txt.php
    git-svn-id: svn://suivarro/touchstone/trunk@3146 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- mapping/load_session_from_txt.php
+++ mapping/load_session_from_txt.php
@@ -96,6 +96,8 @@ if (isset($_POST['submit'])) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta http-equiv="content-type" content="text/html;charset=<?php echo $cfg_page_charset ?>" />
   <title>Rogo: <?php echo $string['importfromfile'] . ' ' . $cfg_install_type; ?></title>
+  <link rel="stylesheet" type="text/css" href="../css/body.css" />
+  <link rel="stylesheet" type="text/css" href="../css/header.css" />
   <link rel="stylesheet" type="text/css" href="../css/submenu.css" />
   <style type="text/css">
     img {border:none;}
