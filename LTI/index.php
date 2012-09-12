fcbd489453207ac0bf3936a956f92226eb3b0bcb
Simon Atack <Simon.Atack@notitngham.ac.uk>
2012-09-12 09:56:33 +0000

    remove LTI debug
    git-svn-id: svn://suivarro/touchstone/trunk@3215 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- LTI/index.php
+++ LTI/index.php
@@ -263,7 +263,6 @@ END;
 
     echo "<table border=\"0\" style=\"padding-bottom:5px; width:100%; color:#1E3287\"><tr><td><nobr>" . $string['papersoncurrentmodule'] . "</nobr></td><td style=\"width:98%\"><hr noshade=\"noshade\" style=\"border:0px; height:1px; color:#E5E5E5; background-color:#E5E5E5; width:100%\" /></td></tr></table>\n";
 
-var_dump($data);
     foreach ($data as $v) {
       $moduleid = $v[1];
 
