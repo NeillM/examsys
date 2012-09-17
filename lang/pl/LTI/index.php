3b4cc7cb94ff37df7282a8888cf607d2bd1acd91
Nikodem Miranowicz <Nikodem.Miranowicz@notitngham.ac.uk>
2012-09-17 09:12:49 +0000

    lang for lang/pl/LTI/index.php
    git-svn-id: svn://suivarro/touchstone/trunk@3266 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- lang/pl/LTI/index.php
+++ lang/pl/LTI/index.php
@@ -5,9 +5,8 @@ include 'lti_common.php';
 $string['NoPapers'] = 'Brak arkuszy dla tego modułu'; 
 $string['NoPapersDesc'] = 'Brak arkuszy dla tego modułu.  Być może wynika to z faktu, że właśnie utworzyłeś link z VLE wraz z nowym modułem i stąd nie masz jeszcze skonfigurowanych żadnych arkuszy.<br /><br />Zamknij przeglądarkę (<strong>jest to bardzo ważne</strong>) i przejdź do głównej strony Rogo aby utworzyć arkusz.';
 
-$string['NoModCreateTitle'] = 'Module Creation is not enabled';  //TODO Nikko
-$string['NoModCreate'] = 'Module Creation from LTI is not enabled in the config so we cannot create the module with course code: '; //TODO Nikko
-$string['NotAddedToModuleTitle'] = 'Not Added to Module Team'; //TODO Nikko
-$string['NotAddedToModule'] = 'Adding to Module Team is not enabled for LTI, this happened for module: '; //TODO Nikko
-
+$string['NoModCreateTitle'] = 'Tworzenie modułów nie jest umożliwione '; 
+$string['NoModCreate'] = 'Tworzenie modułów z LTI nie jest umożliwione w konfuguracji, nie jest więc możliwe utworzenie modułu z kodem kursu: '; 
+$string['NotAddedToModuleTitle'] = 'Nie dodano do zespołu modułu'; 
+$string['NotAddedToModule'] = 'Dodawanie do zespołu modułu nie jest umożliwione dla LTI, dotyczy to modułu: '; 
 ?>
