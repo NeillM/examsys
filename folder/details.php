39067a9603bb2bce6f02e3cd2946393139585d7c
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2012-09-26 11:03:38 +0000

    Fix for personal folder warning (Ticket #879).
    git-svn-id: svn://suivarro/touchstone/trunk@3389 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- folder/details.php
+++ folder/details.php
@@ -272,7 +272,7 @@ if (isset($_GET['module']) and $_GET['module'] != '') {
 }
 
 // Is it a self-enrol module.
-if ($module_details['selfenroll'] == 1) {
+if (isset($module_details['selfenroll']) and $module_details['selfenroll'] == 1) {
   $selfenrol_url = $protocol . $_SERVER['HTTP_HOST'] . $cfg_root_path . '/self_enrol.php?moduleid=' . $_GET['module'];
   echo "<div style=\"padding-left:10px\"><img src=\"../artwork/module_icon_16.png\" width=\"16\" height=\"16\" alt=\"modules\" /> <span style=\"color:#C00000\">Self-enrol URL:</span> <a href=\"$selfenrol_url\">$selfenrol_url</a></div>\n<br />";
 }
