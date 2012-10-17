feddb0e2a7e4aa20ad5f03360140ed1d0bd69b1d
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2012-10-17 10:41:15 +0000

    Fix for Ticket #894 - external examiners with non-scheduled papers
    git-svn-id: svn://suivarro/touchstone/trunk@3412 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- reviews/index.php
+++ reviews/index.php
@@ -76,7 +76,7 @@
 <p style="margin-left:15px; font-weight:bold"><?php echo $string['yourpapersforreview']; ?></p>
 <table cellpadding="0" cellspacing="2" border="0" style="margin-left:10px; font-size:90%">
 <?php
-  $result = $mysqli->prepare("SELECT paper_type, paper_title, property_id, bidirectional, fullscreen, MAX(screen) AS max_screen, DATE_FORMAT(external_review_deadline,'%Y%m%d') AS external_review_deadline, DATE_FORMAT(external_review_deadline,'$cfg_short_date') AS display_deadline, crypt_name FROM (properties, papers) WHERE deleted IS NULL AND DATE_ADD(start_date, INTERVAL 1 WEEK) > NOW() AND properties.property_id=papers.paper AND externals LIKE '%$userID%' GROUP BY paper");
+  $result = $mysqli->prepare("SELECT paper_type, paper_title, property_id, bidirectional, fullscreen, MAX(screen) AS max_screen, DATE_FORMAT(external_review_deadline,'%Y%m%d') AS external_review_deadline, DATE_FORMAT(external_review_deadline,'$cfg_short_date') AS display_deadline, crypt_name FROM (properties, papers) WHERE deleted IS NULL AND (DATE_ADD(start_date, INTERVAL 1 WEEK) > NOW() or start_date IS NULL) AND properties.property_id=papers.paper AND externals LIKE '%$userID%' GROUP BY paper");
   $result->execute();
   $result->store_result();
   $result->bind_result($paper_type, $paper_title, $property_id, $bidirectional, $fullscreen, $max_screen, $external_review_deadline, $display_deadline, $crypt_name);
