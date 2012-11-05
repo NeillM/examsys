1637c251ff7b6bef910b88e527ec048a7b95224c
Simon Atack <Simon.Atack@notitngham.ac.uk>
2012-11-05 11:42:07 +0000

    Creating new class file
    git-svn-id: svn://suivarro/touchstone/branches/userobject@3457 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- classes/userobject.class.php
+++ classes/userobject.class.php
@@ -6,14 +6,14 @@
  * Time: 11:32
  *
  *
- * Userobject Class
+ * UserObject Class
  *
  * @author Simon Atack
  * @version 1.0
  * @copyright Copyright (c) 2012 The University of Nottingham
  * @package
  */
-class userobject {
+class UserObject {
 
   // include old variables as private ones in this class
   private $userId,$password, $userID, $userroles, $title, $initials, $surname, $username, $email, $grade, $year, $special_needs, $record_no, $split_username;
