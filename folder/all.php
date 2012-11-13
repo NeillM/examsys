1ccbf8196c28c5506c8b0563b69bc8bf475bfd5f
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2012-11-13 15:32:31 +0000

    Fix error message in All Modules list.
    git-svn-id: svn://suivarro/touchstone/trunk@3504 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- folder/all.php
+++ folder/all.php
@@ -48,8 +48,8 @@
   <script src="../js/sidebar.js" type="text/javascript"></script>
   <script language="JavaScript">
     function displayCredits(){
-      notice=window.open("../credits/credits.php","credits","width=700,height=487,scrollbars=no,resizable=no,toolbar=no,location=no,directories=no,status=0,menubar=0");
-      notice.moveTo(screen.width/2-350,screen.height/2-243)
+      notice=window.open("../credits/index.php","credits","width=696,height=500,scrollbars=no,resizable=no,toolbar=no,location=no,directories=no,status=0,menubar=0");
+      notice.moveTo(screen.width/2-350,screen.height/2-250)
       if (window.focus) {
         notice.focus();
       }
