3dc7aae60a01d22ec22574e5093aa804301d0541
Nikodem Miranowicz <Nikodem.Miranowicz@notitngham.ac.uk>
2012-11-14 12:17:10 +0000

    lang translations
    git-svn-id: svn://suivarro/touchstone/trunk@3515 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- lang/pl/question/longitudinal_performance.php
+++ lang/pl/question/longitudinal_performance.php
@@ -1,10 +1,10 @@
 <?php
-$string['longitudinalperformance'] = 'Longitudinal Performance';   // Niko, whole file please
-$string['papername'] = 'Paper Name';
-$string['calendaryear'] = 'Calendar Year';
-$string['questionno'] = 'Question No';
-$string['datetaken'] = 'Date Taken';
-$string['cohort'] = 'Cohort';
+$string['longitudinalperformance'] = 'Wyniki długoterminowe';  
+$string['papername'] = 'Nazwa arkusza';
+$string['calendaryear'] = 'Rok kalendarzowy';
+$string['questionno'] = 'Numer pytania';
+$string['datetaken'] = 'Data egazminu';
+$string['cohort'] = 'Grupa';
 $string['p'] = 'P';
 $string['d'] = 'D';
 ?>
