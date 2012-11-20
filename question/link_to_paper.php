0afaa24e68970e57d94e8cec77ae88388868efce
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2012-11-20 14:30:05 +0000

    Fix for link to paper (Ticket #941).
    git-svn-id: svn://suivarro/touchstone/trunk@3576 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- question/link_to_paper.php
+++ question/link_to_paper.php
@@ -24,7 +24,8 @@
 
 require '../include/staff_auth.inc';
 require '../include/errors.inc';
-require '../classes/dateutils.class.php';
+require_once '../classes/dateutils.class.php';
+require_once '../classes/paperutils.class.php';
 
 check_var('q_id', 'GET', true, false);
 
@@ -88,28 +89,23 @@ if (!isset($_POST['submit'])) {
   <tr><td style="width:32px; background-color:white; border-bottom:1px solid #CCD9EA"><img src="../artwork/link_to_paper.png" width="32" height="32" alt="<?php echo $string['linktopaper']; ?>" /></td><td class="dialog_header" style="border-bottom:1px solid #CCD9EA"><?php echo $string['linktopaper']; ?></td></tr>
   </table>
 
-  <p style="margin:4px; text-align:justify; font-size:90%"><img src="../artwork/small_warning_16.png" width="16" height="16" alt="<?php echo $string['warning']; ?>" border="0" /> <?php echo $string['msg1']; ?></p>
-  <p style="margin:4px; text-align:justify; font-size:90%"><img src="../artwork/small_padlock.png" width="16" height="16" alt="<?php echo $string['warning']; ?>" border="0" /> <?php echo $string['msg2']; ?></p>
+  <p style="margin:4px; text-align:justify; font-size:90%"><img src="../artwork/small_warning_16.png" width="16" height="16" alt="<?php echo $string['warning']; ?>" /> <?php echo $string['msg1']; ?></p>
+  <p style="margin:4px; text-align:justify; font-size:90%"><img src="../artwork/small_padlock.png" width="16" height="16" alt="<?php echo $string['warning']; ?>" /> <?php echo $string['msg2']; ?></p>
 
   <div style="height:200px; overflow:auto; background-color:white; border:1px solid #CCD9EA; margin:4px" id="paperlist">
   <table cellpadding="0" cellspacing="1" border="0">
 <?php
-  $teamSQL = '';
-  foreach ($teams as $team) {
-    $teamSQL .= " OR moduleID LIKE '%$team%'";
-  }
-
-  $result = $mysqli->prepare("SELECT DISTINCT property_id, paper_title, start_date, end_date, paper_type FROM properties WHERE (paper_ownerID=? $teamSQL) AND deleted IS NULL ORDER BY paper_title");
-  $result->bind_param('s', $userObject->get_user_ID());
+  $result = $mysqli->prepare("SELECT DISTINCT properties.property_id, paper_title, start_date, end_date, paper_type FROM properties, properties_modules WHERE properties.property_id = properties_modules.property_id AND (paper_ownerID = ? OR idMod IN ('" . implode("','",array_keys($staff_modules)) . "')) AND deleted IS NULL ORDER BY paper_title");
+  $result->bind_param('i', $userObject->get_user_ID());
   $result->execute();
   $result->bind_result($property_id, $paper_title, $start_date, $end_date, $paper_type);
   while ($result->fetch()) {
     if (($paper_type == '2' or $paper_type == '4') and date("Y-m-d H:i:s") > $end_date) {
-      echo "<tr><td style=\"width:20px\"><img src=\"../artwork/small_padlock.png\" width=\"16\" height=\"16\" alt=\"" . $string['warning'] . "\" border=\"0\" /></td><td><input type=\"radio\" name=\"property_id\" value=\"$paper_title\" disabled><span style=\"color:#808080\">$paper_title</span></td></tr>\n";
+      echo "<tr><td style=\"width:20px\"><img src=\"../artwork/small_padlock.png\" width=\"16\" height=\"16\" alt=\"" . $string['warning'] . "\" /></td><td><input type=\"radio\" name=\"property_id\" value=\"$paper_title\" disabled><span style=\"color:#808080\">$paper_title</span></td></tr>\n";
     } elseif ($start_date < date("Y-m-d H:i:s") and $end_date > date("Y-m-d H:i:s")) {
-      echo "<tr><td style=\"width:20px\"><img src=\"../artwork/small_warning_16.png\" width=\"16\" height=\"16\" alt=\"" . $string['warning'] . "\" border=\"0\" /></td><td><input type=\"radio\" name=\"property_id\" value=\"$paper_title\" disabled><span style=\"color:#808080\">$paper_title</span></td></tr>\n";
+      echo "<tr><td style=\"width:20px\"><img src=\"../artwork/small_warning_16.png\" width=\"16\" height=\"16\" alt=\"" . $string['warning'] . "\" /></td><td><input type=\"radio\" name=\"property_id\" value=\"$paper_title\" disabled><span style=\"color:#808080\">$paper_title</span></td></tr>\n";
     } else {
-      echo "<tr><td style=\"width:20px\">&nbsp;</td><td><input type=\"radio\" name=\"property_id\" value=\"$property_id\">$paper_title</td></tr>\n";
+      echo "<tr><td style=\"width:20px\">&nbsp;</td><td><input type=\"radio\" name=\"property_id\" value=\"$property_id\" id=\"$property_id\"><label for=\"$property_id\">$paper_title</label></td></tr>\n";
     }
   }
   $result->close();
@@ -124,7 +120,7 @@ if (!isset($_POST['submit'])) {
 <head>
   <title><?php echo $string['linktopaper']; ?></title>
   <link rel="stylesheet" type="text/css" href="../css/body.css" />
-    <link rel="stylesheet" type="text/css" href="../css/dialog.css" />
+  <link rel="stylesheet" type="text/css" href="../css/dialog.css" />
   <link rel="stylesheet" type="text/css" href="../css/header.css" />
 </head>
 
@@ -145,10 +141,7 @@ if (!isset($_POST['submit'])) {
 
   $q_IDs = explode(',', $q_id);
   for ($i=1; $i<count($q_IDs); $i++) {
-    $result = $mysqli->prepare("INSERT INTO papers VALUES (NULL, ?, ?, ?, ?)");
-    $result->bind_param('iiii', $property_id, $q_IDs[$i], $screen, $display_pos);
-    $result->execute();  
-    $result->close();
+    Paper_utils::add_question($property_id, $q_IDs[$i], $screen, $display_pos, $mysqli);
 
     $display_pos++;    
   }
@@ -156,6 +149,7 @@ if (!isset($_POST['submit'])) {
   echo "<p>" . $string['success'] . "</p>\n";
   echo "<p><input type=\"button\" value=\"" . $string['ok'] . "\" style=\"width:100px\" onclick=\"window.close();\" /></p>\n";
 }
+$mysqli->close();
 ?>
 </body>
 </html>
