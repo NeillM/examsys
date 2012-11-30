31586b497f438035e76bc4ba0c1855e7602eae5a
Rob Ingram <Rob.Ingram@notitngham.ac.uk>
2012-11-30 14:21:19 +0000

    Add assertions on page title
    git-svn-id: svn://suivarro/touchstone/trunk@3657 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- testing/selenium/tests/001-setup/001-SetupModuleTest.php
+++ testing/selenium/tests/001-setup/001-SetupModuleTest.php
@@ -44,9 +44,11 @@ class Example extends PHPUnit_Extensions_SeleniumTestCase
     $this->open("/admin/index.php");
     $this->click("css=#13 > tbody > tr > td > img");
     $this->waitForPageToLoad("30000");
+    $this->assertTitle('Schools' . $this->install_type);
 
     $this->click("link=Create new School");
     $this->waitForPageToLoad("30000");
+    $this->assertTitle('Add Schools' . $this->install_type);
 
     $this->type("id=school", "School of Selenium Testing");
     $this->select("name=facultyID", "label=Faculty of Selenium Testing");
@@ -65,9 +67,11 @@ class Example extends PHPUnit_Extensions_SeleniumTestCase
     $this->open("/admin/index.php");
     $this->click("css=#10 > tbody > tr > td > img");
     $this->waitForPageToLoad("30000");
+    $this->assertTitle('Modules' . $this->install_type);
 
     $this->click("link=Create new Module");
     $this->waitForPageToLoad("30000");
+    $this->assertTitle('Create new Module' . $this->install_type);
 
     $this->type("name=modulecode", "S01SET");
     $this->type("name=fullname", "Selenium Testing");
