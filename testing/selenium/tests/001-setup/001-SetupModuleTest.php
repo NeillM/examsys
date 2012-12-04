e4d458670c61efe99b566b58774fadce32a19d0d
Rob Ingram <Rob.Ingram@notitngham.ac.uk>
2012-12-04 11:40:53 +0000

    Add test for editing faculty
    git-svn-id: svn://suivarro/touchstone/trunk@3677 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- testing/selenium/tests/001-setup/001-SetupModuleTest.php
+++ testing/selenium/tests/001-setup/001-SetupModuleTest.php
@@ -15,26 +15,37 @@ class SetupModuleTest extends PHPUnit_Extensions_SeleniumTestCase
   {
     do_admin_login($this);
 
-    $this->open("/staff/index.php");
-    $this->click("link=Administrative Tools");
-    $this->waitForPageToLoad("30000");
-    $this->assertTitle('Rogō: Admin' . $this->install_type);
+    $this->create_faculty('Faculty of Selenium Testing');
+  }
 
-    $this->click("css=#8 > tbody > tr > td > img");
-    $this->waitForPageToLoad("30000");
-    $this->assertTitle('Faculties' . $this->install_type);
+  // TODO: Can't create faculty with same name
+  // NOTE: not possible with current implementation
 
-    $this->click("link=Create new Faculty");
+  public function testCreateFacultyForDeletion()
+  {
+    do_admin_login($this);
+
+    $this->create_faculty('Faculty of Short Lived');
+  }
+
+  public function testEditFaculty() {
+    do_admin_login($this);
+
+    $this->open("/admin/list_faculties.php");
+    $this->click("css=#4 > td > div.col10");
+    $this->click("link=Edit Faculty");
     $this->waitForPopUp("faculties", "30000");
     $this->selectWindow("name=faculties");
-    $this->type("name=add_faculty", "Faculty of Selenium Testing");
-    $this->click("name=ok");
+    $this->type("name=new_faculty", "Faculty of Short Lived2");
+    $this->click("name=submit");
     $this->selectWindow('null');
 
     $this->open("/admin/list_faculties.php");
     $this->waitForPageToLoad("30000");
-    $this->assertTextPresent('Faculty of Selenium Testing');
+    $this->assertTextPresent('Faculty of Short Lived2');
   }
+
+  // TODO: Delete Faculty
 
   /**
    * @depends testCreateFaculty
@@ -59,6 +70,9 @@ class SetupModuleTest extends PHPUnit_Extensions_SeleniumTestCase
     $this->assertTextPresent('School of Selenium Testing');
   }
 
+  // TODO: Edit School
+  // TODO: Delete School
+
   /**
    * @depends testTestCreateSchool
    */
@@ -81,6 +95,32 @@ class SetupModuleTest extends PHPUnit_Extensions_SeleniumTestCase
     $this->click("name=submit");
     $this->waitForPageToLoad("30000");
     $this->assertTextPresent('S01SET');
+  }
+
+  // TODO: Edit Module
+  // TODO: Delete Module
+
+
+  private function create_faculty($name) {
+    $this->open("/staff/index.php");
+    $this->click("link=Administrative Tools");
+    $this->waitForPageToLoad("30000");
+    $this->assertTitle('Rogō: Admin' . $this->install_type);
+
+    $this->click("css=#8 > tbody > tr > td > img");
+    $this->waitForPageToLoad("30000");
+    $this->assertTitle('Faculties' . $this->install_type);
+
+    $this->click("link=Create new Faculty");
+    $this->waitForPopUp("faculties", "30000");
+    $this->selectWindow("name=faculties");
+    $this->type("name=add_faculty", $name);
+    $this->click("name=ok");
+    $this->selectWindow('null');
+
+    $this->open("/admin/list_faculties.php");
+    $this->waitForPageToLoad("30000");
+    $this->assertTextPresent($name);
   }
 }
 ?>
