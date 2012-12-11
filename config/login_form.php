a8953f40b1c9452d20f8a67514be7d8666083cbc
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2012-12-11 16:23:09 +0000

    New log in form.
    git-svn-id: svn://suivarro/touchstone/trunk@3740 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

---
+++ config/login_form.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<title>Log In</title>
+
+<link rel="stylesheet" type="text/css" href="../css/body.css" />
+<link rel="stylesheet" type="text/css" href="../css/login_form.css" />
+</head>
+
+<body>
+<form method="post">
+<div class="mainbox">
+
+  <img src="../artwork/r_logo.gif" width="56" height="60" alt="logo" border="0" style="float:left; padding-right:8px" />
+  <div style="color:#1F497D;font-size:28pt; font-weight:bold">Rogo</div>
+  <div style="color:#1F497D;font-size:9pt">e-Assessment Management System</div>
+
+  <br />
+  <br />
+  
+  <div class="msg">The page you are trying to access requires authentication. Please sign in using your username and password:</div>
+    
+  <div style="margin-left:65px">
+  <table>
+  <tr><td>Username</td><td><input type="text" name="username" /></td></tr>
+  <tr><td>Password</td><td><input type="password" name="password" /></td></tr>
+  </table>
+  <br />
+  <input type="submit" name="submit" value=" Sign In " style="width:160px" />&nbsp;&nbsp;<input type="submit" name="submit" value=" Cancel " />
+  </div>
+
+</div>
+</form>
+
+</body>
+</html>
