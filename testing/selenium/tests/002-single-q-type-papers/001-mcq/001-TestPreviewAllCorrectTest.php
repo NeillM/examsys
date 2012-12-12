ddf959500b41cd4be9fa39ddeda29fbf81cafacc
Rob Ingram <Rob.Ingram@notitngham.ac.uk>
2012-12-12 11:33:50 +0000

    Remove redundant assertions in second test
    git-svn-id: svn://suivarro/touchstone/trunk@3758 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- testing/selenium/tests/002-single-q-type-papers/001-mcq/001-TestPreviewAllCorrectTest.php
+++ testing/selenium/tests/002-single-q-type-papers/001-mcq/001-TestPreviewAllCorrectTest.php
@@ -70,37 +70,21 @@ class ManageFacultyTest extends PHPUnit_Extensions_SeleniumTestCase
     $this->click("id=start");
     $this->waitForPopUp("paper", "30000");
     $this->selectWindow("name=paper");
-    $this->assertTextPresent('MCQ 1, vertical, display order, 1 mark, Option One correct');
-    $this->assertTextPresent('MCQ 2, horizontal, display order, 1 mark, Option Two correct');
-    $this->assertCssCount('css=input[type="radio"]', 6);
 
     $this->click("xpath=(//input[@name='q1'])[2]");
     $this->click("xpath=(//input[@name='q2'])[1]");
     $this->click("id=next");
     $this->waitForPageToLoad("30000");
-    $this->assertTextPresent('MCQ 3, DDL, display order, 2 marks, Option Two correct');
-    $this->assertTextPresent('MCQ 4, vertical, alphabetic, 1 mark, Option M correct');
-    $this->assertCssCount('css=input[type="radio"]', 3);
-    $this->assertCssCount('css=select', 2); // Include page jump DDL
 
     $this->select("name=q1", "label=Option Three");
     $this->click("xpath=(//input[@name='q2'])[1]");
     $this->click("id=next");
     $this->waitForPageToLoad("30000");
-    $this->assertTextPresent('MCQ 5, horizontal, alphabetic, 1 mark, Option B correct');
-    $this->assertTextPresent('MCQ 6, DDL, alphabetic, 2 marks, Option X correct');
-    $this->assertCssCount('css=input[type="radio"]', 3);
-    $this->assertCssCount('css=select', 2); // Include page jump DDL
 
     $this->click("xpath=(//input[@name='q1'])[3]");
     $this->select("name=q2", "label=Option M");
     $this->click("id=next");
     $this->waitForPageToLoad("30000");
-    $this->assertTextPresent('MCQ 7, vertical, random, 1 mark, -1 mark incorrect, Option One correct');
-    $this->assertTextPresent('MCQ 8, horizontal, random, 1 mark, -2 marks incorrect, Option Two correct');
-    $this->assertTextPresent('MCQ 9, DDL, random, 2 marks, -1 mark incorrect, Option Three correct');
-    $this->assertCssCount('css=input[type="radio"]', 6);
-    $this->assertCssCount('css=select', 2); // Include page jump DDL
 
 
     $this->click("xpath=(//input[@name='q1' and @value='2'])");
