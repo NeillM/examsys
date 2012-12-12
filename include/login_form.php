816355f169c6c33eee966e9dedfb3c4989417b0d
Simon Atack <Simon.Atack@notitngham.ac.uk>
2012-12-12 09:50:45 +0000

    Merged revision(s) 3740 from touchstone/trunk: New log in form. ........
    git-svn-id: svn://suivarro/touchstone/branches/loignauth2@3747 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- include/login_form.php
+++ include/login_form.php
@@ -1,27 +1,37 @@
-<?php
-/**
- * Created by JetBrains PhpStorm.
- * User: cczsa1
- * Date: 11/12/12
- * Time: 11:34
- * To change this template use File | Settings | File Templates.
- */
-  /**
-   *
-   * Form to display login form
-   *
-   * @author Simon Atack
-   * @version 1.0
-   * @copyright Copyright (c) 2012 The University of Nottingham
-   * @package
-   */
+<html>
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-echo <<<END
-<div>
-<form method="POST">
-<p>Username:<input type="text" size="20" name="ROGO_USER"><br /></p>
-<p>Password:<input type="password" size="20" name="ROGO_PW"><br /></p>
-<p><input type="submit" name="rogo-login-form-std" value="Login"><br /></p>
-</form>
+<title>Log In</title>
+
+<link rel="stylesheet" type="text/css" href="../css/body.css" />
+<link rel="stylesheet" type="text/css" href="../css/login_form.css" />
+</head>
+
+<body>
+<form method="post">
+<div class="mainbox">
+
+  <img src="../artwork/r_logo.gif" width="56" height="60" alt="logo" border="0" style="float:left; padding-right:8px" />
+  <div style="color:#1F497D;font-size:28pt; font-weight:bold">Rogo</div>
+  <div style="color:#1F497D;font-size:9pt">e-Assessment Management System</div>
+
+  <br />
+  <br />
+  
+  <div class="msg">The page you are trying to access requires authentication. Please sign in using your username and password:</div>
+    
+  <div style="margin-left:65px">
+  <table>
+  <tr><td>Username</td><td><input type="text" name="username" /></td></tr>
+  <tr><td>Password</td><td><input type="password" name="password" /></td></tr>
+  </table>
+  <br />
+  <input type="submit" name="submit" value=" Sign In " style="width:160px" />&nbsp;&nbsp;<input type="submit" name="submit" value=" Cancel " />
+  </div>
+
 </div>
-END;
+</form>
+
+</body>
+</html>
