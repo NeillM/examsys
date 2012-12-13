24416e4b7c01a40ae243889d0bfb1b8d816f9a4f
Rob Ingram <Rob.Ingram@notitngham.ac.uk>
2012-12-13 09:50:10 +0000

    Add test for class totals on MCQ paper
    git-svn-id: svn://suivarro/touchstone/trunk@3772 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

---
+++ testing/selenium/tests/002-single-q-type-papers/001-mcq/002-ClassTotalsMCQTest.php
@@ -0,0 +1,33 @@
+<?php
+require_once 'shared.inc.php';
+
+class ManageFacultyTest extends PHPUnit_Extensions_SeleniumTestCase
+{
+  protected $install_type = ' \(local\)';
+  protected $page_root = 'https://rogo.local';
+
+  protected function setUp()
+  {
+    $this->setBrowser("*firefox");
+    $this->setBrowserUrl($this->page_root . '/');
+  }
+
+  public function testResults()
+  {
+    do_staff_login($this);
+
+    $this->open("/reports/class_totals.php?paperID=1&startdate=20120111000000&enddate=20121213100000&repmodule=&repcourse=%&sortby=name&module=3&folder=&percent=100&absent=0&studentsonly=1&direction=asc");
+    $this->assertElementContainsText('//tr[@id="res1"]/td[5]', '12');
+    $this->assertElementContainsText('//tr[@id="res1"]/td[6]', '100%');
+    $this->assertElementContainsText('//tr[@id="res1"]/td[7]', 'Distinction');
+
+    $this->assertElementContainsText('//tr[@id="res2"]/td[5]', '-4');
+    $this->assertElementContainsText('//tr[@id="res2"]/td[6]', '-33%');
+    $this->assertElementContainsText('//tr[@id="res2"]/td[7]', 'Fail');
+
+    $this->assertElementContainsText('//tr[@id="res3"]/td[5]', '5');
+    $this->assertElementContainsText('//tr[@id="res3"]/td[6]', '42%');
+    $this->assertElementContainsText('//tr[@id="res3"]/td[7]', 'Pass');
+  }
+}
+?>
