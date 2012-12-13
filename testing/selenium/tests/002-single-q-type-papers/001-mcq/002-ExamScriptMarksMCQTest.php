bc3116a0f6e392046e33affce90e516d0075f7c2
Rob Ingram <Rob.Ingram@notitngham.ac.uk>
2012-12-13 16:01:09 +0000

    Add test for marks in exam scripts
    git-svn-id: svn://suivarro/touchstone/trunk@3786 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

---
+++ testing/selenium/tests/002-single-q-type-papers/001-mcq/002-ExamScriptMarksMCQTest.php
@@ -0,0 +1,96 @@
+<?php
+require_once 'shared.inc.php';
+
+class ExamScrtiptMarksMCQTest extends PHPUnit_Extensions_SeleniumTestCase
+{
+  protected $install_type = ' \(local\)';
+  protected $page_root = 'https://rogo.local';
+
+  protected function setUp()
+  {
+    $this->setBrowser("*firefox");
+    $this->setBrowserUrl($this->page_root . '/');
+  }
+
+  public function testAllCorrect()
+  {
+    do_staff_login($this);
+
+    $this->open("/reports/class_totals.php?paperID=1&startdate=20120111000000&enddate=20121213150000&repmodule=&repcourse=%&sortby=name&module=3&folder=&percent=100&absent=0&studentsonly=1&direction=asc");
+    $this->click("//span[@onclick=\"popMenu('2012-12-12 12:12:42',104,'0','n','n','100',event);hideTimerReset();\"]");
+    $this->click("id=item1b");
+    $this->waitForPopUp("paper", "30000");
+    $this->selectWindow("name=paper");
+
+    // Individual Question Marks
+    $this->assertElementContainsText('//table[2]/tbody/tr/td[2]/p/span', '1 out of 1');
+    $this->assertElementContainsText('//table[2]/tbody/tr[3]/td[2]/p/span', '1 out of 1');
+    $this->assertElementContainsText('//table[3]/tbody/tr/td[2]/p/span', '2 out of 2');
+    $this->assertElementContainsText('//table[3]/tbody/tr[3]/td[2]/p/span', '1 out of 1');
+    $this->assertElementContainsText('//table[4]/tbody/tr/td[2]/p/span', '1 out of 1');
+    $this->assertElementContainsText('//table[4]/tbody/tr[3]/td[2]/p/span', '2 out of 2');
+    $this->assertElementContainsText('//table[5]/tbody/tr/td[2]/p/span', '1 out of 1');
+    $this->assertElementContainsText('//table[5]/tbody/tr[3]/td[2]/p/span', '1 out of 1');
+    $this->assertElementContainsText('//table[5]/tbody/tr[5]/td[2]/p/span', '2 out of 2');
+
+    // Overall Marks
+    $this->assertElementContainsText('//div[5]/table/tbody/tr/td/table/tbody/tr[2]/td[2]', '12 out of 12');
+    $this->assertElementContainsText('//div[5]/table/tbody/tr/td/table/tbody/tr[3]/td[2]', '40%');
+    $this->assertElementContainsText('//div[5]/table/tbody/tr/td/table/tbody/tr[4]/td[2]', '100%');
+  }
+
+  public function testAllIncorrect()
+  {
+    do_staff_login($this);
+
+    $this->open("/reports/class_totals.php?paperID=1&startdate=20120111000000&enddate=20121213150000&repmodule=&repcourse=%&sortby=name&module=3&folder=&percent=100&absent=0&studentsonly=1&direction=asc");
+    $this->click("//span[@onclick=\"popMenu('2012-12-12 12:12:50',105,'0','n','n','-33',event);hideTimerReset();\"]");
+    $this->click("id=item1b");
+    $this->waitForPopUp("paper", "30000");
+    $this->selectWindow("name=paper");
+
+    // Individual Question Marks
+    $this->assertElementContainsText('//table[2]/tbody/tr/td[2]/p/span', '0 out of 1');
+    $this->assertElementContainsText('//table[2]/tbody/tr[3]/td[2]/p/span', '0 out of 1');
+    $this->assertElementContainsText('//table[3]/tbody/tr/td[2]/p/span', '0 out of 2');
+    $this->assertElementContainsText('//table[3]/tbody/tr[3]/td[2]/p/span', '0 out of 1');
+    $this->assertElementContainsText('//table[4]/tbody/tr/td[2]/p/span', '0 out of 1');
+    $this->assertElementContainsText('//table[4]/tbody/tr[3]/td[2]/p/span', '0 out of 2');
+    $this->assertElementContainsText('//table[5]/tbody/tr/td[2]/p/span', '-1 out of 1');
+    $this->assertElementContainsText('//table[5]/tbody/tr[3]/td[2]/p/span', '-2 out of 1');
+    $this->assertElementContainsText('//table[5]/tbody/tr[5]/td[2]/p/span', '-1 out of 2');
+
+    // Overall Marks
+    $this->assertElementContainsText('//div[5]/table/tbody/tr/td/table/tbody/tr[2]/td[2]', '-4 out of 12');
+    $this->assertElementContainsText('//div[5]/table/tbody/tr/td/table/tbody/tr[3]/td[2]', '40%');
+    $this->assertElementContainsText('//div[5]/table/tbody/tr/td/table/tbody/tr[4]/td[2]', '-33%');
+  }
+
+  public function testMixed()
+  {
+    do_staff_login($this);
+
+    $this->open("/reports/class_totals.php?paperID=1&startdate=20120111000000&enddate=20121213150000&repmodule=&repcourse=%&sortby=name&module=3&folder=&percent=100&absent=0&studentsonly=1&direction=asc");
+    $this->click("//span[@onclick=\"popMenu('2012-12-12 12:12:57',106,'0','n','n','42',event);hideTimerReset();\"]");
+    $this->click("id=item1b");
+    $this->waitForPopUp("paper", "30000");
+    $this->selectWindow("name=paper");
+
+    // Individual Question Marks
+    $this->assertElementContainsText('//table[2]/tbody/tr/td[2]/p/span', '1 out of 1');
+    $this->assertElementContainsText('//table[2]/tbody/tr[3]/td[2]/p/span', '0 out of 1');
+    $this->assertElementContainsText('//table[3]/tbody/tr/td[2]/p/span', '0 out of 2');
+    $this->assertElementContainsText('//table[3]/tbody/tr[3]/td[2]/p/span', '1 out of 1');
+    $this->assertElementContainsText('//table[4]/tbody/tr/td[2]/p/span', '1 out of 1');
+    $this->assertElementContainsText('//table[4]/tbody/tr[3]/td[2]/p/span', '0 out of 2');
+    $this->assertElementContainsText('//table[5]/tbody/tr/td[2]/p/span', '-1 out of 1');
+    $this->assertElementContainsText('//table[5]/tbody/tr[3]/td[2]/p/span', '1 out of 1');
+    $this->assertElementContainsText('//table[5]/tbody/tr[5]/td[2]/p/span', '2 out of 2');
+
+    // Overall Marks
+    $this->assertElementContainsText('//div[5]/table/tbody/tr/td/table/tbody/tr[2]/td[2]', '5 out of 12');
+    $this->assertElementContainsText('//div[5]/table/tbody/tr/td/table/tbody/tr[3]/td[2]', '40%');
+    $this->assertElementContainsText('//div[5]/table/tbody/tr/td/table/tbody/tr[4]/td[2]', '42%');
+  }
+}
+?>
