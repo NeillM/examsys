fff15f0fbfc2adc08faa39e3cfd50d45ea9806d1
Rob Ingram <Rob.Ingram@notitngham.ac.uk>
2013-01-09 15:31:31 +0000

    Update to take into account user answers with abstentions
    git-svn-id: svn://suivarro/touchstone/trunk@3991 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- testing/selenium/tests/002-single-q-type-papers/004-dichotomous/002-ExamScriptMarksDichotomousTest.php
+++ testing/selenium/tests/002-single-q-type-papers/004-dichotomous/002-ExamScriptMarksDichotomousTest.php
@@ -274,5 +274,70 @@ class ExamScrtiptMarksDichotomousTest extends PHPUnit_Extensions_SeleniumTestCas
     $this->assertElementContainsText('//div[25]/table/tbody/tr/td/table/tbody/tr[3]/td[2]', '40%');
     $this->assertElementContainsText('//div[25]/table/tbody/tr/td/table/tbody/tr[4]/td[2]', '30%');
   }
+
+  public function testAbstentions() {
+    do_staff_login($this);
+
+    $this->open("/reports/class_totals.php?paperID=4&startdate=20130101000000&enddate=20130209160000&repmodule=&repcourse=%&sortby=name&module=3&folder=&percent=100&absent=0&studentsonly=1&direction=asc");
+    $this->click("//span[@onclick=\"popMenu('2013-01-08 17:01:11',108,'0','n','n','4',event);hideTimerReset();\"]");
+    $this->click("id=item1b");
+    $this->waitForPopUp("paper", "30000");
+    $this->selectWindow("name=paper");
+
+    // Individual Question Marks
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td[2]/p[2]/span', '0 out of 4');
+    $this->assertElementContainsText('//table[2]/tbody/tr[4]/td[2]/p/span', '0 out of 8');
+    $this->assertElementContainsText('//table[3]/tbody/tr/td[2]/p/span', '0 out of 5');
+    $this->assertElementContainsText('//table[3]/tbody/tr[4]/td[2]/p/span', '0 out of 1');
+    $this->assertElementContainsText('//table[4]/tbody/tr/td[2]/p/span', '0 out of 2');
+    $this->assertElementContainsText('//table[4]/tbody/tr[3]/td[2]/p/span', '0 out of 1');
+    $this->assertElementContainsText('//table[5]/tbody/tr[2]/td[2]/p/span', '0 out of 4');
+    $this->assertElementContainsText('//table[5]/tbody/tr[4]/td[2]/p/span', '0 out of 8');
+    $this->assertElementContainsText('//table[6]/tbody/tr/td[2]/p/span', '0 out of 5');
+    $this->assertElementContainsText('//table[6]/tbody/tr[4]/td[2]/p/span', '0 out of 1');
+    $this->assertElementContainsText('//table[7]/tbody/tr/td[2]/p/span', '0 out of 2');
+    $this->assertElementContainsText('//table[7]/tbody/tr[3]/td[2]/p/span', '0 out of 1');
+    $this->assertElementContainsText('//table[8]/tbody/tr[2]/td[2]/p[2]/span', '0 out of 4');
+    $this->assertElementContainsText('//table[8]/tbody/tr[4]/td[2]/p/span', '0 out of 8');
+    $this->assertElementContainsText('//table[9]/tbody/tr/td[2]/p/span', '0 out of 5');
+    $this->assertElementContainsText('//table[9]/tbody/tr[4]/td[2]/p/span', '0 out of 1');
+    $this->assertElementContainsText('//table[10]/tbody/tr/td[2]/p/span', '0 out of 2');
+    $this->assertElementContainsText('//table[10]/tbody/tr[3]/td[2]/p/span', '0 out of 1');
+    $this->assertElementContainsText('//table[11]/tbody/tr[2]/td[2]/p/span', '0 out of 4');
+    $this->assertElementContainsText('//table[11]/tbody/tr[4]/td[2]/p/span', '0 out of 8');
+    $this->assertElementContainsText('//table[12]/tbody/tr/td[2]/p/span', '0 out of 5');
+    $this->assertElementContainsText('//table[12]/tbody/tr[4]/td[2]/p/span', '0 out of 1');
+    $this->assertElementContainsText('//table[13]/tbody/tr/td[2]/p/span', '0 out of 2');
+    $this->assertElementContainsText('//table[13]/tbody/tr[3]/td[2]/p/span', '0 out of 1');
+    $this->assertElementContainsText('//table[14]/tbody/tr[2]/td[2]/p[2]/span', '2 out of 4');
+    $this->assertElementContainsText('//table[14]/tbody/tr[4]/td[2]/p/span', '4 out of 8');
+    $this->assertElementContainsText('//table[15]/tbody/tr/td[2]/p/span', '0 out of 5');
+    $this->assertElementContainsText('//table[15]/tbody/tr[4]/td[2]/p/span', '0 out of 1');
+    $this->assertElementContainsText('//table[16]/tbody/tr/td[2]/p/span', '0 out of 2');
+    $this->assertElementContainsText('//table[16]/tbody/tr[3]/td[2]/p/span', '0 out of 1');
+    $this->assertElementContainsText('//table[17]/tbody/tr[2]/td[2]/p/span', '0.5 out of 4');
+    $this->assertElementContainsText('//table[17]/tbody/tr[4]/td[2]/p/span', '3 out of 8');
+    $this->assertElementContainsText('//table[18]/tbody/tr/td[2]/p/span', '0 out of 5');
+    $this->assertElementContainsText('//table[18]/tbody/tr[4]/td[2]/p/span', '-0.5 out of 1');
+    $this->assertElementContainsText('//table[19]/tbody/tr/td[2]/p/span', '-1 out of 2');
+    $this->assertElementContainsText('//table[19]/tbody/tr[3]/td[2]/p/span', '-1 out of 1');
+    $this->assertElementContainsText('//table[20]/tbody/tr[2]/td[2]/p[2]/span', '0 out of 4');
+    $this->assertElementContainsText('//table[20]/tbody/tr[4]/td[2]/p/span', '0 out of 8');
+    $this->assertElementContainsText('//table[21]/tbody/tr/td[2]/p/span', '0 out of 5');
+    $this->assertElementContainsText('//table[21]/tbody/tr[4]/td[2]/p/span', '0 out of 1');
+    $this->assertElementContainsText('//table[22]/tbody/tr/td[2]/p/span', '0 out of 2');
+    $this->assertElementContainsText('//table[22]/tbody/tr[3]/td[2]/p/span', '0 out of 1');
+    $this->assertElementContainsText('//table[23]/tbody/tr[2]/td[2]/p/span', '0 out of 4');
+    $this->assertElementContainsText('//table[23]/tbody/tr[4]/td[2]/p/span', '0 out of 8');
+    $this->assertElementContainsText('//table[24]/tbody/tr/td[2]/p/span', '0 out of 5');
+    $this->assertElementContainsText('//table[24]/tbody/tr[4]/td[2]/p/span', '0 out of 1');
+    $this->assertElementContainsText('//table[25]/tbody/tr/td[2]/p/span', '0 out of 2');
+    $this->assertElementContainsText('//table[25]/tbody/tr[3]/td[2]/p/span', '0 out of 1');
+
+    // Overall Marks
+    $this->assertElementContainsText('//div[25]/table/tbody/tr/td/table/tbody/tr[2]/td[2]', '7 out of 168');
+    $this->assertElementContainsText('//div[25]/table/tbody/tr/td/table/tbody/tr[3]/td[2]', '40%');
+    $this->assertElementContainsText('//div[25]/table/tbody/tr/td/table/tbody/tr[4]/td[2]', '4%');
+  }
 }
 ?>
