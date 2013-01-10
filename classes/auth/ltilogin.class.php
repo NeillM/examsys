fdc449ace3c2b509fd8f43d35979dfca87810c92
Simon Atack <Simon.Atack@notitngham.ac.uk>
2013-01-10 16:51:14 +0000

    Removed incorrect header and replace with standard rogo header
    git-svn-id: svn://suivarro/touchstone/trunk@4022 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- classes/auth/ltilogin.class.php
+++ classes/auth/ltilogin.class.php
@@ -1,11 +1,21 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: cczsa1
- * Date: 20/12/12
- * Time: 14:51
- * To change this template use File | Settings | File Templates.
- */
+// This file is part of Rogō
+//
+// Rogō is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Rogō is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Rogō.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 /**
  *
  * The lti login authentication function.
