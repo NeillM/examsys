52200ab1c03ff35ff6dbb4fda47b0ce7d21ee540
Rob Ingram <Rob.Ingram@notitngham.ac.uk>
2013-01-10 14:20:21 +0000

    Add test for unanswered, correct and incorrect for FITB paper
    git-svn-id: svn://suivarro/touchstone/trunk@4012 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

---
+++ testing/selenium/tests/002-single-q-type-papers/005-blank/003-RunAsStudentBlankTest.php
@@ -0,0 +1,232 @@
+<?php
+require_once 'shared.inc.php';
+
+class RunAsStudentBlankTest extends PHPUnit_Extensions_SeleniumTestCase
+{
+  protected $install_type;
+  protected $page_root;
+
+  protected function setUp() {
+    $this->install_type = get_install_type();
+    $this->page_root = get_root_url();
+
+    $this->setBrowser("*firefox");
+    $this->setBrowserUrl($this->page_root . '/');
+  }
+
+  // public function testQuestionPresenceAndOrderPlusUnanswered() {
+  //   do_student_login($this, 'teststudent10', 'jgl!34Z^');
+
+  //   $this->open("/user_index.php?id=51357812182102");
+  //   $this->click("id=start");
+  //   $this->waitForPopUp("paper", "30000");
+  //   $this->selectWindow("name=paper");
+  //   $this->assertTextPresent('Fill in the Blank DDL Questions');
+  //   $this->assertTextPresent('Note: Blank 1 notes for students');
+  //   $this->assertTextPresent('Blank 1, DDL, 1 mark, 2 blanks');
+  //   $this->assertTextPresent('Fill in the Blank, DDL, Negative Marking');
+  //   $this->assertTextPresent('Blank 2, DDL, 2 marks correct, -1 mark incorrect, 3 blanks');
+  //   $this->assertTextPresent('Fill in the Blank, DDL, Mark per Question');
+  //   $this->assertTextPresent('Blank 3, DDL, mark per question, 2 marks, 2 blanks');
+  //   $this->assertCssCount('css=select', 8); // Include page jump DDL
+
+  //   $this->selectWindow("name=paper");
+  //   $this->click("id=next");
+  //   $this->waitForPageToLoad("30000");
+  //   $this->assertTextPresent('Fill in the Blank, DDL, Mark per Question, Negative Marking');
+  //   $this->assertTextPresent('Blank 4, DDL, mark per question, 3 marks correct, -1 mark incorrect, 3 blanks');
+  //   $this->assertTextPresent('Fill in the Blank Textbox Questions');
+  //   $this->assertTextPresent('Note: Blank 5 notes for students');
+  //   $this->assertTextPresent('Blank 5, textbox, 1 mark, 2 blanks');
+  //   $this->assertTextPresent('Fill in the Blank, Textbox, Negative Marking');
+  //   $this->assertTextPresent('Blank 6, textbox, 2 marks correct, -1 mark incorrect, 3 blanks');
+  //   $this->assertCssCount('css=select', 4); // Include page jump DDL
+  //   $this->assertCssCount('css=input[type="text"]', 6);    // Include timer box
+
+  //   $this->click("id=next");
+  //   $this->waitForPageToLoad("30000");
+  //   $this->assertTextPresent('Fill in the Blank, Textbox, Mark per Question');
+  //   $this->assertTextPresent('Blank 7, textbox, mark per question, 2 marks, 2 blanks');
+  //   $this->assertTextPresent('Fill in the Blank, Textbox, Mark per Question, Negative Marking');
+  //   $this->assertTextPresent('Blank 8, textbox, 3 marks correct, -1 mark incorrect, 3 blanks');
+  //   $this->assertCssCount('css=input[type="text"]', 6);    // Include timer box
+
+  //   $this->click("id=finish");
+  //   $this->assertEquals("Are you sure you wish to finish. After clicking 'OK' you will not be able to go back.", $this->getConfirmation());
+  //   $this->waitForPageToLoad("30000");
+  //   $this->assertLocation($this->page_root . '/paper/finish.php?id=51357812182102');
+
+  //   // Individual Question Marks
+  //   $this->assertElementContainsText('//table[2]/tbody/tr[2]/td[2]/p[4]/span', '0 out of 2');
+  //   $this->assertElementContainsText('//table[2]/tbody/tr[5]/td[2]/p[3]/span', '0 out of 6');
+  //   $this->assertElementContainsText('//table[2]/tbody/tr[8]/td[2]/p[3]/span', '0 out of 2');
+  //   $this->assertElementContainsText('//table[3]/tbody/tr[2]/td[2]/p[3]/span', '0 out of 3');
+  //   $this->assertElementContainsText('//table[3]/tbody/tr[5]/td[2]/p[4]/span', '0 out of 2');
+  //   $this->assertElementContainsText('//table[3]/tbody/tr[8]/td[2]/p[3]/span', '0 out of 6');
+  //   $this->assertElementContainsText('//table[4]/tbody/tr[2]/td[2]/p[3]/span', '0 out of 2');
+  //   $this->assertElementContainsText('//table[4]/tbody/tr[5]/td[2]/p[3]/span', '0 out of 3');
+
+  //   // Overall Marks
+  //   $this->assertElementContainsText('//div[4]/table/tbody/tr/td/table/tbody/tr[2]/td[2]', '0 out of 26');
+  //   $this->assertElementContainsText('//div[4]/table/tbody/tr/td/table/tbody/tr[3]/td[2]', '40%');
+  //   $this->assertElementContainsText('//div[4]/table/tbody/tr/td/table/tbody/tr[4]/td[2]', '0.0%');
+  // }
+
+  // public function testCompletePaperCorrect() {
+  //   do_student_login($this, 'teststudent11', 'bkt_66Y4');
+
+  //   $this->open("/user_index.php?id=51357812182102");
+  //   $this->click("id=start");
+  //   $this->waitForPopUp("paper", "30000");
+  //   $this->selectWindow("name=paper");
+
+  //   $this->select("name=q1_1", "label=colour");
+  //   $this->select("name=q1_2", "label=country");
+  //   $this->select("name=q2_1", "label=colour");
+  //   $this->select("name=q2_2", "label=country");
+  //   $this->select("name=q2_3", "label=Visual Display Unit");
+  //   $this->select("name=q3_1", "label=colour");
+  //   $this->select("name=q3_2", "label=country");
+  //   $this->click("id=next");
+
+  //   $this->waitForPageToLoad("30000");
+  //   $this->select("name=q1_1", "label=colour");
+  //   $this->select("name=q1_2", "label=country");
+  //   $this->select("name=q1_3", "label=Visual Display Unit");
+  //   $this->type("name=q2_1", "colour");
+  //   $this->type("name=q2_2", "country");
+  //   $this->type("name=q3_1", "colour");
+  //   $this->type("name=q3_2", "country");
+  //   $this->type("name=q3_3", "Visual Display Unit");
+  //   $this->click("id=next");
+
+  //   $this->waitForPageToLoad("30000");
+  //   $this->type("name=q1_1", "colour");
+  //   $this->type("name=q1_2", "country");
+  //   $this->type("name=q2_1", "colour");
+  //   $this->type("name=q2_2", "country");
+  //   $this->type("name=q2_3", "Visual Display Unit");
+  //   $this->click("id=finish");
+  //   $this->assertEquals("Are you sure you wish to finish. After clicking 'OK' you will not be able to go back.", $this->getConfirmation());
+  //   $this->waitForPageToLoad("30000");
+  //   $this->assertLocation($this->page_root . '/paper/finish.php?id=51357812182102');
+
+  //   // Individual Question Marks
+  //   $this->assertElementContainsText('//table[2]/tbody/tr[2]/td[2]/p[4]/span', '2 out of 2');
+  //   $this->assertElementContainsText('//table[2]/tbody/tr[5]/td[2]/p[3]/span', '6 out of 6');
+  //   $this->assertElementContainsText('//table[2]/tbody/tr[8]/td[2]/p[3]/span', '2 out of 2');
+  //   $this->assertElementContainsText('//table[3]/tbody/tr[2]/td[2]/p[3]/span', '3 out of 3');
+  //   $this->assertElementContainsText('//table[3]/tbody/tr[5]/td[2]/p[4]/span', '2 out of 2');
+  //   $this->assertElementContainsText('//table[3]/tbody/tr[8]/td[2]/p[3]/span', '6 out of 6');
+  //   $this->assertElementContainsText('//table[4]/tbody/tr[2]/td[2]/p[3]/span', '2 out of 2');
+  //   $this->assertElementContainsText('//table[4]/tbody/tr[5]/td[2]/p[3]/span', '3 out of 3');
+
+  //   // Overall Marks
+  //   $this->assertElementContainsText('//div[4]/table/tbody/tr/td/table/tbody/tr[2]/td[2]', '26 out of 26');
+  //   $this->assertElementContainsText('//div[4]/table/tbody/tr/td/table/tbody/tr[3]/td[2]', '40%');
+  //   $this->assertElementContainsText('//div[4]/table/tbody/tr/td/table/tbody/tr[4]/td[2]', '100.0%');
+  // }
+
+  public function testCompletePaperIncorrect() {
+    do_student_login($this, 'teststudent12', 'rmu_74L4');
+
+    $this->open("/user_index.php?id=51357812182102");
+    $this->click("id=start");
+    $this->waitForPopUp("paper", "30000");
+    $this->selectWindow("name=paper");
+    $this->select("name=q1_1", "label=texture");
+    $this->select("name=q1_2", "label=continent");
+    $this->select("name=q2_1", "label=texture");
+    $this->select("name=q2_2", "label=continent");
+    $this->select("name=q2_3", "label=Video Display Unit");
+    $this->select("name=q3_1", "label=country");
+    $this->select("name=q3_2", "label=city");
+    $this->click("id=next");
+
+    $this->waitForPageToLoad("30000");
+    $this->select("name=q1_1", "label=texture");
+    $this->select("name=q1_2", "label=continent");
+    $this->select("name=q1_3", "label=Video Display Unit");
+    $this->type("name=q2_1", "country");
+    $this->type("name=q2_2", "city");
+    $this->type("name=q3_1", "texture");
+    $this->type("name=q3_2", "continent");
+    $this->type("name=q3_3", "Video Display Unit");
+    $this->click("id=next");
+
+    $this->waitForPageToLoad("30000");
+    $this->type("name=q1_1", "country");
+    $this->type("name=q1_2", "continent");
+    $this->type("name=q2_1", "texture");
+    $this->type("name=q2_2", "colour");
+    $this->type("name=q2_3", "Vibrant Display Unit");
+    $this->click("id=finish");
+    $this->assertEquals("Are you sure you wish to finish. After clicking 'OK' you will not be able to go back.", $this->getConfirmation());
+    $this->waitForPageToLoad("30000");
+    $this->assertLocation($this->page_root . '/paper/finish.php?id=51357812182102');
+
+    // Individual Question Marks
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td[2]/p[4]/span', '0 out of 2');
+    $this->assertElementContainsText('//table[2]/tbody/tr[5]/td[2]/p[3]/span', '-3 out of 6');
+    $this->assertElementContainsText('//table[2]/tbody/tr[8]/td[2]/p[3]/span', '0 out of 2');
+    $this->assertElementContainsText('//table[3]/tbody/tr[2]/td[2]/p[3]/span', '-1 out of 3');
+    $this->assertElementContainsText('//table[3]/tbody/tr[5]/td[2]/p[4]/span', '0 out of 2');
+    $this->assertElementContainsText('//table[3]/tbody/tr[8]/td[2]/p[3]/span', '-3 out of 6');
+    $this->assertElementContainsText('//table[4]/tbody/tr[2]/td[2]/p[3]/span', '0 out of 2');
+    $this->assertElementContainsText('//table[4]/tbody/tr[5]/td[2]/p[3]/span', '-1 out of 3');
+
+    // Overall Marks
+    $this->assertElementContainsText('//div[4]/table/tbody/tr/td/table/tbody/tr[2]/td[2]', '-8 out of 26');
+    $this->assertElementContainsText('//div[4]/table/tbody/tr/td/table/tbody/tr[3]/td[2]', '40%');
+    $this->assertElementContainsText('//div[4]/table/tbody/tr/td/table/tbody/tr[4]/td[2]', '-30.8%');
+  }
+
+  // public function testCompletePaperMixed() {
+  //   do_student_login($this, 'teststudent13', 'hii.420R');
+
+  //   $this->open("/user_index.php?id=11355244387102");
+  //   $this->click("id=start");
+  //   $this->waitForPopUp("paper", "30000");
+  //   $this->selectWindow("name=paper");
+
+  //   $this->click("name=q1");
+  //   $this->click("xpath=(//input[@name='q2'])[1]");
+  //   $this->click("id=next");
+  //   $this->waitForPageToLoad("30000");
+
+  //   $this->select("name=q1", "label=Option Three");
+  //   $this->click("xpath=(//input[@name='q2'])[2]");
+  //   $this->click("id=next");
+  //   $this->waitForPageToLoad("30000");
+
+  //   $this->click("name=q1");
+  //   $this->select("name=q2", "label=Option M");
+  //   $this->click("id=next");
+  //   $this->waitForPageToLoad("30000");
+
+  //   $this->click("xpath=(//input[@name='q1' and @value='2'])");
+  //   $this->click("xpath=(//input[@name='q2' and @value='2'])");
+  //   $this->select("name=q3", "label=Option Three");
+  //   $this->click("id=finish");
+  //   $this->assertEquals("Are you sure you wish to finish. After clicking 'OK' you will not be able to go back.", $this->getConfirmation());
+  //   $this->waitForPageToLoad("30000");
+  //   $this->assertLocation($this->page_root . '/paper/finish.php?id=11355244387102');
+
+  //   // Individual Question Marks
+  //   $this->assertElementContainsText('//table[2]/tbody/tr/td[2]/p/span', '1 out of 1');
+  //   $this->assertElementContainsText('//table[2]/tbody/tr[4]/td[2]/p/span', '0 out of 1');
+  //   $this->assertElementContainsText('//table[3]/tbody/tr/td[2]/p/span', '0 out of 2');
+  //   $this->assertElementContainsText('//table[3]/tbody/tr[3]/td[2]/p/span', '1 out of 1');
+  //   $this->assertElementContainsText('//table[4]/tbody/tr/td[2]/p/span', '1 out of 1');
+  //   $this->assertElementContainsText('//table[4]/tbody/tr[3]/td[2]/p/span', '0 out of 2');
+  //   $this->assertElementContainsText('//table[5]/tbody/tr/td[2]/p/span', '-1 out of 1');
+  //   $this->assertElementContainsText('//table[5]/tbody/tr[3]/td[2]/p/span', '1 out of 1');
+  //   $this->assertElementContainsText('//table[5]/tbody/tr[5]/td[2]/p/span', '2 out of 2');
+
+  //   // Overall Marks
+  //   $this->assertElementContainsText('//div[5]/table/tbody/tr/td/table/tbody/tr[2]/td[2]', '5 out of 12');
+  //   $this->assertElementContainsText('//div[5]/table/tbody/tr/td/table/tbody/tr[3]/td[2]', '40%');
+  //   $this->assertElementContainsText('//div[5]/table/tbody/tr/td/table/tbody/tr[4]/td[2]', '41.7%');
+  // }
+}
+?>
