8f1a556e49b0688e5c5e9af490ebd34285200bf9
Nikodem Miranowicz <Nikodem.Miranowicz@notitngham.ac.uk>
2013-01-11 12:32:16 +0000

    lang translations
    git-svn-id: svn://suivarro/touchstone/trunk@4027 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- lang/pl/folder/edit_multi_teams_popup.php
+++ lang/pl/folder/edit_multi_teams_popup.php
@@ -14,6 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Rogo.  If not, see <http://www.gnu.org/licenses/>.
 
-$string['manageteams'] = 'Manage Teams';  // Niko
+$string['manageteams'] = 'Zarządzaj zespołami'; 
 $string['teams'] = 'Zespoły:';
 ?>
--- lang/pl/students/index.php
+++ lang/pl/students/index.php
@@ -16,7 +16,7 @@
 
 require_once '../lang/' . $language . '/folder/details.php';
 
-$string['staffmsg'] = 'This is the Rog&#333; student landing page. Were you looking for the <a href="../staff/">staff management screens</a>?'; // Niko
+$string['staffmsg'] = 'To jest strona systemu Rog&#333; przeznaczona dla studentów. Czy poszukiwałeś/łaś<a href="../staff/">stron zarządzania dla kadry</a>?'; // Niko
 $string['nopapers'] = 'Obecnie brak dostępnych arkuszy.';
 $string['to'] = 'do';
 $string['objectivesbased'] = 'Odzew dot. celów<br />dla oceny w';
