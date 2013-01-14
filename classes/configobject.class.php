d85fa0b28ecbf70a0b18ce71f32a0e8d72ea0e73
Anthony Brown <Anthony.Brown@nottingham.ac.uk>
2013-01-14 17:59:50 +0000

    Fixed Muppet coding error!!!
    git-svn-id: svn://suivarro/touchstone/trunk@4070 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- classes/configobject.class.php
+++ classes/configobject.class.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Rogō.  If not, see <http://www.gnu.org/licenses/>.
 
-include __DIR__ . '/../classes/rogostaticsingleton.class.php';
+require_once __DIR__ . '/../classes/rogostaticsingleton.class.php';
 
 /**
  *
