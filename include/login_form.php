e7290730f9488027ab93b1dc12797c427dc99d2b
Simon Atack <Simon.Atack@notitngham.ac.uk>
2013-01-15 10:57:51 +0000

    Fix notice for missing variable check on login form
    git-svn-id: svn://suivarro/touchstone/trunk@4079 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- include/login_form.php
+++ include/login_form.php
@@ -1,97 +1,105 @@
 <html xmlns="http://www.w3.org/1999/html">
 <head>
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 
-<title>Log In</title>
+    <title>Log In</title>
 
-<link rel="stylesheet" type="text/css" href="../css/body.css" />
-<link rel="stylesheet" type="text/css" href="../css/login_form.css" />
+    <link rel="stylesheet" type="text/css" href="../css/body.css"/>
+    <link rel="stylesheet" type="text/css" href="../css/login_form.css"/>
 </head>
 
 <body>
 <form method="post">
-<div class="mainbox">
+    <div class="mainbox">
 
-  <img src="../artwork/r_logo.gif" width="56" height="60" alt="logo" border="0" style="float:left; padding-right:8px" />
-  <div style="color:#1F497D;font-size:28pt; font-weight:bold">Rogo</div>
-  <div style="color:#1F497D;font-size:9pt">e-Assessment Management System</div>
+        <img src="../artwork/r_logo.gif" width="56" height="60" alt="logo" border="0"
+             style="float:left; padding-right:8px"/>
 
-  <br />
-  <br />
-  <?php
-  if(isset($displaystdformobj->messages)) {
-    foreach($displaystdformobj->messages as $object) {
-      echo <<<HTML
+        <div style="color:#1F497D;font-size:28pt; font-weight:bold">Rogo</div>
+        <div style="color:#1F497D;font-size:9pt">e-Assessment Management System</div>
+
+        <br/>
+        <br/>
+      <?php
+      if (isset($displaystdformobj->messages)) {
+        foreach ($displaystdformobj->messages as $object) {
+          echo <<<HTML
 $object->pretext
 <div class="msg">$object->content</div>
 $object->posttext
 HTML;
-    }
-  }
+        }
+      }
 
-  if(!isset($displaystdformobj) or $displaystdformobj->replace!==true) {
+      if (!isset($displaystdformobj) or (isset($displaystdformobj->replace) and $displaystdformobj->replace !== true)) {
 
-    echo <<<HTML
+        echo <<<HTML
   <div class="msg">The page you are trying to access requires authentication. Please sign in using your username and password:</div>
 HTML;
 
-  }
-?>
-  <div style="margin-left:65px">
-  <table>
-  <tr><td>Username</td><td><input type="text" name="ROGO_USER" /></td></tr>
-  <tr><td>Password</td><td><input type="password" name="ROGO_PW" /></td></tr>
-  </table>
-  <br />
-  <input type="submit" name="rogo-login-form-std" value=" Sign In " style="width:160px" />
-<?php
-    if(isset($displaystdformobj->buttons)) {
-      foreach($displaystdformobj->buttons as $object) {
-        echo <<<HTML
+      }
+      ?>
+        <div style="margin-left:65px">
+            <table>
+                <tr>
+                    <td>Username</td>
+                    <td><input type="text" name="ROGO_USER"/></td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td><input type="password" name="ROGO_PW"/></td>
+                </tr>
+            </table>
+            <br/>
+            <input type="submit" name="rogo-login-form-std" value=" Sign In " style="width:160px"/>
+          <?php
+          if (isset($displaystdformobj->buttons)) {
+            foreach ($displaystdformobj->buttons as $object) {
+              echo <<<HTML
 $object->pretext
 <input type="$object->type" name="$object->name" value="$object->value" style="$object->style" />
 $object->posttext
 HTML;
-      }
-    }
-    //<input type="submit" name="cancel" value=" Cancel " />
-?>
-  </div>
+            }
+          }
+          //<input type="submit" name="cancel" value=" Cancel " />
+          ?>
+        </div>
 
-  <?php
-  if(isset($displaystdformobj->postbuttonmessages)) {
-    foreach($displaystdformobj->postbuttonmessages as $object) {
-      $cssclass='msg';
-      if(isset($object->cssclass)) {
-        $cssclass=$object->cssclass;
-      }
-      echo <<<HTML
+      <?php
+      if (isset($displaystdformobj->postbuttonmessages)) {
+        foreach ($displaystdformobj->postbuttonmessages as $object) {
+          $cssclass = 'msg';
+          if (isset($object->cssclass)) {
+            $cssclass = $object->cssclass;
+          }
+          echo <<<HTML
 $object->pretext
 <div class="$cssclass">$object->content</div>
 $object->posttext
 HTML;
-    }
-  }
-  ?>
+        }
+      }
+      ?>
 
-</div>
+    </div>
 </form>
 
 <?php
-if(isset($displaystdformobj->postformmessages)) {
+if (isset($displaystdformobj->postformmessages)) {
 
-  $cssareaclass='mainbox';
-  if(isset($displaystdformobj->postformmessages[0]->cssareaclass)) {
-    $cssclass=$object->cssclass;
+  $cssareaclass = 'mainbox';
+  if (isset($displaystdformobj->postformmessages[0]->cssareaclass)) {
+    $cssclass = $object->cssclass;
   }
-  if(!isset($displaystdformobj->postformmessages[0]->rawhtml)) {
+  if (!isset($displaystdformobj->postformmessages[0]->rawhtml)) {
     echo <<<HTML
 <div class="$cssmainclass">
 HTML;
-    foreach($displaystdformobj->postformmessages as $object) {
-      $cssclass='msg';
-      if(isset($object->cssclass)) {
-        $cssclass=$object->cssclass;
+    foreach ($displaystdformobj->postformmessages as $object) {
+      $cssclass = 'msg';
+      if (isset($object->cssclass)) {
+        $cssclass = $object->cssclass;
       }
       echo <<<HTML
 $object->pretext
