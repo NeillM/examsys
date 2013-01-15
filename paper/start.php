106daff6a5602756b4287b00a3f6cce0c479a778
Nikodem Miranowicz <Nikodem.Miranowicz@notitngham.ac.uk>
2013-01-15 15:10:17 +0000

    revert
    git-svn-id: svn://suivarro/touchstone/trunk@4092 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- paper/start.php
+++ paper/start.php
@@ -429,8 +429,7 @@ if (isset($_POST['sessionid'])) {
 }
 
 require '../config/start.inc';
-//niko echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"\n\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n<html>\n<head>\n";
-echo "<!DOCTYPE html>\n<html>\n<head>\n";
+echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"\n\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n<html>\n<head>\n";
 if ($paper_type == '3') {
   echo "<title>" . $string['survey'] . "</title>\n";
 } else {
@@ -490,7 +489,6 @@ if ($css != '') {
   <script type="text/javascript" src="../tools/mee/mee/js/mee_src.js"></script>
 <?php }?>
 <script type="text/javascript" src="../js/flash_include.js"></script>
-<script type="text/javascript" src="../js/q_labelling.js"></script>
 <script type="text/javascript" src="../js/jquery.flash_q.js"></script>
 <script language="javascript">
   window.history.go(1);
