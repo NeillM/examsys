f251d21ea6e52cef2d7174c84d7dc5950e84ff84
Rob Ingram <Rob.Ingram@notitngham.ac.uk>
2013-01-18 10:25:21 +0000

    Update tests to include partially answered questions
    git-svn-id: svn://suivarro/touchstone/trunk@4119 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- testing/selenium/tests/002-single-q-type-papers/002-mrq/003-RunAsStudentMRQTest.php
+++ testing/selenium/tests/002-single-q-type-papers/002-mrq/003-RunAsStudentMRQTest.php
@@ -334,27 +334,91 @@ class RunAsStudentMRQTest extends PHPUnit_Extensions_SeleniumTestCase
     $this->assertElementContainsText('//div[7]/table/tbody/tr/td/table/tbody/tr[4]/td[2]', '13.0%');
   }
 
+  public function testCompletePaperPartialAnswers() {
+    do_student_login($this, 'teststudent14', 'mon~61Qt');
+
+    $this->open("/user_index.php?id=21355414508102");
+    $this->click("id=start");
+    $this->waitForPopUp("paper", "30000");
+    $this->selectWindow("name=paper");
+
+    $this->click("id=q1_1");
+    $this->click("id=q2_1");
+    $this->click("id=next");
+    $this->waitForPageToLoad("30000");
+
+    $this->click("id=q1_2");
+    $this->click("id=q2_2");
+    $this->click("id=next");
+    $this->waitForPageToLoad("30000");
+
+    $this->click("id=q1_1");
+    $this->click("id=q2_4");
+    $this->click("id=next");
+    $this->waitForPageToLoad("30000");
+
+    $this->click("id=q1_2");
+    $this->click("id=q2_3");
+    $this->click("id=next");
+    $this->waitForPageToLoad("30000");
+
+    $this->click("id=q1_1");
+    $this->click("id=q2_3");
+    $this->click("id=next");
+    $this->waitForPageToLoad("30000");
+
+    $this->click("id=q1_4");
+    $this->click("id=q2_2");
+    $this->click("id=finish");
+    $this->assertEquals("Are you sure you wish to finish. After clicking 'OK' you will not be able to go back.", $this->getConfirmation());
+    $this->waitForPageToLoad("30000");
+    $this->assertLocation($this->page_root . '/paper/finish.php?id=21355414508102');
+
+    // Individual Question Marks
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td[2]/p[2]/span', '1 out of 2');
+    $this->assertElementContainsText('//table[2]/tbody/tr[4]/td[2]/p/span', '0 out of 2');
+    $this->assertElementContainsText('//table[3]/tbody/tr/td[2]/p/span', '0 out of 4');
+    $this->assertElementContainsText('//table[3]/tbody/tr[3]/td[2]/p/span', '0 out of 2');
+    $this->assertElementContainsText('//table[4]/tbody/tr/td[2]/p/span', '0 out of 1');
+    $this->assertElementContainsText('//table[4]/tbody/tr[3]/td[2]/p/span', '0 out of 2');
+    $this->assertElementContainsText('//table[5]/tbody/tr[2]/td[2]/p[2]/span', '-2 out of 2');
+    $this->assertElementContainsText('//table[5]/tbody/tr[4]/td[2]/p/span', '1 out of 4');
+    $this->assertElementContainsText('//table[6]/tbody/tr/td[2]/p/span', '0.5 out of 2');
+    $this->assertElementContainsText('//table[6]/tbody/tr[3]/td[2]/p/span', '-1 out of 2');
+    $this->assertElementContainsText('//table[7]/tbody/tr/td[2]/p/span', '-1 out of 1');
+    $this->assertElementContainsText('//table[7]/tbody/tr[3]/td[2]/p/span', '-2 out of 3');
+
+    // Overall Marks
+    $this->assertElementContainsText('//div[7]/table/tbody/tr/td/table/tbody/tr[2]/td[2]', '-3.5 out of 27');
+    $this->assertElementContainsText('//div[7]/table/tbody/tr/td/table/tbody/tr[3]/td[2]', '40%');
+    $this->assertElementContainsText('//div[7]/table/tbody/tr/td/table/tbody/tr[4]/td[2]', '-13.0%');
+  }
+
   public function testClassTotals() {
     do_staff_login($this);
 
     $this->open("/reports/class_totals.php?paperID=2&startdate=20120113000000&enddate=20230117150000&repmodule=&repcourse=%&sortby=name&module=3&folder=&percent=100&absent=0&studentsonly=1&direction=asc");
 
     // Individuals
-    $this->assertElementContainsText('//tr[@id="res5"]/td[5]', '-9');
-    $this->assertElementContainsText('//tr[@id="res5"]/td[6]', '-33%');
-    $this->assertElementContainsText('//tr[@id="res5"]/td[7]', 'Fail');
+    $this->assertElementContainsText('//tr[@id="res6"]/td[5]', '-9');
+    $this->assertElementContainsText('//tr[@id="res6"]/td[6]', '-33%');
+    $this->assertElementContainsText('//tr[@id="res6"]/td[7]', 'Fail');
 
-    $this->assertElementContainsText('//tr[@id="res6"]/td[5]', '27');
-    $this->assertElementContainsText('//tr[@id="res6"]/td[6]', '100%');
-    $this->assertElementContainsText('//tr[@id="res6"]/td[7]', 'Distinction');
+    $this->assertElementContainsText('//tr[@id="res7"]/td[5]', '27');
+    $this->assertElementContainsText('//tr[@id="res7"]/td[6]', '100%');
+    $this->assertElementContainsText('//tr[@id="res7"]/td[7]', 'Distinction');
 
-    $this->assertElementContainsText('//tr[@id="res7"]/td[5]', '-9');
-    $this->assertElementContainsText('//tr[@id="res7"]/td[6]', '-33%');
-    $this->assertElementContainsText('//tr[@id="res7"]/td[7]', 'Fail');
-
-    $this->assertElementContainsText('//tr[@id="res8"]/td[5]', '3.5');
-    $this->assertElementContainsText('//tr[@id="res8"]/td[6]', '13%');
+    $this->assertElementContainsText('//tr[@id="res8"]/td[5]', '-9');
+    $this->assertElementContainsText('//tr[@id="res8"]/td[6]', '-33%');
     $this->assertElementContainsText('//tr[@id="res8"]/td[7]', 'Fail');
+
+    $this->assertElementContainsText('//tr[@id="res9"]/td[5]', '3.5');
+    $this->assertElementContainsText('//tr[@id="res9"]/td[6]', '13%');
+    $this->assertElementContainsText('//tr[@id="res9"]/td[7]', 'Fail');
+
+    $this->assertElementContainsText('//tr[@id="res10"]/td[5]', '-3.5');
+    $this->assertElementContainsText('//tr[@id="res10"]/td[6]', '-13%');
+    $this->assertElementContainsText('//tr[@id="res10"]/td[7]', 'Fail');
   }
 }
 ?>
