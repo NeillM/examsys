69bc3023a0842abb1d6385a547b678d6feab1355
Simon Atack <Simon.Atack@notitngham.ac.uk>
2013-02-12 17:08:46 +0000

    updating users/ldaplookup.php but can only test on varro
    git-svn-id: svn://suivarro/touchstone/trunk@4346 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- plugins/lookup/UoNSaturnTranslation.class.php
+++ plugins/lookup/UoNSaturnTranslation.class.php
@@ -61,12 +61,12 @@ class UoNSaturnTranslation_lookup extends outline_lookup {
 
   function usertranslate($datapart) {
 
-    if (isset($this->orsearchlist($datapart->role) and $this->orsearchlist($datapart->role, array('Undergraduate', 'Postgraduate', 'UG', 'PGT', 'PG'))) {
+    if (isset($datapart->role) and $this->orsearchlist($datapart->role, array('Undergraduate', 'Postgraduate', 'UG', 'PGT', 'PG'))) {
       $this->savetodebug('Detected Student, correcting role');
       $datapart->role = 'Student';
     }
 
-    if (isset($this->orsearchlist($datapart->role) and $this->orsearchlist($datapart->role, array('S')) or isset($datapart->staffID)) {
+    if ((isset($datapart->role) and $this->orsearchlist($datapart->role, array('S'))) or isset($datapart->staffID)) {
       $this->savetodebug('Detected staff, correcting role and filling in fields');
       $datapart->role = 'Staff';
       $datapart->coursecode = 'University Lecturer';
