3f3f77e733f0fc49fc2b2f6ea6e466e6ed4743f6
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2013-02-13 16:43:51 +0000

    Standardise paper icon functions.
    git-svn-id: svn://suivarro/touchstone/trunk@4364 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- paper/search.php
+++ paper/search.php
@@ -24,103 +24,69 @@
 * @package
 */
 
-  require '../include/staff_auth.inc';
-  require_once '../classes/moduleutils.class.php';
+require '../include/staff_auth.inc';
+require_once '../classes/moduleutils.class.php';
+require_once '../classes/paperutils.class.php';
 
-  function displayIcon($paper_type, $title, $initials, $surname, $shared, $locked, $retired) {
-    global $string;
+if (isset($_POST['formative']) and isset($_POST['progress']) and isset($_POST['summative']) and isset($_POST['survey']) and isset($_POST['osce']) and isset($_POST['offline'])) {
+  // All types are selected so don't build into query.
+  $type = '';
+} else {
+  $type = '';
+  if (isset($_POST['formative']) and $_POST['formative'] == '1') $type .= " OR paper_type='0'";
+  if (isset($_POST['progress']) and $_POST['progress'] == '1') $type .= " OR paper_type='1'";
+  if (isset($_POST['summative']) and $_POST['summative'] == '1') $type .= " OR paper_type='2'";
+  if (isset($_POST['survey']) and $_POST['survey'] == '1') $type .= " OR paper_type='3'";
+  if (isset($_POST['osce']) and $_POST['osce'] == '1') $type .= " OR paper_type='4'";
+  if (isset($_POST['offline']) and $_POST['offline'] == '1') $type .= " OR paper_type='5'";
+  if (isset($_POST['peerreview']) and $_POST['peerreview'] == '1') $type .= " OR paper_type='6'";
+  if (strlen($type) > 0) $type = 'AND (' . substr($type,4) . ')';
+}
 
-    if (strlen($retired) == 0) {
-      $retired = '';
-    } else {
-      $retired = '_retired';
-    }
-
-    switch ($paper_type) {
-      case 0:
-        $html = "<img src=\"../artwork/formative" . $retired . ".png\" width=\"48\" height=\"48\" alt=\"" . $string['type'] . ": " . $string['formative'] ."&#013;" . $string['author'] . ": $title $initials $surname\" />";
-        break;
-      case 1:
-        $html = "<img src=\"../artwork/progress" . $retired . ".png\" width=\"48\" height=\"48\" alt=\"" . $string['type'] . ": " . $string['progresstest'] . "&#013;" . $string['author'] . ": $title $initials $surname\" />";
-        break;
-      case 2:
-        $html = "<img src=\"../artwork/summative" . $retired . $locked . ".png\" width=\"48\" height=\"48\" alt=\"" . $string['type'] . ": " . $string['summative'] . "&#013;" . $string['author'] . ": $title $initials $surname\" />";
-        break;
-      case 3:
-        $html = "<img src=\"../artwork/survey" . $retired . ".png\" width=\"48\" height=\"48\" alt=\"" . $string['type'] . ": " . $string['survey'] . "&#013;" . $string['author'] . ": $title $initials $surname\" />";
-        break;
-      case 4:
-        $html = "<img src=\"../artwork/osce" . $retired . ".png\" width=\"48\" height=\"48\" alt=\"" . $string['type'] . ": " . $string['oscestation'] . "&#013;" . $string['author'] . ": $title $initials $surname\" />";
-        break;
-      case 5:
-        $html = "<img src=\"../artwork/offline" . $retired . ".png\" width=\"48\" height=\"48\" alt=\"" . $string['type'] . ": " . $string['offlinepaper'] . "&#013;" . $string['author'] . ": $title $initials $surname\" />";
-        break;
-      case 6:
-        $html = "<img src=\"../artwork/peer_review" . $retired . ".png\" width=\"48\" height=\"48\" alt=\"" . $string['type'] . ": " . $string['peerreview'] . "&#013;" . $string['author'] . ": $title $initials $surname\" />";
-        break;
-    }
-    return $html;
+$params = '';
+$variables = array();
+if (isset($_POST['searchterm']) and $_POST['searchterm'] != '') {
+  $paper = 'AND paper_title LIKE ?';
+  $variables[] = '%' . $_POST['searchterm'] . '%';
+  $params .= 's';
+} else {
+  $paper = '';
+}
+if (isset($_POST['owner']) and $_POST['owner'] != '') {
+  $owner = 'AND paper_ownerID=?';
+  $variables[] = $_POST['owner'];
+  $params .= 'i';
+  setcookie("papersearch[2]", $_POST['owner'], time()+60*60*24*365);
+} else {
+  $owner = '';
+  setcookie("papersearch[2]", '', time()+60*60*24*365);
+}
+if (isset($_POST['lab']) and $_POST['lab'] != '') {
+  $lab = 'AND labs LIKE ?';
+  $variables[] = '%' . $_POST['lab'] . '%';
+  $params .= 's';
+} else {
+  $lab = '';
+}
+$moduleid = '';
+if (isset($_POST['module']) and $_POST['module'] != '') {
+  $moduleid = 'AND idMod = ?';
+  $variables[] = $_POST['module'];
+  $params .= 'i';
+} else {
+  if (!$userObject->has_role('SysAdmin')) {
+    $moduleid = "AND idMod IN ('" . implode("','",array_keys($staff_modules)) . "')";
   }
-
-  if (isset($_POST['formative']) and isset($_POST['progress']) and isset($_POST['summative']) and isset($_POST['survey']) and isset($_POST['osce']) and isset($_POST['offline'])) {
-    // All types are selected so don't build into query.
-    $type = '';
-  } else {
-    $type = '';
-    if (isset($_POST['formative']) and $_POST['formative'] == '1') $type .= " OR paper_type='0'";
-    if (isset($_POST['progress']) and $_POST['progress'] == '1') $type .= " OR paper_type='1'";
-    if (isset($_POST['summative']) and $_POST['summative'] == '1') $type .= " OR paper_type='2'";
-    if (isset($_POST['survey']) and $_POST['survey'] == '1') $type .= " OR paper_type='3'";
-    if (isset($_POST['osce']) and $_POST['osce'] == '1') $type .= " OR paper_type='4'";
-    if (isset($_POST['offline']) and $_POST['offline'] == '1') $type .= " OR paper_type='5'";
-    if (isset($_POST['peerreview']) and $_POST['peerreview'] == '1') $type .= " OR paper_type='6'";
-    if (strlen($type) > 0) $type = 'AND (' . substr($type,4) . ')';
-  }
-
-  $params = '';
-  $variables = array();
-  if (isset($_POST['searchterm']) and $_POST['searchterm'] != '') {
-    $paper = 'AND paper_title LIKE ?';
-    $variables[] = '%' . $_POST['searchterm'] . '%';
-    $params .= 's';
-  } else {
-    $paper = '';
-  }
-  if (isset($_POST['owner']) and $_POST['owner'] != '') {
-    $owner = 'AND paper_ownerID=?';
-    $variables[] = $_POST['owner'];
-    $params .= 'i';
-    setcookie("papersearch[2]", $_POST['owner'], time()+60*60*24*365);
-  } else {
-    $owner = '';
-    setcookie("papersearch[2]", '', time()+60*60*24*365);
-  }
-  if (isset($_POST['lab']) and $_POST['lab'] != '') {
-    $lab = 'AND labs LIKE ?';
-    $variables[] = '%' . $_POST['lab'] . '%';
-    $params .= 's';
-  } else {
-    $lab = '';
-  }
-  $moduleid = '';
-  if (isset($_POST['module']) and $_POST['module'] != '') {
-    $moduleid = 'AND idMod = ?';
-    $variables[] = $_POST['module'];
-    $params .= 'i';
-  } else {
-    if (!$userObject->has_role('SysAdmin')) {
-      $moduleid = "AND idMod IN ('" . implode("','",array_keys($staff_modules)) . "')";
-    }
-  }
-  if (isset($_POST['day']) and $_POST['day'] != '') {
-    $date = 'AND start_date <= ? AND end_date >= ?';
-    $variables[] = $_POST['year'] . $_POST['month'] . $_POST['day'] . '000000';
-    $variables[] = $_POST['year'] . $_POST['month'] . $_POST['day'] . '235959';
-    $params .= 'ss';
-  } else {
-    $date = '';
-  }
-  ?>
+}
+if (isset($_POST['day']) and $_POST['day'] != '') {
+  $date = 'AND start_date <= ? AND end_date >= ?';
+  $variables[] = $_POST['year'] . $_POST['month'] . $_POST['day'] . '000000';
+  $variables[] = $_POST['year'] . $_POST['month'] . $_POST['day'] . '235959';
+  $params .= 'ss';
+} else {
+  $date = '';
+}
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <head>
@@ -190,13 +156,13 @@
         if ($paper_ownerID == $userObject->get_user_ID() or $userObject->has_role(array('SysAdmin','Admin'))or $type != 2) {
           $codes = module_utils::get_idMod(explode(',', $moduleID), $mysqli);
           $html = implode(',', $codes);
-          echo "<a href=\"../paper/details.php?paperID=$property_id&module=$html\">" . displayIcon($type, $title, $initials, $surname, '', $locked, $retired) . "</a></td>\n";
+          echo "<a href=\"../paper/details.php?paperID=$property_id&module=$html\">" . Paper_utils::displayIcon($type, $title, $initials, $surname, $locked, $retired) . "</a></td>\n";
           echo "</td><td><a href=\"../paper/details.php?paperID=$property_id&module=$html\">$paper_title</a><br />";
         } else {
           if ($userObject->is_staff_user_on_module($moduleID)) {
             $codes = module_utils::get_idMod(explode(',', $moduleID), $mysqli);
             $html = implode(',', $codes);
-            echo "<a href=\"../paper/details.php?paperID=$property_id&module=$html\">" . displayIcon(2, $title, $initials, $surname, '', $locked, $retired) . "</a></td>\n";
+            echo "<a href=\"../paper/details.php?paperID=$property_id&module=$html\">" . Paper_utils::displayIcon(2, $title, $initials, $surname, $locked, $retired) . "</a></td>\n";
             echo "</td><td><a href=\"../paper/details.php?paperID=$property_id&module=$html\">$paper_title</a><br />";
           } else {
             echo "<img src=\"../artwork/noentry_question_icon_48.png\" width=\"48\" height=\"48\" alt=\"Type: Summative Exam (Restricted Access)&#013;Author: $title $initials $surname\" border=\"0\" /></td>\n";
