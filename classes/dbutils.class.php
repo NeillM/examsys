7312b1f71e9391c4dab94fafbf426c17390de4a0
Simon Atack <Simon.Atack@notitngham.ac.uk>
2013-02-18 11:57:23 +0000

    added support to change the port for the db connection
    git-svn-id: svn://suivarro/touchstone/trunk@4376 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- classes/dbutils.class.php
+++ classes/dbutils.class.php
@@ -39,6 +39,8 @@ Class DBUtils {
    * @return object
    */
   static function get_mysqli_link($host, $user, $passwd, $database, $charset, $notice, $dbclass = 'mysqli', $port = 3306) {
+    var_dump(get_defined_vars());
+
     @$mysqli = new $dbclass($host, $user, $passwd, $database, $port);
     if ($mysqli->connect_error == '') {
       $mysqli->set_charset($charset);
