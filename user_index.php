4d1daa3aa00be4b691a103be544e1f38d33d63b7
Rob Ingram <Rob.Ingram@notitngham.ac.uk>
2013-02-20 15:43:28 +0000

    Remaining time can be FALSE if not set
    git-svn-id: svn://suivarro/touchstone/trunk@4388 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- user_index.php
+++ user_index.php
@@ -185,19 +185,22 @@ if ($exam_duration !== null) {
     $extra_time_mins    = $extra_time_secs / 60;
     $summative_timer    = new SummativeTimer( $log_extra_time );
     $remaining_time     = $summative_timer->calculate_remaining_time_secs();
-    if ($remaining_time > ($exam_duration_sec + $extra_time_secs) ) {
-      // sanity check if we have longer remaining then the exam duration set the time remaining
-      // to the exam duration (happens in summative exams if we have not started yet)
-      $remaining_time = $exam_duration_sec + $extra_time_secs;
-    }
-    if ($exam_started == false and $remaining_time == 0) {
-      // sanity check if we have not started the exam but time remaing is 0 
-      // happens in summative exams if we have the start and end time set wider
-      // then the paper duration e.g in multiple sittings
-      $remaining_time = $exam_duration_sec + $extra_time_secs;
+    if ($remaining_time === false) {
       $display_remaining_time = false;
+    } else {
+      if ($remaining_time > ($exam_duration_sec + $extra_time_secs) ) {
+        // sanity check if we have longer remaining then the exam duration set the time remaining
+        // to the exam duration (happens in summative exams if we have not started yet)
+        $remaining_time = $exam_duration_sec + $extra_time_secs;
+      }
+      if ($exam_started == false and $remaining_time == 0) {
+        // sanity check if we have not started the exam but time remaing is 0
+        // happens in summative exams if we have the start and end time set wider
+        // then the paper duration e.g in multiple sittings
+        $remaining_time = $exam_duration_sec + $extra_time_secs;
+        $display_remaining_time = false;
+      }
     }
-
   } else {
     $studentID         = $userObject->get_user_ID();
     $timer             = new Timer($log_metadata, $exam_duration);
