412743982290a0a6bc15623ac6b87a43b101442d
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2013-02-21 16:17:25 +0000

    Inclusion of additional indexes and fixes to class_totals.inc.
    git-svn-id: svn://suivarro/touchstone/trunk@4402 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- classes/moduleutils.class.php
+++ classes/moduleutils.class.php
@@ -172,9 +172,9 @@ Class module {
       $result = $db->prepare("SELECT id FROM modules WHERE moduleid = ?");
       if ($db->error) {
         try {
-          throw new Exception("MySQL error $db->error <br> Query:<br> $query", $db->errno);
+          throw new Exception("MySQL error $db->error <br> Query:<br /> $query", $db->errno);
         } catch (Exception $e) {
-          echo "Error No: " . $e->getCode() . " - " . $e->getMessage() . "<br >";
+          echo "Error No: " . $e->getCode() . " - " . $e->getMessage() . "<br />";
           echo nl2br($e->getTraceAsString());
         }
       }
