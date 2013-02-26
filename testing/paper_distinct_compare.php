4d976aad4e57b6f1f33c1a8d81e6a323f4005627
Simon Atack <Simon.Atack@notitngham.ac.uk>
2013-02-26 17:37:55 +0000

    fixing testing/paper_distinct_compare.php
    git-svn-id: svn://suivarro/touchstone/trunk@4446 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- testing/paper_distinct_compare.php
+++ testing/paper_distinct_compare.php
@@ -25,11 +25,16 @@
 
 require '../classes/configobject.class.php';
 require '../classes/dbutils.class.php';
+@apache_setenv('no-gzip', 1);
+@ini_set('zlib.output_compression', 0);
+@ini_set('implicit_flush', 1);
+
+@ob_implicit_flush(1);
 $configObject = Config::get_instance();
 $notice = null;
 $mysqli = DBUtils::get_mysqli_link($configObject->get('cfg_db_host'), $configObject->get('cfg_db_staff_user'), $configObject->get('cfg_db_staff_passwd'), $configObject->get('cfg_db_database'), $configObject->get('cfg_db_charset'), $notice, $configObject->get('dbclass'), $configObject->get('cfg_db_port'));
 
-$sql = "select property_id,date_format(start_date,'%Y%m%d%H%i%S') as start_date, date_format(end_date,'%Y%m%d%H%i%S') as end_date, paper_title from properties where paper_type='2'";
+$sql = "select property_id,date_format(start_date,'%Y%m%d%H%i%S') as start_date, date_format(end_date,'%Y%m%d%H%i%S') as end_date, paper_title from properties where paper_type='2' limit 3";
 
 $result = $mysqli->prepare($sql);
 if ($mysqli->error) {
@@ -47,46 +52,8 @@ $result->bind_result($propertyid, $start_date, $end_date, $papertitle);
 
 $records = $result->num_rows;
 
-$roles_sql = " AND (users.roles='Student' OR users.roles='graduate')";
-while ($result->fetch()) {
 
-  //	  $log_query = $mysqli->prepare("SELECT DISTINCT log2.q_id, 2 AS paper_type, grade, roles, screen, duration, started, user_answer, DATE_FORMAT(started, '{$configObject->get('cfg_long_date_time')}') AS display_started, year, title, surname, initials, first_names, gender, ipaddress, lab_name, username, users.id, student_id, user_answer, q_type, log_metadata.userID, mark, status, attempt FROM (log2, log_metadata, questions, users ) LEFT JOIN sid ON users.id = sid.userID WHERE log_metadata.userID = users.id AND log2.metadataID = log_metadata.id AND log2.q_id = questions.q_id AND paperID = ? $roles_sql AND DATE_ADD(started, INTERVAL 2 MINUTE) >= ? AND started <= ? ORDER BY userID, started, screen");
-  $log_query = $mysqli->prepare("SELECT DISTINCT count(log2.q_id) as count FROM (log2, log_metadata, questions, users ) LEFT JOIN sid ON users.id = sid.userID WHERE log_metadata.userID = users.id AND log2.metadataID = log_metadata.id AND log2.q_id = questions.q_id AND paperID = ? $roles_sql AND DATE_ADD(started, INTERVAL 2 MINUTE) >= ? AND started <= ?");
-  if ($mysqli->error) {
-    try {
-      throw new Exception("MySQL error $mysqli->error <br> Query:<br> ", $mysqli->errno);
-    } catch (Exception $e) {
-      echo "Error No: " . $e->getCode() . " - " . $e->getMessage() . "<br />";
-      echo nl2br($e->getTraceAsString());
-      exit();
-    }
-  }
-  $log_query->bind_param('iss', $propertyid, $start_date, $end_date);
-  $log_query->execute();
-  $log_query->bind_result($count);
-  $log_query->fetch();
-  $log_query->close();
-  $distinctCNT = $count;
-  $log_query = $mysqli->prepare("SELECT count(log2.q_id) as count FROM (log2, log_metadata, questions, users ) LEFT JOIN sid ON users.id = sid.userID WHERE log_metadata.userID = users.id AND log2.metadataID = log_metadata.id AND log2.q_id = questions.q_id AND paperID = ? $roles_sql AND DATE_ADD(started, INTERVAL 2 MINUTE) >= ? AND started <= ?");
-  if ($mysqli->error) {
-    try {
-      throw new Exception("MySQL error $mysqli->error <br> Query:<br> ", $mysqli->errno);
-    } catch (Exception $e) {
-      echo "Error No: " . $e->getCode() . " - " . $e->getMessage() . "<br />";
-      echo nl2br($e->getTraceAsString());
-      exit();
-    }
-  }
-  $log_query->bind_param('iss', $propertyid, $start_date, $end_date);
-  $log_query->execute();
-  $log_query->bind_result($count);
-  $log_query->fetch();
-  $log_query->close();
-  $NOTdistinctCNT = $count;
 
-  $data[$propertyid] = array($papertitle, $distinctCNT, $NOTdistinctCNT,$start_date, $end_date);
-
-}
 
 echo <<<HTML
 	<html>
@@ -96,25 +63,70 @@ echo <<<HTML
 <table>
 <tr><th>PaperID</th><th>Paper Name</th><th>Count</th><th>Distinct Count</th><th>Error</th><th></th><th></th></tr>
 HTML;
-if (isset($data)) {
-  foreach ($data as $key => $value) {
-    $same = true;
-    if ($value[1] != $value[2]) {
-      $same = false;
 
+
+$roles_sql = " AND (users.roles='Student' OR users.roles='graduate')";
+while ($result->fetch()) {
+
+  //	  $log_query = $mysqli->prepare("SELECT DISTINCT log2.q_id, 2 AS paper_type, grade, roles, screen, duration, started, user_answer, DATE_FORMAT(started, '{$configObject->get('cfg_long_date_time')}') AS display_started, year, title, surname, initials, first_names, gender, ipaddress, lab_name, username, users.id, student_id, user_answer, q_type, log_metadata.userID, mark, status, attempt FROM (log2, log_metadata, questions, users ) LEFT JOIN sid ON users.id = sid.userID WHERE log_metadata.userID = users.id AND log2.metadataID = log_metadata.id AND log2.q_id = questions.q_id AND paperID = ? $roles_sql AND DATE_ADD(started, INTERVAL 2 MINUTE) >= ? AND started <= ? ORDER BY userID, started, screen");
+  $log_query = $mysqli->prepare("SELECT DISTINCT log2.q_id, 2 AS paper_type, grade, roles, screen, duration, started, user_answer, DATE_FORMAT(started, '{$configObject->get('cfg_long_date_time')}') AS display_started, year, title, surname, initials, first_names, gender, ipaddress, lab_name, username, users.id, student_id, user_answer, q_type, log_metadata.userID, mark, status, attempt FROM (log2, log_metadata, questions, users ) LEFT JOIN sid ON users.id = sid.userID WHERE log_metadata.userID = users.id AND log2.metadataID = log_metadata.id AND log2.q_id = questions.q_id AND paperID = ? $roles_sql AND DATE_ADD(started, INTERVAL 2 MINUTE) >= ? AND started <= ?");
+  if ($mysqli->error) {
+    try {
+      throw new Exception("MySQL error $mysqli->error <br> Query:<br> ", $mysqli->errno);
+    } catch (Exception $e) {
+      echo "Error No: " . $e->getCode() . " - " . $e->getMessage() . "<br />";
+      echo nl2br($e->getTraceAsString());
+      exit();
     }
-    if ($same == false) {
-      $extra = ' style="background-color:red" ';
-      $error = 'ERROR';
-    } else {
-      $extra = ' style="background-color:green" ';
-      $error = '';
+  }
+  $log_query->bind_param('iss', $propertyid, $start_date, $end_date);
+  $log_query->execute();
+  $log_query->bind_result($q_id, $paper_type, $grade, $tmp_roles, $screen, $duration, $started, $user_answer, $display_started, $year, $title, $surname, $initials, $first_names, $gender, $ipaddress, $lab_name, $username, $tmp_userID, $student_id, $user_answer, $q_type, $tmp_userID, $mark, $status, $attempt);
+
+
+  $log_query->store_result();
+  $distinctCNT = $log_query->num_rows;
+  $log_query->close();
+  $log_query = $mysqli->prepare("SELECT log2.q_id, 2 AS paper_type, grade, roles, screen, duration, started, user_answer, DATE_FORMAT(started, '{$configObject->get('cfg_long_date_time')}') AS display_started, year, title, surname, initials, first_names, gender, ipaddress, lab_name, username, users.id, student_id, user_answer, q_type, log_metadata.userID, mark, status, attempt FROM (log2, log_metadata, questions, users ) LEFT JOIN sid ON users.id = sid.userID WHERE log_metadata.userID = users.id AND log2.metadataID = log_metadata.id AND log2.q_id = questions.q_id AND paperID = ? $roles_sql AND DATE_ADD(started, INTERVAL 2 MINUTE) >= ? AND started <= ?");
+  if ($mysqli->error) {
+    try {
+      throw new Exception("MySQL error $mysqli->error <br> Query:<br> ", $mysqli->errno);
+    } catch (Exception $e) {
+      echo "Error No: " . $e->getCode() . " - " . $e->getMessage() . "<br />";
+      echo nl2br($e->getTraceAsString());
+      exit();
     }
-    echo <<<HTML
+  }
+  $log_query->bind_param('iss', $propertyid, $start_date, $end_date);
+  $log_query->execute();
+  $log_query->bind_result($q_id, $paper_type, $grade, $tmp_roles, $screen, $duration, $started, $user_answer, $display_started, $year, $title, $surname, $initials, $first_names, $gender, $ipaddress, $lab_name, $username, $tmp_userID, $student_id, $user_answer, $q_type, $tmp_userID, $mark, $status, $attempt);
+
+
+  $log_query->store_result();
+  $NOTdistinctCNT = $log_query->num_rows;
+  $log_query->close();
+
+  $value = array($papertitle, $distinctCNT, $NOTdistinctCNT,$start_date, $end_date);
+  $same = true;
+  if ($value[1] != $value[2]) {
+    $same = false;
+
+  }
+  if ($same == false) {
+    $extra = ' style="background-color:red" ';
+    $error = 'ERROR';
+  } else {
+    $extra = ' style="background-color:green" ';
+    $error = '';
+  }
+  echo <<<HTML
 	<tr><td $extra>$key</td><td $extra>$value[0]</td><td>$value[1]</td><td>$value[2]</td><td $extra>$error</td><td>$value[3]</td><td>$value[4]</td></tr>
 HTML;
-  }
+  @flush();
+
 }
+
+
 echo <<<HTML
 		</table></body></html>
 HTML;
