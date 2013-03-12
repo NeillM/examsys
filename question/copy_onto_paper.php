838d9e4bb17f1c7991dc1712f1c62c6aad27f663
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2013-03-12 14:26:02 +0000

    Fix for ticket #1079.
    git-svn-id: svn://suivarro/touchstone/trunk@4643 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- question/copy_onto_paper.php
+++ question/copy_onto_paper.php
@@ -32,7 +32,7 @@ require_once '../classes/logger.class.php';
 
 check_var('q_id', 'GET', true, false, false);
 
-if (!QuestionUtils::question_exists($_GET['q_id'], $mysqli)) {
+if (!QuestionUtils::question_exists(substr($_GET['q_id'],1), $mysqli)) {
   $msg = sprintf($string['furtherassistance'], $configObject->get('support_email'), $configObject->get('support_email'));
   $notice->display_notice_and_exit($string['questionnotfound'], $msg, '../artwork/paper_not_found.png', '#C00000', true, true);
 }
