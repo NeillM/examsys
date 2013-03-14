ec11a437f20e7f9145539ac6adce0d0cc71dcc2a
Rob Ingram <Rob.Ingram@notitngham.ac.uk>
2013-03-14 17:04:03 +0000

    Ticket #1080: Fix some stray characters that were slipping through. Now reject any keypress where shift or alt are pressed
    git-svn-id: svn://suivarro/touchstone/trunk@4692 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- js/start.js
+++ js/start.js
@@ -162,6 +162,10 @@ function write_string(p_string) {
 }
   
 function filterKeypress(event) {
+  // There is no situation where a shifted key is valid
+  if (event.shiftKey === true || event.altKey === true) {
+    event.preventDefault();
+  }
   // Allow: backspace, delete, tab and escape
   if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 189 || event.keyCode == 190 || event.keyCode == 110 || event.keyCode == 109 || event.keyCode == 173 || event.keyCode == 9 || event.keyCode == 27 ||
   // Allow: Ctrl+A
@@ -173,8 +177,7 @@ function filterKeypress(event) {
   }
   else {
     // Ensure that it is a number and stop the keypress
-    if (((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 ))
-            || event.shiftKey === true) {
+    if (((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 ))) {
       event.preventDefault();
     }
   }
