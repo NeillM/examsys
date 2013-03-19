3c569fe71c015266b18ef40eafc9fcc592ca3fb1
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2013-03-19 14:02:42 +0000

    Fix for ticket #1113.
    git-svn-id: svn://suivarro/touchstone/trunk@4739 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- paper/properties.php
+++ paper/properties.php
@@ -335,7 +335,7 @@ if (isset($_POST['Submit'])) {
       $hide_if_unanswered = '0';
     }
 
-    if (($configObject->get('cfg_summative_mgmt') and $paper_type == '2' and $userObject->has_role(array('Admin','SysAdmin'))) or !$configObject->get('cfg_summative_mgmt') or!$configObject->get('cfg_summative_mgmt') or  $paper_type != '2') {
+    if (($configObject->get('cfg_summative_mgmt') and $paper_type == '2' and $userObject->has_role(array('Admin','SysAdmin'))) or !$configObject->get('cfg_summative_mgmt') or !$configObject->get('cfg_summative_mgmt') or  $paper_type != '2') {
   		$local_time = new DateTimeZone($configObject->get('cfg_timezone'));
   		$target_timezone = new DateTimeZone($_POST['timezone']);
 
@@ -382,6 +382,9 @@ if (isset($_POST['Submit'])) {
         }
         $tmp_end_date = $end_date->format("YmdHis");
       }
+    } else {
+      echo "hi";
+      exit;
     }
 
     $leap = is_leap($_POST['ext_tyear']);
