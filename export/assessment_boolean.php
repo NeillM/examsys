443e599b67a08c8e95463a4b4948b359c22897b8
Nikodem Miranowicz <Nikodem.Miranowicz@notitngham.ac.uk>
2013-03-20 11:17:22 +0000

    fix for boolean export of ranking question
    git-svn-id: svn://suivarro/touchstone/trunk@4748 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- export/assessment_boolean.php
+++ export/assessment_boolean.php
@@ -236,8 +236,8 @@ foreach ($user_results as $individual) {
             if ($paper_answers[$a] != '' and substr($tmp_exclude, $a+$sub_parts, 1) == '0') $csv .= ',Q' . $q_no . chr($a+65);
           }
         } elseif (($question['q_type'] == 'dichotomous' or $question['q_type'] == 'blank' or $question['q_type']=='rank') and ($question['score_method'] == 'Mark per Option' or $question['score_method'] == 'Bonus Mark')) {
-          for ($a=0; $a<count($question['correct']); $a++) {
-            if (substr($tmp_exclude, $a, 1) == '0') $csv .= ',Q' . $q_no . chr($a+65);
+          for ($a=0; $a<count($question['correct']); $a++) {            
+            if (substr($tmp_exclude, $a, 1) == '0' && ($question['q_type']!='rank' || $question['correct'][$a]>0)) $csv .= ',Q' . $q_no . chr($a+65);
           }
         } elseif ($question['q_type']=='mrq' and $question['score_method'] == 'Mark per Option') {
           for ($a=0; $a<count($question['correct']); $a++) {
@@ -352,7 +352,7 @@ foreach ($user_results as $individual) {
               
               $answer .= '                                   ';                  
               foreach ($question['correct'] as $qi => $question_part) {
-                if ($question_part != 'n') {
+                if ($question_part != 'n' && ($question['q_type']!='rank' || $question_part>0)) {
                   $csv .= ',' . (($question_part == $answer[$qi]) ? 1:0);
                 }
               }            
