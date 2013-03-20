5133ecb12043b29ec2849b0d65eeb5ad61d5f4e1
Simon Atack <Simon.Atack@notitngham.ac.uk>
2013-03-20 15:11:19 +0000

    Update to LTI landing page to protect against students being launched from vle as staff
    git-svn-id: svn://suivarro/touchstone/trunk@4769 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- LTI/index.php
+++ LTI/index.php
@@ -247,6 +247,7 @@ if (!$lti->isInstructor()) {
         if (!$userObject->is_staff_user_on_module($v[1]) and $lti_i::allow_staff_module_register($v) and $userObject->has_role(array('Staff', 'Admin', 'SysAdmin'))) {
           UserUtils::add_staff_to_module($userObject->get_user_ID(), $v[1], $mysqli);
           print "ADDING TO MODULE $v[1] " . $v[1] ." END<br>";
+          exit();
         } elseif (!$userObject->is_staff_user_on_module($v[1]) and !$lti_i::allow_staff_module_register($v)) {
           UserNotices::display_notice($string['NotAddedToModuleTitle'], $string['NotAddedToModule'] . $v[1], '../artwork/exclamation_64.png','#C00000');
           exit();
