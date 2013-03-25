e9a2343e605b2e8b0811e29426f315eaa43125a3
Simon Atack <Simon.Atack@notitngham.ac.uk>
2013-03-25 15:38:13 +0000

    Add debug to userutils to find issue on test
    git-svn-id: svn://suivarro/touchstone/trunk@4831 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- classes/userutils.class.php
+++ classes/userutils.class.php
@@ -486,6 +486,14 @@ Class UserUtils {
       $result->bind_param('i', $tmp_userID);
     } else {
       $result = $db->prepare("SELECT userID FROM modules_student WHERE userID = ? AND idMod IN ($idMod) AND calendar_year = ?");
+      if ($db->error) {
+        try {
+          throw new Exception("MySQL error $db->error <br /> Query:<br /> $query", $db->errno);
+        } catch (Exception $e) {
+          echo "Error No: " . $e->getCode() . " - " . $e->getMessage() . "<br />";
+          echo nl2br($e->getTraceAsString());
+        }
+      }
       $result->bind_param('is', $tmp_userID, $session);
     }
     
