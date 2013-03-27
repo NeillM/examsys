d1faa004ec2d205a02e5757ba192781fa40ef97c
Nikodem Miranowicz <Nikodem.Miranowicz@notitngham.ac.uk>
2013-03-27 11:40:35 +0000

    lang translations
    git-svn-id: svn://suivarro/touchstone/trunk@4865 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- lang/pl/LTI/index.php
+++ lang/pl/LTI/index.php
@@ -27,5 +27,5 @@ $string['NotAddedToModule'] = 'Dodawanie do zespołu modułu nie jest umożliwio
 $string['SELECT']='Wybierz';
 
 $string['NoModCreateTitle2'] = 'Tworzenie modułu nie nastapiło'; 
-$string['NoModCreate2'] = 'Tworzenie modułu z LTI nie nastąpiło jako że użytkownik nie ma wystarczających przywilejów, stąd też my nie możemy utworzyć modułu z kodem kursu: '; //TODO Niko
+$string['NoModCreate2'] = 'Tworzenie modułu z LTI nie nastąpiło jako że użytkownik nie ma wystarczających przywilejów, stąd też my nie możemy utworzyć modułu z kodem kursu: '; 
 ?>
