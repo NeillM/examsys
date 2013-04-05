39529bae0017015427c675f0338902299b523d8f
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2013-04-05 15:17:18 +0000

    Fixes to two tables that could not create.
    git-svn-id: svn://suivarro/touchstone/trunk@4912 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- classes/installutils.class.php
+++ classes/installutils.class.php
@@ -2203,7 +2203,7 @@ QUERY;
           `property_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
           `idMod` int(11) unsigned NOT NULL DEFAULT '0',
           PRIMARY KEY (`property_id`,`idMod`),
-          KEY `idx_idmod` (`idMod`),
+          KEY `idx_idmod` (`idMod`)
         ) ENGINE=InnoDB DEFAULT CHARSET={$charset}
 QUERY;
 
@@ -2282,7 +2282,7 @@ $this->tableList['questions_modules'] = <<<QUERY
           `q_id` int(4) unsigned NOT NULL DEFAULT '0',
           `idMod` int(11) unsigned NOT NULL DEFAULT '0',
           KEY `idx_idmod` (`idMod`),
-          PRIMARY KEY (`q_id`,`idMod`),
+          PRIMARY KEY (`q_id`,`idMod`)
         ) ENGINE=InnoDB DEFAULT CHARSET={$charset}
 QUERY;
 
