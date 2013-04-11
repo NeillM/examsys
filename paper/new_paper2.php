522cfaff352a818e966915577a3c20345a621d1c
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2013-04-11 14:50:10 +0000

    Fixes to make Rogo more Chrome compatible.
    git-svn-id: svn://suivarro/touchstone/trunk@4984 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- paper/new_paper2.php
+++ paper/new_paper2.php
@@ -52,6 +52,8 @@ require '../lang/' . $language. '/include/timezones.inc';
   <style type="text/css">
     body {background-color:#F0F0F0; margin:6px; font-size:90%}
     .icon {color:#001687; padding: 15px 0px; vertical-align:top; width:98px; font-size:8pt}
+    select {margin:0px; padding:0px}
+    input[type="text"] {margin:0px}
   </style>
 
   <script type="text/javascript">
@@ -207,6 +209,8 @@ require '../lang/' . $language. '/include/timezones.inc';
   <style type="text/css">
     body {background-color:#F0F0F0; margin:4px; font-size:90%}
     h2 { font-size: 100%; margin: 8px 0 0 0; }
+    select {margin:0px; padding:0px}
+    input[type="text"] {margin:0px}
   </style>
 
 <?php
@@ -356,9 +360,7 @@ if ($_POST['paper_type'] == 'summative') {
       if ($i < 10) echo '0';
       echo "$i</option>\n";
     }
-    echo "</select>\n";
-    // Available from Month
-    echo "<select id=\"fmonth\" name=\"fmonth\" class=\"datecopy\">\n";
+    echo "</select><select id=\"fmonth\" name=\"fmonth\" class=\"datecopy\">\n";
     $current_month = (date('n') + 1);
     if ($current_month > 12) $current_month = 1;
     $months = array('', 'january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december');
@@ -378,9 +380,7 @@ if ($_POST['paper_type'] == 'summative') {
         }
       }
     }
-    echo "</select>\n";
-    // Available from Year
-    echo "<select id=\"fyear\" name=\"fyear\" class=\"datecopy\">\n";
+    echo "</select><select id=\"fyear\" name=\"fyear\" class=\"datecopy\">\n";
     for ($i = $date_array['year']; $i < ($date_array['year']+21); $i++) {
       if ($current_month == 1 and $i == ($date_array['year'] + 1)) {
         echo "<option value=\"$i\" selected>$i</option>\n";
@@ -388,7 +388,7 @@ if ($_POST['paper_type'] == 'summative') {
         echo "<option value=\"$i\">$i</option>\n";
       }
     }
-    echo "</select>\n<select id=\"ftime\" name=\"ftime\" class=\"datecopy\">\n";
+    echo "</select><select id=\"ftime\" name=\"ftime\" class=\"datecopy\">\n";
     // Available from Hour
     $times = array('000000'=>'00:00','003000'=>'00:30','010000'=>'01:00','013000'=>'01:30','020000'=>'02:00','023000'=>'02:30','030000'=>'03:00','033000'=>'03:30','040000'=>'04:00','043000'=>'04:30','050000'=>'05:00','053000'=>'05:30','060000'=>'06:00','063000'=>'06:30','070000'=>'07:00','073000'=>'07:30','080000'=>'08:00','083000'=>'08:30','090000'=>'09:00','093000'=>'09:30','100000'=>'10:00','103000'=>'10:30','110000'=>'11:00','113000'=>'11:30','120000'=>'12:00','123000'=>'12:30','130000'=>'13:00','133000'=>'13:30','140000'=>'14:00','143000'=>'14:30','150000'=>'15:00','153000'=>'15:30','160000'=>'16:00','163000'=>'16:30','170000'=>'17:00','173000'=>'17:30','180000'=>'18:00','183000'=>'18:30','190000'=>'19:00','193000'=>'19:30','200000'=>'20:00','203000'=>'20:30','210000'=>'21:00','213000'=>'21:30','220000'=>'22:00','223000'=>'22:30','230000'=>'23:00','233000'=>'23:30');
     foreach ($times as $key => $value) {
@@ -408,9 +408,7 @@ if ($_POST['paper_type'] == 'summative') {
       if ($i < 10) echo '0';
       echo "$i</option>\n";
     }
-    echo "</select>\n";
-    // Available to Month
-    echo "<select id=\"tmonth\" name=\"tmonth\" class=\"datecopy\">\n";
+    echo "</select><select id=\"tmonth\" name=\"tmonth\" class=\"datecopy\">\n";
     for ($i=1; $i<=12; $i++) {
       $trans_month = mb_substr($string[$months[$i]],0,3,'UTF-8');
       if ($i < 10) {
@@ -427,7 +425,7 @@ if ($_POST['paper_type'] == 'summative') {
         }
       }
     }
-    echo "</select>\n";
+    echo "</select>";
     // Available to Year
     if ($_POST['paper_type'] == 'summative' or $_POST['paper_type'] == 'osce' or $_POST['paper_type'] == 'offline') {
       $target_year = $date_array['year'];
@@ -442,7 +440,7 @@ if ($_POST['paper_type'] == 'summative') {
         echo "<option value=\"$i\">$i</option>\n";
       }
     }
-    echo "</select>&nbsp;<select id=\"ttime\" name=\"ttime\" class=\"datecopy\">\n";
+    echo "</select><select id=\"ttime\" name=\"ttime\" class=\"datecopy\">\n";
     // Available to Hour
     foreach ($times as $key => $value) {
       echo "<option value=\"" . $key . "\">" . $value . "</option>\n";
@@ -498,7 +496,7 @@ if ($_POST['paper_type'] == 'summative') {
 
   echo "</table>\n";
 
-  echo "<div style=\"font-weight:bold; color:#001687; font-size:120%\">" . $string['modules'] . "</div><div style=\"display:block; background-color:white; height:230px; overflow-y:scroll; border:1px solid #95AEC8; font-size:90%\">";
+  echo "<div style=\"font-weight:bold; color:#001687; font-size:120%\">" . $string['modules'] . "</div><div style=\"display:block; background-color:white; height:340px; overflow-y:scroll; border:1px solid #95AEC8; font-size:90%\">";
   $staff_modules_sql = "'" . implode("','", array_keys($staff_modules)) . "'";
 
   $module_no = 0;
@@ -510,9 +508,9 @@ if ($_POST['paper_type'] == 'summative') {
       $current_school = $module['school'];
     }
     if (isset($_POST['module']) and $_POST['module'] == $module['idMod']) {
-      echo "<div style=\"background-color:#B3C8E8\" id=\"divmodule$module_no\"><input type=\"checkbox\" onclick=\"toggle('divmodule$module_no')\" name=\"module$module_no\" id=\"module$module_no\" value=\"" . $module['idMod'] . "\" checked />&nbsp;" . $module['id'] . " - " . substr($module['fullname'],0,60) . "</div>\n";
+      echo "<div style=\"background-color:#B3C8E8\" id=\"divmodule$module_no\"><input type=\"checkbox\" onclick=\"toggle('divmodule$module_no')\" name=\"module$module_no\" id=\"module$module_no\" value=\"" . $module['idMod'] . "\" checked />&nbsp;<label for=\"module$module_no\">" . $module['id'] . " - " . substr($module['fullname'],0,60) . "</label></div>\n";
     } else {
-      echo "<div style=\"background-color:white\" id=\"divmodule$module_no\"><input type=\"checkbox\" onclick=\"toggle('divmodule$module_no')\" name=\"module$module_no\" id=\"module$module_no\" value=\"" . $module['idMod'] . "\" />&nbsp;" . $module['id'] . " - " . substr($module['fullname'],0,60) . "</div>\n";
+      echo "<div style=\"background-color:white\" id=\"divmodule$module_no\"><input type=\"checkbox\" onclick=\"toggle('divmodule$module_no')\" name=\"module$module_no\" id=\"module$module_no\" value=\"" . $module['idMod'] . "\" />&nbsp;<label for=\"module$module_no\">" . $module['id'] . " - " . substr($module['fullname'],0,60) . "</label></div>\n";
     }
     $module_no++;
   }
