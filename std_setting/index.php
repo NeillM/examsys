d36ca4a784e0e66b8e9e30721acf65c2b279e018
Simon Wilkinson <Simon.Wilkinson@notitngham.ac.uk>
2013-04-11 13:02:43 +0000

    Security improvements to GET parameters.
    git-svn-id: svn://suivarro/touchstone/trunk@4977 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- std_setting/index.php
+++ std_setting/index.php
@@ -23,9 +23,18 @@
 */
 
 require '../include/staff_auth.inc';
-require '../include/std_set_shared_functions.inc';
+require_once '../include/errors.inc';
+require_once '../include/std_set_shared_functions.inc';
+require_once '../classes/paperproperties.class.php';
 
-$paperID = $_GET['paperID'];
+$paperID = check_var('paperID', 'GET', true, false, true);
+
+//get the paper properties
+$propertyObj = PaperProperties::get_paper_properties_by_id($paperID, $mysqli);
+if ($propertyObj == false) {  // No properties found, this crypt_name
+  $msg = sprintf($string['furtherassistance'], $configObject->get('support_email'), $configObject->get('support_email'));
+  $notice->display_notice_and_exit($mysqli, $string['pagenotfound'], $msg, $string['pagenotfound'], '../artwork/page_not_found.png', '#C00000', true, true);
+}
 
 function displayReview($review, $userObj) {
   $setter_id = $review['setter_id'];
@@ -140,39 +149,33 @@ function displayReview($review, $userObj) {
 $reviews_html = '';
 $total_marks = 0;
 
-$results = $mysqli->prepare("SELECT paper_title, total_mark FROM properties WHERE property_id=? LIMIT 1");
-$results->bind_param('i', $paperID);
-$results->execute();
-$results->store_result();
-$results->bind_result($paper_title, $total_mark);
-while ($results->fetch()) {
-  $reviews_html .= '<table class="header"><tr><th><div class="breadcrumb"><a href="../staff/index.php">' . $string['home'] . '</a>';
-  if (isset($_GET['module']) and $_GET['module'] != '') {
-    $reviews_html .= '&nbsp;&nbsp;<img src="../artwork/breadcrumb_arrow.png" width="4" height="7" alt="-" />&nbsp;&nbsp;<a href="../folder/details.php?module=' . $_GET['module'] . '">' . module_utils::get_moduleid_from_id($_GET['module'], $mysqli) . '</a>';
-  }
-  $reviews_html .= '&nbsp;&nbsp;<img src="../artwork/breadcrumb_arrow.png" width="4" height="7" alt="-" />&nbsp;&nbsp;<a href="../paper/details.php?paperID=' . $_GET['paperID'] . '&folder=' . $_GET['folder'] . '&module=' . $_GET['module'] . '">' . $paper_title . ' </a></div><div style="font-size:220%; color:black; font-weight:bold; margin-left:10px">' . $string['standardssetting'] . '</div></th><th style="text-align:right; vertical-align:top; padding-top:2px; padding-right:6px"><a href="#" onclick="launchHelp(97); return false;"><img src="../artwork/small_help_icon.gif" width="16" height="16" alt="Help" border="0" /></a></th></tr></table>';
+$paper_title  = $propertyObj->get_paper_title();
+$total_mark   = $propertyObj->get_total_mark();
 
-  $reviews_html .= <<< TABLEHEADER
-<table class="header">
-  <tr>
-  	<th style="width:18px">&nbsp;</td>
-  	<th class="vert_div" style="width:18%">&nbsp;{$string['standardsetter']}&nbsp;</th>
-  	<th class="vert_div" style="width:13%">&nbsp;{$string['date']}&nbsp;</th>
-  	<th class="vert_div" style="width:10%">&nbsp;{$string['passscore']}</th>
-  	<th class="vert_div" style="width:10%">&nbsp;{$string['distinction']}</th>
-  	<th class="vert_div" style="width:12%">&nbsp;{$string['reviewmarks']}</th>
-  	<th class="vert_div" style="width:10%">&nbsp;{$string['papertotal']}</th>
-  	<th class="vert_div" style="width:14%">&nbsp;{$string['method']}</th>
-  	<th class="vert_div" width="25%">&nbsp;</th>
- </tr>
- <tr><th colspan="9" class="bevel"></th></tr>
-TABLEHEADER;
-  $total_marks = $total_mark;
+$reviews_html .= '<table class="header"><tr><th><div class="breadcrumb"><a href="../staff/index.php">' . $string['home'] . '</a>';
+if (isset($_GET['module']) and $_GET['module'] != '') {
+  $reviews_html .= '&nbsp;&nbsp;<img src="../artwork/breadcrumb_arrow.png" width="4" height="7" alt="-" />&nbsp;&nbsp;<a href="../folder/details.php?module=' . $_GET['module'] . '">' . module_utils::get_moduleid_from_id($_GET['module'], $mysqli) . '</a>';
 }
-$results->close();
+$reviews_html .= '&nbsp;&nbsp;<img src="../artwork/breadcrumb_arrow.png" width="4" height="7" alt="-" />&nbsp;&nbsp;<a href="../paper/details.php?paperID=' . $_GET['paperID'] . '&folder=' . $_GET['folder'] . '&module=' . $_GET['module'] . '">' . $paper_title . ' </a></div><div style="font-size:220%; color:black; font-weight:bold; margin-left:10px">' . $string['standardssetting'] . '</div></th><th style="text-align:right; vertical-align:top; padding-top:2px; padding-right:6px"><a href="#" onclick="launchHelp(97); return false;"><img src="../artwork/small_help_icon.gif" width="16" height="16" alt="Help" border="0" /></a></th></tr></table>';
+
+$reviews_html .= <<< TABLEHEADER
+<table class="header">
+<tr>
+  <th style="width:18px">&nbsp;</td>
+  <th class="vert_div" style="width:18%">&nbsp;{$string['standardsetter']}&nbsp;</th>
+  <th class="vert_div" style="width:13%">&nbsp;{$string['date']}&nbsp;</th>
+  <th class="vert_div" style="width:10%">&nbsp;{$string['passscore']}</th>
+  <th class="vert_div" style="width:10%">&nbsp;{$string['distinction']}</th>
+  <th class="vert_div" style="width:12%">&nbsp;{$string['reviewmarks']}</th>
+  <th class="vert_div" style="width:10%">&nbsp;{$string['papertotal']}</th>
+  <th class="vert_div" style="width:14%">&nbsp;{$string['method']}</th>
+  <th class="vert_div" width="25%">&nbsp;</th>
+</tr>
+<tr><th colspan="9" class="bevel"></th></tr>
+TABLEHEADER;
 
 $no_reviews = 0;
-$reviews = get_reviews($mysqli, 'index', $paperID, $total_marks, $no_reviews);
+$reviews = get_reviews($mysqli, 'index', $paperID, $total_mark, $no_reviews);
 
 foreach ($reviews as $review) {
   $reviews_html .= displayReview($review, $userObject);
