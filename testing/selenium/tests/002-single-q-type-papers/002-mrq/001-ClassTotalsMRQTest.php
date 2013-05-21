7f11eb7977115cf1f7a3e3e44b46c3d8c65c3fef
Rob Ingram <Rob.Ingram@notitngham.ac.uk>
2013-05-21 14:49:48 +0000

    Fix test for tweaks to class totals display
    git-svn-id: svn://suivarro/touchstone/trunk@5222 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- testing/selenium/tests/002-single-q-type-papers/002-mrq/001-ClassTotalsMRQTest.php
+++ testing/selenium/tests/002-single-q-type-papers/002-mrq/001-ClassTotalsMRQTest.php
@@ -21,67 +21,72 @@ class ClassTotalsMRQTest extends PHPUnit_Extensions_SeleniumTestCase
 
     // Individuals
     $this->assertElementContainsText('//tr[@id="res1"]/td[5]', '-9');
-    $this->assertElementContainsText('//tr[@id="res1"]/td[6]', '-33%');
+    $this->assertElementContainsText('//tr[@id="res1"]/td[6]', '-33.33%');
     $this->assertElementContainsText('//tr[@id="res1"]/td[7]', 'Fail');
 
     $this->assertElementContainsText('//tr[@id="res2"]/td[5]', '27');
-    $this->assertElementContainsText('//tr[@id="res2"]/td[6]', '100%');
+    $this->assertElementContainsText('//tr[@id="res2"]/td[6]', '100.00%');
     $this->assertElementContainsText('//tr[@id="res2"]/td[7]', 'Distinction');
 
     $this->assertElementContainsText('//tr[@id="res3"]/td[5]', '-9');
-    $this->assertElementContainsText('//tr[@id="res3"]/td[6]', '-33%');
+    $this->assertElementContainsText('//tr[@id="res3"]/td[6]', '-33.33%');
     $this->assertElementContainsText('//tr[@id="res3"]/td[7]', 'Fail');
 
     $this->assertElementContainsText('//tr[@id="res4"]/td[5]', '3.5');
-    $this->assertElementContainsText('//tr[@id="res4"]/td[6]', '13%');
+    $this->assertElementContainsText('//tr[@id="res4"]/td[6]', '12.96%');
     $this->assertElementContainsText('//tr[@id="res4"]/td[7]', 'Fail');
 
     $this->assertElementContainsText('//tr[@id="res5"]/td[5]', '-3.5');
-    $this->assertElementContainsText('//tr[@id="res5"]/td[6]', '-13%');
+    $this->assertElementContainsText('//tr[@id="res5"]/td[6]', '-12.96%');
     $this->assertElementContainsText('//tr[@id="res5"]/td[7]', 'Fail');
 
     // Overall
 
     // Failures
-    $this->assertElementContainsText('//table/tbody/tr[22]/td/table/tbody/tr[3]/td[2]', '4');
-    $this->assertElementContainsText('//table/tbody/tr[22]/td/table/tbody/tr[3]/td[3]', '(80% of cohort)');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td/table/tbody/tr[3]/td[2]', '4');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td/table/tbody/tr[3]/td[3]', '(80% of cohort)');
     // Passes
-    $this->assertElementContainsText('//table/tbody/tr[22]/td/table/tbody/tr[4]/td[2]', '0');
-    $this->assertElementContainsText('//table/tbody/tr[22]/td/table/tbody/tr[4]/td[3]', '(0% of cohort)');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td/table/tbody/tr[4]/td[2]', '0');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td/table/tbody/tr[4]/td[3]', '(0% of cohort)');
     // Distinctions
-    $this->assertElementContainsText('//table/tbody/tr[22]/td/table/tbody/tr[5]/td[2]', '1');
-    $this->assertElementContainsText('//table/tbody/tr[22]/td/table/tbody/tr[5]/td[3]', '(20% of cohort)');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td/table/tbody/tr[5]/td[2]', '1');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td/table/tbody/tr[5]/td[3]', '(20% of cohort)');
 
     // Total marks
-    $this->assertElementContainsText('//table/tbody/tr[22]/td/table/tbody/tr[6]/td[2]', '27');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td/table/tbody/tr[6]/td[2]', '27');
     // Mean
-    $this->assertElementContainsText('//table/tbody/tr[22]/td/table/tbody/tr[8]/td[2]', '1.8');
-    $this->assertElementContainsText('//table/tbody/tr[22]/td/table/tbody/tr[8]/td[3]', '(6.8%)');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td/table/tbody/tr[8]/td[2]', '1.8');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td/table/tbody/tr[8]/td[3]', '(6.67%)');
     // Median
-    $this->assertElementContainsText('//table/tbody/tr[22]/td/table/tbody/tr[9]/td[2]', '-9');
-    $this->assertElementContainsText('//table/tbody/tr[22]/td/table/tbody/tr[9]/td[3]', '(-33%)');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td/table/tbody/tr[9]/td[2]', '-3.5');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td/table/tbody/tr[9]/td[3]', '(-12.96%)');
     // Standard Deviation
-    $this->assertElementContainsText('//table/tbody/tr[22]/td/table/tbody/tr[10]/td[2]', '14.99');
-    $this->assertElementContainsText('//table/tbody/tr[22]/td/table/tbody/tr[10]/td[3]', '(55.4%)');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td/table/tbody/tr[10]/td[2]', '14.99');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td/table/tbody/tr[10]/td[3]', '(55.53%)');
     // Max
-    $this->assertElementContainsText('//table/tbody/tr[22]/td/table/tbody/tr[11]/td[2]', '27');
-    $this->assertElementContainsText('//table/tbody/tr[22]/td/table/tbody/tr[11]/td[3]', '(100%)');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td/table/tbody/tr[11]/td[2]', '27');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td/table/tbody/tr[11]/td[3]', '(100.00%)');
     // Min
-    $this->assertElementContainsText('//table/tbody/tr[22]/td/table/tbody/tr[12]/td[2]', '-9');
-    $this->assertElementContainsText('//table/tbody/tr[22]/td/table/tbody/tr[12]/td[3]', '(-33%)');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td/table/tbody/tr[12]/td[2]', '-9');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td/table/tbody/tr[12]/td[3]', '(-33.33%)');
     // Range
-    $this->assertElementContainsText('//table/tbody/tr[22]/td/table/tbody/tr[13]/td[2]', '36');
-    $this->assertElementContainsText('//table/tbody/tr[22]/td/table/tbody/tr[13]/td[3]', '(133%)');
-    // Top 10%
-    $this->assertElementContainsText('//table/tbody/tr[22]/td/table/tbody/tr[14]/td[2]', '65.2%');
-    // Top 15%
-    $this->assertElementContainsText('//table/tbody/tr[22]/td/table/tbody/tr[15]/td[2]', '47.8%');
-    // Top 20%
-    $this->assertElementContainsText('//table/tbody/tr[22]/td/table/tbody/tr[16]/td[2]', '30.4%');
-    // Top 25%
-    $this->assertElementContainsText('//table/tbody/tr[22]/td/table/tbody/tr[17]/td[2]', '13%');
-    // Bottom 10%
-    $this->assertElementContainsText('//table/tbody/tr[22]/td/table/tbody/tr[18]/td[2]', '-33%');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td/table/tbody/tr[13]/td[2]', '36');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td/table/tbody/tr[13]/td[3]', '(133.33%)');
+
+    // Deciles
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td[3]/table/tbody/tr/td[2]', '65.19%');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td[3]/table/tbody/tr[2]/td[2]', '30.37%');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td[3]/table/tbody/tr[3]/td[2]', '7.78%');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td[3]/table/tbody/tr[4]/td[2]', '-2.59%');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td[3]/table/tbody/tr[5]/td[2]', '-12.96%');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td[3]/table/tbody/tr[6]/td[2]', '-21.11%');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td[3]/table/tbody/tr[7]/td[2]', '-29.26%');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td[3]/table/tbody/tr[8]/td[2]', '-33.33%');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td[3]/table/tbody/tr[9]/td[2]', '-33.33%');
+
+    // Quartiles
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td[5]/table/tbody/tr/td[2]', '-33.33%');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td[5]/table/tbody/tr[2]/td[2]', '-12.96%');
+    $this->assertElementContainsText('//table[2]/tbody/tr[2]/td[5]/table/tbody/tr[3]/td[2]', '12.96%');
   }
 }
-?>
