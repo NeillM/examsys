831ee1d97b0f91c18acb6357bb9b7bc0a48e2dd8
Simon Wilkinson <Simon.Wilkinson@nottingham.ac.uk>
2013-06-19 16:36:56 +0100

    Fix for Boolean Response report when Ranking questions use allow half marks for neighbors.

--- export/assessment_boolean.php
+++ export/assessment_boolean.php
@@ -252,7 +252,7 @@ foreach ($user_results as $individual) {
 
             if ($paper_answers[$a] != '' and substr($tmp_exclude, $a+$sub_parts, 1) == '0') $csv .= ',Q' . $q_no . chr($a+65);
           }
-        } elseif (($question['q_type'] == 'dichotomous' or $question['q_type'] == 'blank' or $question['q_type']=='rank') and $question['score_method'] != 'Mark per Question') {
+        } elseif (($question['q_type'] == 'dichotomous' or $question['q_type'] == 'blank' or $question['q_type']=='rank') and ($question['score_method'] != 'Mark per Question' or $question['score_method'] != 'Allow partial Marks')) {
           if ($tmp_exclude{0} == '0' or $question['q_type']!='rank') {
             for ($a=0; $a<count($question['correct']); $a++) {            
               if ($tmp_exclude{$a} == '0' and ($question['q_type'] !='rank' || $question['correct'][$a] > 0)) $csv .= ',Q' . $q_no . chr($a+65);
