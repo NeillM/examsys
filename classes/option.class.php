610e764896ec496c8a92d737cea21bc066866233
Rob Ingram <rob.ingram@nottingham.ac.uk>
2013-06-24 10:13:27 +0100

    Add proper Rogo header

--- classes/option.class.php
+++ classes/option.class.php
@@ -1,17 +1,26 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+// This file is part of Rogō
+//
+// Rogō is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Rogō is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Rogō.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Description of option
+ * An option for a question
  *
  * @author brzab3
  */
 class Option {
-  
+
   public $id = -1;
   protected $question_id = null;
   protected $text = '';
@@ -24,7 +33,7 @@ class Option {
   protected $marks_correct = 1;
   protected $marks_incorrect = 0;
   protected $marks_partial = 0;
-  
+
 }
 
 ?>
