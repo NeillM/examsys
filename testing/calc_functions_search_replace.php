44a4e54b5efacb3c75fa2748acc8fe59a7bc22bf
Simon Atack <Simon.Atack@nottingham.ac.uk>
2013-07-24 18:49:23 +0100

    Convert calculation questions to enhanced calculation questions

--- testing/calc_functions_search_replace.php
+++ testing/calc_functions_search_replace.php
@@ -12,7 +12,19 @@
 
 <body>
 <?php
+set_time_limit(0);
 
+
+$sql="select settings from questions where q_id=123507 or q_id=122995";
+$result = $mysqli->prepare("$sql");
+$result->execute();
+$result->store_result();
+$result->bind_result($settings);
+while ($result->fetch()) {
+ var_dump(json_decode($settings,true));
+}
+
+/*
 function dopowrep($correct) {
 
   unset($left);
@@ -170,34 +182,100 @@ $result->bind_result($qid,$settings);
 while ($result->fetch()) {
  $qids[$qid]=$settings;
 }
-
+if(!isset($qids) or count($qids)==0) {
+  exit();
+}
 var_dump($qids);
 $vars=array('$A','$B','$C','$D','$E','$F','$G','$H','$I','$J','$K','$L');
 
 foreach($qids as $qid=>$settings) {
-  $result = $mysqli->prepare("SELECT option_text,correct,id_num from  options WHERE o_id=? order by id_num;");
+
+  print $qid . "   ";
+  $result = $mysqli->prepare("SELECT option_text,correct,id_num,marks_correct,marks_incorrect,marks_partial from  options WHERE o_id=? order by id_num;");
   $result->bind_param('i',$qid);
   $result->execute();
   $result->store_result();
-  $result->bind_result($optiontext,$correct,$id_num);
+  $result->bind_result($optiontext,$correct,$id_num,$marks_correct,$marks_incorrect,$marks_partial);
   $settings=json_decode($settings,true);
   $changed=false;
   $loc=0;
+  unset($optionids);
+  $optionids=array();
   while ($result->fetch()) {
+    $optionids[]=$id_num;
     $changed=true;
     $opts=explode(',',$optiontext);
 $settings['vars'][$vars[$loc]]['min']=$opts[0];
 $settings['vars'][$vars[$loc]]['max']=$opts[1];
 $settings['vars'][$vars[$loc]]['inc']=$opts[2];
 $settings['vars'][$vars[$loc]]['dec']=$opts[3];
-$settings['answers']['formula']=$correct;
-    $settings['answers']['units']=$settings['units'];
+$ansdat['formula']=$correct;
+    $settings['marks_correct']=$marks_correct;
+    $settings['marks_incorrect']=$marks_incorrect;
+    $settings['marks_partial']=$marks_partial;
 
+    $ansdat['units']=$settings['units'];
+
+    $sql="DELETE from options where id_num=?";
+    $delete = $mysqli->prepare($sql);
+    print $mysqli->error;
+    $delete->bind_param('i', $id_num);
+
+    $delete->execute();
 $loc++;
-
-var_dump($optiontext);
+print "$id_num :: ";
+//var_dump($optiontext);
   }
+  if(!isset($settings['strictdisplay'])) {
+    $settings['strictdisplay']=false;
+  }
+  if(!isset($settings['strictzeros'])) {
+    $settings['strictzeros']=false;
+  }
+
+  if(!isset($settings['dp'])) {
+    if(!isset($settings['answer_decimals'])) {
+          $settings['dp']=0;
+    } else {
+      $settings['dp']=$settings['answer_decimals'];
+      unset($settings['answer_decimals']);
+    }
+
+  }
+
+  if(!isset($settings['fulltoltyp'])) {
+    $rep='#';
+    if(strpos($settings['tolerance_full'],'%') !== false){
+      $settings['tolerance_full']=substr($settings['tolerance_full'],0,strpos($settings['tolerance_full'],'%'));
+      $rep='%';
+    }
+    $settings['fulltoltyp']=$rep;
+
+  }  if(!isset($settings['parttoltyp'])) {
+    $rep='#';
+    if(strpos($settings['tolerance_partial'],'%') !== false){
+      $settings['tolerance_partial']=substr($settings['tolerance_partial'],0,strpos($settings['tolerance_partial'],'%'));
+      $rep='%';
+    }
+    $settings['parttoltyp']=$rep;
+  }
+
+  if(!isset($settings['marks_unit'])) {
+    $settings['marks_unit']=0;
+  }
+
+  if(!isset($settings['show_units'])) {
+    $settings['show_units']=true;
+  }
+
+  $settings['answers'][]=$ansdat;
   unset($settings['units']);
+
+  $sql="UPDATE questions set settings=?,q_type='enhancedcalc' where q_id=?";
+  $update = $mysqli->prepare($sql);
+  $settings=json_encode($settings);
+  $update->bind_param('si',$settings, $qid);
+  $update->execute();
   var_dump($settings);
   print "<BR>";
 }
