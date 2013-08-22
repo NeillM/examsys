ed1d6dcc1504ce14e4d776db7c5e6ac103c3bef2
Simon Wilkinson <Simon.Wilkinson@nottingham.ac.uk>
2013-08-22 14:03:35 +0100

    Fix for adding modules.

--- admin/add_module.php
+++ admin/add_module.php
@@ -110,7 +110,6 @@ if (isset($_POST['submit']) and $unique_moduleid == true) {
 
   $modID = module_utils::add_modules($modulecode, $fullname, $active, $schoolid, $vle_api, $sms_api, $selfenroll, $peer, $external, $stdset, $mapping, $neg_marking, $ebel_grid_template, $mysqli, $sms_import, $timed_exams, $exam_q_feedback, $add_team_members, $map_level);
 
-  $mysqli->close();
   header("location: list_modules.php");
   exit();
 } else {
