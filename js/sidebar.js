10607275d3b5b2ff2671064f7e11937770ddec82
Simon Wilkinson <Simon.Wilkinson@nottingham.ac.uk>
2013-08-27 15:04:44 +0100

    Inclusion of more JQuery in the popup sub-menus.

--- js/sidebar.js
+++ js/sidebar.js
@@ -4,7 +4,7 @@
   var myDownInterval = 0;
   if (typeof cfgRootPath == 'undefined') var cfgRootPath = '';
   
-  function scrollUpStart(submenuID,arrayID,urlID,arrayName) {
+  function scrollUpStart(submenuID, arrayID, urlID, arrayName) {
     myUpInterval = window.setInterval(function () {
       if (scrollLine > 0) {
         scrollLine--;
@@ -13,51 +13,39 @@
           limit = arrayID.length-1;
         }
         var line = 0;
-        for (i=scrollLine;i<=limit;i++) {
+        for (i=scrollLine; i<=limit; i++) {
           submenuItemID = submenuID.substr(5,1) + '_' + line;
           if (urlID[i].substr(0,1) == '-') {
-            if (navigator.appName == 'Microsoft Internet Explorer') {
-              document.getElementById(submenuItemID).outerHTML =  '<div class="popupitem" id="' + submenuItemID + '"><hr nonshade="nonshade" style="height:1px; border:none; background-color:#C0C0C0; color:#C0C0C0" /></div>';
-            } else {
-              document.getElementById(submenuItemID).innerHTML = '<hr nonshade="nonshade" style="height:1px; border:none; background-color:#C0C0C0; color:#C0C0C0" />';
-              document.getElementById(submenuItemID).setAttribute('onclick',"window.location=''");
-            }
+						$('#' + submenuItemID).html('<hr nonshade="nonshade" style="height:1px; border:none; background-color:#C0C0C0; color:#C0C0C0" />');
+						$('#' + submenuItemID).attr('onclick', "window.location=''");
           } else if (urlID[i].substr(0,1) == '#') {
-            if (navigator.appName == 'Microsoft Internet Explorer') {
-              document.getElementById(submenuItemID).outerHTML =  '<div class="popupitembold" id="' + submenuItemID + '">' + urlID[i].substr(1) + '</div>';
-            } else {
-              document.getElementById(submenuItemID).innerHTML = urlID[i].substr(1);
-            }
+            $('#' + submenuItemID).html(urlID[i].substr(1));
           } else {
-            if (navigator.appName == 'Microsoft Internet Explorer') {
-              document.getElementById(submenuItemID).outerHTML =  '<div class="popupitem" id="' + submenuItemID + '" onclick="window.location=\'' + urlID[i] + '\'" onmouseover="menuRowOn(\'' + submenuItemID + '\');" onmouseout="menuRowOff(\'' + submenuItemID + '\');">' + arrayID[i] + '</div>';
-            } else {
-              document.getElementById(submenuItemID).innerHTML = arrayID[i];
-              document.getElementById(submenuItemID).setAttribute('onclick',"window.location='" + urlID[i] + "'");
-            }
+						$('#' + submenuItemID).html(arrayID[i]);
+            $('#' + submenuItemID).attr('onclick', "window.location='" + urlID[i] + "'");
           }
           line++;
         }
         downID = submenuID.substr(5,1) + '_down';
-        document.getElementById(downID).innerHTML = '<img src="' + cfgRootPath + '/artwork/submenu_down_on.png" width="9" height="5" alt="down" border="0" />&nbsp;';
+        $('#' + downID).html('<img src="' + cfgRootPath + '/artwork/submenu_down_on.png" width="9" height="5" alt="down" />&nbsp;');
       } else {
         upID = submenuID.substr(5,1) + '_up';
-        document.getElementById(upID).innerHTML = '<img src="' + cfgRootPath + '/artwork/submenu_up_off.png" width="9" height="5" alt="up" border="0" />&nbsp;';
+        $('#' + upID).html('<img src="' + cfgRootPath + '/artwork/submenu_up_off.png" width="9" height="5" alt="up" />&nbsp;');
         clearInterval(myDownInterval);
       }
     },50);
   }
   
-  function scrollUpEnd() {
+	function scrollUpEnd() {
     clearInterval(myUpInterval);
   }
   
-  function scrollDownStart(submenuID,arrayID,urlID,arrayName) {
+  function scrollDownStart(submenuID, arrayID, urlID, arrayName) {
     myDownInterval = window.setInterval(function () {
       if (scrollLine < (arrayID.length-20)) {
         if (scrollLine == 0) {
           upID = submenuID.substr(5,1) + '_up';
-          document.getElementById(upID).innerHTML = '<img src="' + cfgRootPath + '/artwork/submenu_up_on.png" width="9" height="5" alt="up" border="0" />&nbsp;';
+          $('#' + upID).html('<img src="' + cfgRootPath + '/artwork/submenu_up_on.png" width="9" height="5" alt="up" />&nbsp;');
         }
         scrollLine++;
         var limit = (scrollLine + 19);
@@ -65,53 +53,39 @@
           limit = arrayID.length-1;
         }
         var line = 0;
-        for (i=scrollLine;i<=limit;i++) {
+        for (i=scrollLine; i<=limit; i++) {
           submenuItemID = submenuID.substr(5,1) + '_' + line;
           if (urlID[i].substr(0,1) == '-') {
-            if (navigator.appName == 'Microsoft Internet Explorer') {
-              document.getElementById(submenuItemID).outerHTML =  '<div class="popupitem" id="' + submenuItemID + '"><hr nonshade="nonshade" style="height:1px; border:none; background-color:#C0C0C0; color:#C0C0C0" /></div>';
-            } else {
-              document.getElementById(submenuItemID).innerHTML = '<hr nonshade="nonshade" style="height:1px; border:none; background-color:#C0C0C0; color:#C0C0C0" />';
-              document.getElementById(submenuItemID).setAttribute('onclick',"window.location=''");
-            }
+						$('#' + submenuItemID).html('<hr nonshade="nonshade" style="height:1px; border:none; background-color:#C0C0C0; color:#C0C0C0" />');
+						$('#' + submenuItemID).attr('onclick', "window.location=''");
           } else if (urlID[i].substr(0,1) == '#') {
-          if (navigator.appName == 'Microsoft Internet Explorer') {
-            document.getElementById(submenuItemID).outerHTML =  '<div class="popupitembold" id="' + submenuItemID + '">' + urlID[i].substr(1) + '</div>';
+						$('#' + submenuItemID).html(urlID[i].substr(1));
           } else {
-            document.getElementById(submenuItemID).innerHTML = urlID[i].substr(1);
-          }
-          } else {
-            if (navigator.appName == 'Microsoft Internet Explorer') {
-              document.getElementById(submenuItemID).outerHTML =  '<div class="popupitem" id="' + submenuItemID + '" onclick="window.location=\'' + urlID[i] + '\'" onmouseover="menuRowOn(\'' + submenuItemID + '\');" onmouseout="menuRowOff(\'' + submenuItemID + '\');">' + arrayID[i] + '</div>';
-            } else {
-              document.getElementById(submenuItemID).innerHTML = arrayID[i];
-              document.getElementById(submenuItemID).setAttribute('onclick',"window.location='" + urlID[i] + "'");
-            }
+						$('#' + submenuItemID).html(arrayID[i]);
+						$('#' + submenuItemID).attr('onclick', "window.location='" + urlID[i] + "'");
           }
           line++;
         }
       } else {
         downID = submenuID.substr(5,1) + '_down';
-        document.getElementById(downID).innerHTML = '<img src="' + cfgRootPath + '/artwork/submenu_down_off.png" width="9" height="5" alt="down" border="0" />&nbsp;';
+        $('#' + downID).html('<img src="' + cfgRootPath + '/artwork/submenu_down_off.png" width="9" height="5" alt="down" />&nbsp;');
         clearInterval(myDownInterval);
       }
     },50);
   }
-  
+	
   function scrollDownEnd() {
     clearInterval(myDownInterval);
   }
   
   function menuRowOn(rowID) {
-    document.getElementById(rowID).style.backgroundColor='#FFE7A2';
-    document.getElementById(rowID).style.borderColor='#FFBD69';
-    //document.getElementById(rowID).style.color='white';
+    $('#' + rowID).css('background-color', '#FFE7A2');
+    $('#' + rowID).css('border-color', '#FFBD69');
   }
 
   function menuRowOff(rowID) {
-    document.getElementById(rowID).style.backgroundColor='white';
-    document.getElementById(rowID).style.borderColor='white';
-    //document.getElementById(rowID).style.color='black';
+    $('#' + rowID).css('background-color', 'white');
+    $('#' + rowID).css('border-color', 'white');
   }
 
   function showMenu(submenuID, menuID, callingID, arrayID, urlID, e) {    
@@ -123,53 +97,41 @@
     }
     if (arrayID.length > 20) {
       upID = submenuID.substr(5,1) + '_up';
-      document.getElementById(upID).innerHTML = '<img src="' + cfgRootPath + '/artwork/submenu_up_off.png" width="9" height="5" alt="upx" border="0" />&nbsp;';
+      $('#' + upID).html('<img src="' + cfgRootPath + '/artwork/submenu_up_off.png" width="9" height="5" alt="up" />&nbsp;');
       downID = submenuID.substr(5,1) + '_down';
-      document.getElementById(downID).innerHTML = '<img src="' + cfgRootPath + '/artwork/submenu_down_on.png" width="9" height="5" alt="down" border="0" />&nbsp;';
+      $('#' + downID).html('<img src="' + cfgRootPath + '/artwork/submenu_down_on.png" width="9" height="5" alt="down" />&nbsp;');
     }
     var line = 0;
     for (i=scrollLine;i<=limit;i++) {
       submenuItemID = submenuID.substr(5,1) + '_' + line;
       if (urlID[i].substr(0,1) == '-') {
-        if (navigator.appName == 'Microsoft Internet Explorer') {
-          document.getElementById(submenuItemID).outerHTML =  '<div class="popupitem" id="' + submenuItemID + '"><hr nonshade="nonshade" style="height:1px; border:none; background-color:#C0C0C0; color:#C0C0C0" /></div>';
-        } else {
-          document.getElementById(submenuItemID).innerHTML = '<hr nonshade="nonshade" style="height:1px; border:none; background-color:#C0C0C0; color:#C0C0C0" />';
-          document.getElementById(submenuItemID).setAttribute('onclick',"window.location=''");
-        }
+				$('#' + submenuItemID).html('<hr nonshade="nonshade" style="height:1px; border:none; background-color:#C0C0C0; color:#C0C0C0" />');
+				$('#' + submenuItemID).attr('onclick', "window.location=''");
       } else if (urlID[i].substr(0,1) == '#') {
-        if (navigator.appName == 'Microsoft Internet Explorer') {
-          document.getElementById(submenuItemID).outerHTML =  '<div class="popupitembold" id="' + submenuItemID + '">' + urlID[i].substr(1) + '</div>';
-        } else {
-          document.getElementById(submenuItemID).innerHTML = urlID[i].substr(1);
-        }
+				$('#' + submenuItemID).html(urlID[i].substr(1));
       } else {
-        if (navigator.appName == 'Microsoft Internet Explorer') {
-          document.getElementById(submenuItemID).outerHTML =  '<div class="popupitem" id="' + submenuItemID + '" onclick="window.location=\'' + urlID[i] + '\'" onmouseover="menuRowOn(\'' + submenuItemID + '\');" onmouseout="menuRowOff(\'' + submenuItemID + '\');">' + arrayID[i] + '</div>';
-        } else {
-          document.getElementById(submenuItemID).innerHTML = arrayID[i];
-          document.getElementById(submenuItemID).setAttribute('onclick',"window.location='" + urlID[i] + "'");
-        }
+ 				$('#' + submenuItemID).html(arrayID[i]);
+				$('#' + submenuItemID).attr('onclick', "window.location='" + urlID[i] + "'");
       }
       line++;
     }
 
     if (!e) var e = window.event;
-    if (document.getElementById(submenuID).style.display != 'block') {
+    if ($('#' + submenuID).css('display') != 'block') {
       hideMenus(e);
-      document.getElementById(submenuID).style.display = 'block';
+      $('#' + submenuID).show();
     } else {
       hideMenus(e);
     }
-    popupHeight = document.getElementById(submenuID).offsetHeight;
+    popupHeight = $('#' + submenuID).height();
     
-    sidebarHeight = document.getElementById('left-sidebar').offsetHeight;
+    sidebarHeight = $('#left-sidebar').height();
     
-    mytop = document.getElementById(callingID).offsetParent.offsetTop + document.getElementById(menuID).offsetTop + 26;
+    mytop = $('#' + callingID).offset().top - 10;
     if ((mytop + popupHeight) > sidebarHeight) {
       mytop = sidebarHeight - popupHeight - 10;
     }
-    document.getElementById(submenuID).style.top = mytop + 'px';
+    $('#' + submenuID).css('top', mytop + 'px');
     
     e.cancelBubble = true;
     
