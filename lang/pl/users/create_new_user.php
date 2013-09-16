9532ba5a1eecd1d48a3b067b50a1e9694d51312e
Nikodem Miranowicz <Nikodem.Miranowicz@nottingham.ac.uk>
2013-09-16 11:05:11 +0100

    create_new_user.php cfg_web_root fix

--- lang/pl/users/create_new_user.php
+++ lang/pl/users/create_new_user.php
@@ -14,8 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Rogo.  If not, see <http://www.gnu.org/licenses/>.
 
-require '../lang/' . $language . '/include/user_search_options.inc';
-require '../lang/' . $language . '/include/titles.inc';
+require $cfg_web_root . '/lang/' . $language . '/include/user_search_options.inc';
+require $cfg_web_root . '/lang/' . $language . '/include/titles.inc';
 
 $string['createnewuser'] = 'Zdefiniuj nowego użytkownika';
 $string['getldapdetails'] = 'Uzyskaj szczegóły LDAP';
