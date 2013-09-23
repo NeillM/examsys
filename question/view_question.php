5d6f1f3a6ba23446738d5c47f4942f6b04a6cfb9
Nikodem Miranowicz <Nikodem.Miranowicz@nottingham.ac.uk>
2013-09-23 15:32:22 +0100

    qhtml5 question preview js inclusion

--- question/view_question.php
+++ question/view_question.php
@@ -49,6 +49,14 @@
   <script type="text/javascript" src="../js/jquery-1.6.1.min.js"></script>
   <script type="text/javascript" src="../js/jquery.flash_q.js"></script>
   <script type="text/javascript" src="../tools/mee/mee/js/mee_src.js"></script>
+	<!-- HTML5 part start -->
+<script type='text/javascript'><?php echo "var lang_string = ".  json_encode($jstring) . ";\n";?></script>
+<script type="text/javascript" src="../js/html5.images.js"></script>
+<script type="text/javascript" src="../js/qsharedf.js"></script>
+<script type="text/javascript" src="../js/qlabelling.js"></script>
+<script type="text/javascript" src="../js/qhotspot.js"></script>
+<script type="text/javascript" src="../js/qarea.js"></script>
+<!-- HTML5 part end -->
   <script language="JavaScript">
     function write_string(p_string) {
       document.write(p_string);
