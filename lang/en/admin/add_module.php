ec380897e723cd00a862a50d9fbf92864051ea5d
Simon Wilkinson <Simon.Wilkinson@nottingham.ac.uk>
2013-10-07 10:39:26 +0100

    Language fix for sidebar menu.

--- lang/en/admin/add_module.php
+++ lang/en/admin/add_module.php
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Rogo.  If not, see <http://www.gnu.org/licenses/>.
 
+require '../lang/' . $language . '/admin/list_modules.php';
+
 $string['administrativetools'] = 'Administrative Tools';
 $string['entermoduleid'] = 'Please enter an Identifier for the module.';
 $string['entermoduletitle'] = 'Please enter a title for the module.';
