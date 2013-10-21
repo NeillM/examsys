8f570b6c9ed286b3166cc9796ba4e519e00169a8
Simon Atack <simon.atack@nottingham.ac.uk>
2013-10-21 18:20:53 +0100

    Fix for webhost being incorrectly set by update

--- updates/version5/201308191452_flash_html5.php
+++ updates/version5/201308191452_flash_html5.php
@@ -5,7 +5,6 @@
 $new_lines = array("//Questions\n  \$cfg_interactive_qs = 'html5';\n");
 $target_line = '$vle_apis';
 $updater_utils->add_line($string, '$cfg_interactive_qs', $new_lines, 99, $cfg_web_root, $target_line, 1);
-$cfg_web_host = $cfg_db_host;
 
 
 /*
