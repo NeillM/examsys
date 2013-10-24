faa87332d794617306f6ba8156cacf89ff7406a7
Simon Wilkinson <simon.wilkinson@nottingham.ac.uk>
2013-10-24 11:27:13 +0100

    1) Fix time/date bug, 2) Fix missing scenario and leadin on Area question type.

--- classes/questionEdit.class.php
+++ classes/questionEdit.class.php
@@ -319,10 +319,12 @@ Class QuestionEdit extends RogoObject {
       if($this->bloom == '') {
         $this->bloom = null;
       }
+			
+			
 
       // If $id is -1 we're inserting a new record
       if ($this->id == -1) {
-        $this->created = date ('Y-m-d H:i:sMarks (correct)');
+        $this->created = date ('Y-m-d H:i:s');
         $this->last_edited = date ('Y-m-d H:i:s');
         $params = array_merge(array('ssssssssssssssissssissssss'), $this->_data);
         $query = <<< QUERY
