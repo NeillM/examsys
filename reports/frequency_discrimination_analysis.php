3618dbed63d03b95348cb0b4735c59be62df1680
Simon Atack <simon.atack@nottingham.ac.uk>
2013-10-24 16:45:51 +0100

    Debugging to find error in frequency_discrimination_analysis.php

--- reports/frequency_discrimination_analysis.php
+++ reports/frequency_discrimination_analysis.php
@@ -1842,16 +1842,7 @@ function displayQuestion($q_no, $q_id, $theme, $scenario, $leadin, $q_type, $cor
   $result->execute();
   $result->bind_result($username, $tmp_userID, $question_ID, $tmp_answer, $q_type, $score_method, $display_method, $settings, $mark, $totalpos, $option_order, $started);
 
-$sql="SELECT username, log_metadata.userID, log$paper_type.q_id, user_answer, q_type, score_method, display_method, settings, mark, totalpos, option_order, started FROM log$paper_type, log_metadata, questions, users WHERE log$paper_type.metadataID = log_metadata.id AND log$paper_type.q_id = questions.q_id AND paperID = ? AND grade LIKE ? AND users.id = log_metadata.userID AND (users.roles='Student' OR users.roles='graduate') AND DATE_ADD(started, INTERVAL 2 MINUTE) >= ? AND started <= ? $student_modules_sql";
-
-echo "==========";
-var_dump($sql,$paperID, $_GET['repcourse'], $startdate, $enddate,$username, $tmp_userID, $question_ID, $tmp_answer, $q_type, $score_method, $display_method, $settings, $mark, $totalpos, $option_order, $started);
-
-
   while ($result->fetch()) {
-    var_dump('*******', $username, $tmp_userID, $question_ID, $tmp_answer, $q_type, $score_method, $display_method, $settings, $mark, $totalpos, $option_order, $started,'****');
-
-
     storeData($freq_array, $question_ID, $tmp_answer, $q_type, $display_method, $settings, $mark, $totalpos, $stop_words, 'all');
     if (isset($bottom_cohort[$started][$username])) {
       storeData($bottom_log_array, $question_ID, $tmp_answer, $q_type, $display_method, $settings, $mark, $totalpos, $stop_words, 'bottom');
