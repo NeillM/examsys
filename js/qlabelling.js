98c64d0135828826f3386c7ca4a04be3c56b79e4
Nikodem Miranowicz <Nikodem.Miranowicz@nottingham.ac.uk>
2013-11-12 11:01:13 +0000

    qhtml5 dropdown menu fix

--- js/qlabelling.js
+++ js/qlabelling.js
@@ -472,6 +472,15 @@ function setUpLabelling(num, doorId, lang, image, config, answer, extra, colour,
 		this.menu_img.onload = menu_img_onload.bind(this);
 		this.menu_img.src = '/js/images/combined.png'; 
 	}		
+	
+			//renumbering ids
+		for (i=0;i<this.answerBox.length;i++)
+			for (j=0;j<this.answerBox[i].length;j++) 
+				console.log(i,j,this.answerBox[i][j])
+
+		//renumbering ids
+		for (i=0;i<this.pholderBox.length;i++) 
+			console.log(i,this.pholderBox[i])
 }
 
 function combo_scope(answer_set) {
@@ -483,6 +492,7 @@ function combo_scope(answer_set) {
 }
 	
 function ql_draw_box(i,j,temp_x,temp_y) {
+	if (this.qType != "menu") {temp_x++;temp_y++;}
 	var this_box = this.answerBox[i][j];
 	if (j == 99) this_box = this.pholderBox[i];
 	
@@ -569,10 +579,10 @@ function ql_draw_box(i,j,temp_x,temp_y) {
 			this.lineDraw(this.context,'#FF0000',temp_x+5.5,temp_y+this.fontSizes[this.fontSizePos]-1.5,tmp_width-10,0);
 			this.context.strokeStyle = tmp_style;
 		}
-		this.fillWrappedText(this.context,wrapped[0],Math.round(temp_x+tmp_width*0.5)+0.5,Math.round(temp_y+this.fontSizes[this.fontSizePos])-0.5);
+		this.fillWrappedText(this.context,wrapped[0],Math.round(temp_x+tmp_width*0.5)+0.5,Math.round(temp_y+this.fontSizes[this.fontSizePos])+0.5);
     this.context.fillStyle=this.currentColours[0];
 
-		var tmp_halfpoint = this.lineThickness/2-Math.round(this.lineThickness/2);
+		var tmp_halfpoint = Math.round(this.lineThickness/2)-this.lineThickness/2;
 		this.context.strokeRect(temp_x+tmp_halfpoint,temp_y+tmp_halfpoint,this.labelWidthEffect,this.labelHeightEffect);
 
 		if (this.qType == "menu") {
@@ -585,7 +595,7 @@ function ql_draw_box(i,j,temp_x,temp_y) {
 			//dropdown combo triangle sign
 			this.context.lineWidth = 1
 			this.context.strokeStyle='#000';
-			for (a=0;a<4;a++) this.context.strokeRect(tmp_dim[0]+5+a,tmp_dim[1]+Math.round(tmp_dim[3]/2)+a-1.5,7-2*a,0);
+			for (a=0;a<4;a++) this.context.strokeRect(tmp_dim[0]+6+a,tmp_dim[1]+Math.round(tmp_dim[3]/2)+a-1.5,7-2*a,0);
 			
 			this.context.lineWidth = this.lineThickness;
 	    this.context.strokeStyle = this.currentColours[1];
@@ -879,7 +889,7 @@ function ql_redraw_canvas() {
     //testing end
 
 		this.context.clearRect(0,0,this.canvas.width,this.canvas.height);
- 		this.context.drawImage(this.gen_img,220,25-this.yOffset);
+ 		this.context.drawImage(this.gen_img,221,26-this.yOffset);
 
     //frames
     this.context.lineWidth = 1;
@@ -951,8 +961,8 @@ function ql_redraw_canvas() {
 					if (this.pholderBox[i][1] == 'text' ) {loc_width = this.labelWidthEffect;loc_height=this.labelHeightEffect;}
 
 					//fill and strike background rectangle
-					if (this.qmode!='script') this.context.fillRect(this.pholderBox[i][5]+0.5,this.pholderBox[i][6]+0.5,loc_width,loc_height);
-					this.context.strokeRect(this.pholderBox[i][5]+0.5,this.pholderBox[i][6]+0.5,loc_width,loc_height);
+					if (this.qmode!='script') this.context.fillRect(this.pholderBox[i][5]+1.5,this.pholderBox[i][6]+1.5,loc_width,loc_height);
+					this.context.strokeRect(this.pholderBox[i][5]+1.5,this.pholderBox[i][6]+1.5,loc_width,loc_height);
 				}
 			}
 		}
@@ -1088,7 +1098,7 @@ function ql_redraw_canvas() {
 		if (this.qType == "menu" && (this.qmode == 'answer' || this.qmode == 'script')) {		
 			//menus
 			for (i=this.answerBox.length-1;i>=0;i--) {
-				if (this.pholderBox[i][1] == 'text' && this.pholderBox[i][5]>220) {
+				if (typeof(this.pholderBox[i])!='undefined' && this.pholderBox[i][1] == 'text' && this.pholderBox[i][5]>220) {
 					this.ql_draw_box(i,99,this.pholderBox[i][5],this.pholderBox[i][6]);
 				}
 			}
@@ -1160,30 +1170,30 @@ function ql_redraw_canvas() {
 			//draw handlers for active label
 			this.context.strokeStyle='#cc0000';
 			this.context.strokeRect(
-				this.answerBox[this.active_box_id][this.active_box_combo][5]-this.lineThickness/2-0.5,
-				this.answerBox[this.active_box_id][this.active_box_combo][6]-this.lineThickness/2-0.5,
+				this.answerBox[this.active_box_id][this.active_box_combo][5]-this.lineThickness/2+1.5,
+				this.answerBox[this.active_box_id][this.active_box_combo][6]-this.lineThickness/2+1.5,
 				loc_width+this.lineThickness,
 				loc_height+this.lineThickness);
 
 			this.edtDot(
 				this.context,'#cc0000',
-				this.answerBox[this.active_box_id][this.active_box_combo][5]-this.lineThickness/2-0.5,
-				this.answerBox[this.active_box_id][this.active_box_combo][6]-this.lineThickness/2-0.5,
+				this.answerBox[this.active_box_id][this.active_box_combo][5]-this.lineThickness/2+1.5,
+				this.answerBox[this.active_box_id][this.active_box_combo][6]-this.lineThickness/2+1.5,
 				2.5+0.1*this.lineThickness);
 			this.edtDot(
 				this.context,'#cc0000',
-				this.answerBox[this.active_box_id][this.active_box_combo][5]-this.lineThickness/2-0.5,
-				this.answerBox[this.active_box_id][this.active_box_combo][6]+loc_height+this.lineThickness/2-0.5,
+				this.answerBox[this.active_box_id][this.active_box_combo][5]-this.lineThickness/2+1.5,
+				this.answerBox[this.active_box_id][this.active_box_combo][6]+loc_height+this.lineThickness/2+1.5,
 				2.5+0.1*this.lineThickness);
 			this.edtDot(
 				this.context,'#cc0000',
-				this.answerBox[this.active_box_id][this.active_box_combo][5]+loc_width+this.lineThickness/2-0.5,
-				this.answerBox[this.active_box_id][this.active_box_combo][6]-this.lineThickness/2-0.5,
+				this.answerBox[this.active_box_id][this.active_box_combo][5]+loc_width+this.lineThickness/2+1.5,
+				this.answerBox[this.active_box_id][this.active_box_combo][6]-this.lineThickness/2+1.5,
 				2.5+0.1*this.lineThickness);
 			this.edtDot(
 				this.context,'#cc0000',
-				this.answerBox[this.active_box_id][this.active_box_combo][5]+loc_width+this.lineThickness/2-0.5,
-				this.answerBox[this.active_box_id][this.active_box_combo][6]+loc_height+this.lineThickness/2-0.5,
+				this.answerBox[this.active_box_id][this.active_box_combo][5]+loc_width+this.lineThickness/2+1.5,
+				this.answerBox[this.active_box_id][this.active_box_combo][6]+loc_height+this.lineThickness/2+1.5,
 				2.5+0.1*this.lineThickness);
 			this.context.strokeStyle=this.currentColours[1];
 		}
@@ -1364,7 +1374,7 @@ function ql_mouseDragMove(e){
 										}
 								}
 							} else {
-								if ((this.qmode == 'edit' && this.testWithin(this.x,this.y,this.answerBox[i][j][5],this.answerBox[i][j][6],this.labelWidthEffect,this.labelHeightEffect) == true) || (this.qmode!='edit' && this.pholderBox[i][5]>220 && this.testWithin(this.x,this.y,this.pholderBox[i][5],this.pholderBox[i][6],this.labelWidthEffect,this.labelHeightEffect) == true)) {
+								if (typeof(this.pholderBox[i])!='undefined' && ((this.qmode == 'edit' && this.testWithin(this.x,this.y,this.answerBox[i][j][5],this.answerBox[i][j][6],this.labelWidthEffect,this.labelHeightEffect) == true) || (this.qmode!='edit' && this.pholderBox[i][5]>220 && this.testWithin(this.x,this.y,this.pholderBox[i][5],this.pholderBox[i][6],this.labelWidthEffect,this.labelHeightEffect) == true))) {
 									this.drag_box_id = i;
 									this.drag_pho_id = i;
 									this.drag_box_combo = 0;
