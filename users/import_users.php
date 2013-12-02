52d87f71ce1c93a4272103a8885da6d362934f99
Nikodem Miranowicz <Nikodem.Miranowicz@nottingham.ac.uk>
2013-12-02 14:54:17 +0000

    missing jquery fix

--- users/import_users.php
+++ users/import_users.php
@@ -37,6 +37,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta http-equiv="content-type" content="text/html;charset=<?php echo $configObject->get('cfg_page_charset') ?>" />
   <title><?php echo $string['importusers'] . " " . $configObject->get('cfg_install_type') ?></title>
+	<script type="text/javascript" src="/js/jquery-1.6.1.min.js"></script>
   <link rel="stylesheet" type="text/css" href="../css/body.css" />
   <link rel="stylesheet" type="text/css" href="../css/dialog.css" />
   <link rel="stylesheet" type="text/css" href="../css/header.css" />
