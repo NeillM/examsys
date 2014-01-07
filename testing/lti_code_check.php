019d671c7a8d8867c5226d079e59747b3629084d
Simon Atack <simon.atack@nottingham.ac.uk>
2014-01-07 14:06:56 +0000

    added help to the lti_code_check.php test page

--- testing/lti_code_check.php
+++ testing/lti_code_check.php
@@ -5,6 +5,8 @@ require_once '../classes/lti_integration.class.php';
 
 $lti_i = lti_integration::load();
 
+echo "use modcode as get parameter<br>";
+
 print $_GET['modcode'] . '<BR>';
 
 var_dump($lti_i::module_code_translate($_GET['modcode'],'TITLE DFDGDS'));
