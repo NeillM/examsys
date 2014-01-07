1dc275e2214ee6752d9374dfd586413773bd566f
Simon Atack <simon.atack@nottingham.ac.uk>
2014-01-07 11:17:45 +0000

    new testing page to check out lookup function

---
+++ testing/lookuptest.php
@@ -0,0 +1,79 @@
+<?php
+// This file is part of Rogō
+//
+// Rogō is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Rogō is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Rogō.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ *
+ * checks lookup plugin functionality
+ *
+ * @author Simon Atack
+ * @version 1.0
+ * @copyright Copyright (c) 2014 The University of Nottingham
+ * @package
+ */
+
+
+$root = str_replace('/include', '/', str_replace('\\', '/', dirname(__FILE__)));
+require_once $root . '/../include/auth.inc';
+include_once $root . '/../include/load_config.php';
+$cfg_web_root = $configObject->get('cfg_web_root');
+require_once $cfg_web_root . 'classes/configobject.class.php';
+require_once $cfg_web_root . 'classes/lang.class.php';
+require_once $cfg_web_root . 'lang/' . $language . '/include/common.inc'; // Include common language file that all scripts need
+require_once $cfg_web_root . 'include/custom_error_handler.inc';
+require_once $cfg_web_root . 'classes/dbutils.class.php';
+require_once $cfg_web_root . 'classes/userobject.class.php';
+require_once $cfg_web_root . 'classes/authentication.class.php';
+require_once $cfg_web_root . 'classes/lang.class.php';
+
+require_once $cfg_web_root . '/classes/moduleutils.class.php';
+require_once $cfg_web_root . '/classes/schoolutils.class.php';
+require_once $cfg_web_root . '/classes/usernotices.class.php';
+
+require_once '../include/admin_auth.inc';
+
+if (!$userObject->has_role('SysAdmin')) {
+  $msg = sprintf($string['furtherassistance'], $configObject->get('support_email'), $configObject->get('support_email'));
+  $notice->display_notice_and_exit($mysqli, $string['pagenotfound'], $msg, $string['accessdenied'], $configObject->get('cfg_root_path') . '/artwork/page_not_found.png', '#C00000', true, true);
+}
+
+$configObj=Config::get_instance();
+
+$lookup = Lookup::get_instance($configObj, $mysqli);
+
+//$authobj->data contains lookup info;
+$newdata = new stdClass();
+
+$newdata->$_REQUEST['fieldname'] = $_REQUEST['fieldcontent'];
+echo "use fieldname & fieldcontent<br>";
+echo "<h1>looking up $_REQUEST[fieldname] = $_REQUEST[fieldcontent]</h1>";
+
+$data = new stdClass();
+$data->lookupdata = $newdata;
+$info = $lookup->userlookup($data);
+
+echo "<h2>Info</h2>";
+var_dump($info);
+echo "<br>\r\n";
+
+$lookupdebug = $lookup->debug_as_array();
+
+echo "<pre>";
+foreach ($lookupdebug as $line) {
+  print $line ."\r\n";
+}
+
+echo "</pre>";
