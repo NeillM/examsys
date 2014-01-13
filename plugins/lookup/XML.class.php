e62caf4fb753a24be8097ba07ec4bcef4fa0bd39
Simon Atack <simon.atack@nottingham.ac.uk>
2014-01-13 11:43:10 +0000

    fix mistakes in lookupclass/plugins

--- plugins/lookup/XML.class.php
+++ plugins/lookup/XML.class.php
@@ -71,31 +71,31 @@ class XML_lookup extends outline_lookup {
 
     $restrictstop = false;
     if (!is_null($restrict)) {
-      foreach($restrict as $key => $value) {
+      foreach ($restrict as $key => $value) {
         if (!isset($lookupobj->lookupdata->$key)) {
           $this->savetodebug("Restriction stopped debug $key not found");
           $restrictstop = true;
-        }
-        if (strpos($value, '|') === false and $restrictstop != true) {
-          //condition
-          if (!isset($lookupobj->lookupdata->$key) or (isset($lookupobj->lookupdata->$key) and $lookupobj->lookupdata->$key !== $value)) {
-            $this->savetodebug("Restriction stopped debug $key !== $value");
-            $restrictstop = true;
-          }
         } else {
-          // OR condition
-          $restrictstop1 = 0;
-          $exp=explode('|',$value);
-          foreach ($exp as $value1) {
-            if ($lookupobj->lookupdata->$key === $value1) {
-              $restrictstop1++;
+          if (strpos($value, '|') === false) {
+            //condition
+            if (!isset($lookupobj->lookupdata->$key) or (isset($lookupobj->lookupdata->$key) and $lookupobj->lookupdata->$key !== $value)) {
+              $this->savetodebug("Restriction stopped debug $key !== $value");
+              $restrictstop = true;
+            }
+          } else {
+            // OR condition
+            $restrictstop1 = 0;
+            $exp = explode('|', $value);
+            foreach ($exp as $value1) {
+              if ($lookupobj->lookupdata->$key === $value1) {
+                $restrictstop1++;
+              }
+            }
+            if ($restrictstop1 == 0) {
+              $this->savetodebug("Restriction stopped debug one of the ORs of $value in the key $key");
+              $restrictstop = true;
             }
           }
-          if($restrictstop1 == 0) {
-            $this->savetodebug("Restriction stopped debug one of the ORs of $value in the key $key");
-            $restrictstop = true;
-          }
-
         }
 
       }
