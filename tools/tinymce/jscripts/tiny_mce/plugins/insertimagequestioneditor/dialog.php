17062d598775ebc5e2bd0705738b6b4bb14cfd68
Simon Wilkinson <simon.wilkinson@nottingham.ac.uk>
2014-01-30 18:20:53 +0000

    Updated toolbar icons in the editor.

--- tools/tinymce/jscripts/tiny_mce/plugins/insertimagequestioneditor/dialog.php
+++ tools/tinymce/jscripts/tiny_mce/plugins/insertimagequestioneditor/dialog.php
@@ -18,7 +18,7 @@
 *
 * @author Simon Wilkinson
 * @version 1.0
-* @copyright Copyright (c) 2013 The University of Nottingham
+* @copyright Copyright (c) 2014 The University of Nottingham
 * @package
 */
 
@@ -30,7 +30,7 @@ require $include_path . 'staff_auth.inc';
 
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html>
 <html>
 <head>
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
