862bef4174b1b83f482271378783088c68e1a1e9
Adrian Fish <adrian.r.fish@gmail.com>
2014-03-06 15:06:23 +0000

    [1377] Set authn debug to false by default.

--- updates/version5/201401101555_add_debug_variables_to_config.php
+++ updates/version5/201401101555_add_debug_variables_to_config.php
@@ -2,7 +2,7 @@
 
 // Your code here
 //  public function add_line($string, $search, $new_lines, $default_line, $cfg_web_root, $target_line = '', $offset = 1) {
-$new_lines = array("\r\n  \$display_auth_debug = true; // set this to deisplay debug on failed authentication\r\n\r\n\$displayerrors = false;  // overrides settings in php for errors not to be shown to screen (true enables)\r\n\r\n  \$displayallerrors = false; // display/logs any error the system has including notices (true enables)\r\n\r\n  \$errorshutdownhandling=true; //enables log at shutdown (allows you to catch reasons behind fatal errors etc including mysqli errors (true enables)\r\n\r\n  \$errorcontexthandling = 'improved'; //improved gives a good capture of context variables while filtering for security of display/saved data, basic captures all but doesnt run and security routines, none doesnt capture any context variables\r\n");
+$new_lines = array("\r\n  \$display_auth_debug = false; // set this to deisplay debug on failed authentication\r\n\r\n\$displayerrors = false;  // overrides settings in php for errors not to be shown to screen (true enables)\r\n\r\n  \$displayallerrors = false; // display/logs any error the system has including notices (true enables)\r\n\r\n  \$errorshutdownhandling=true; //enables log at shutdown (allows you to catch reasons behind fatal errors etc including mysqli errors (true enables)\r\n\r\n  \$errorcontexthandling = 'improved'; //improved gives a good capture of context variables while filtering for security of display/saved data, basic captures all but doesnt run and security routines, none doesnt capture any context variables\r\n");
 $target_line = '$dbclass ';
 $updater_utils->add_line($string, '$displayerrors', $new_lines, 99, $cfg_web_root, $target_line, 1);
 
