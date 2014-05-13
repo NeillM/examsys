bf882799ae6d1b33113bd4fdcaf19fcd935a6fc6
Rob Ingram <rob@headcrash.org>
2014-05-14 11:46:41 +1200

    Display marking data in Exam Script for staff and admins

--- lang/pl/reports/textbox_marking.php
+++ lang/pl/reports/textbox_marking.php
@@ -33,5 +33,5 @@ $string['next'] = 'Next';
 $string['previous'] = 'Previous';
 $string['finish'] = 'Finish';
 $string['mark_progress'] = '%d of %d';
-$string['answer_saved'] = 'Answer saved';
+$string['answer_saved'] = 'Marks saved';
 ?>
