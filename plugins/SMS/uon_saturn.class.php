1188b4a1442e49afb0173c84f0fa2b39eff3f869
Simon Wilkinson <simon.wilkinson@nottingham.ac.uk>
2014-05-23 15:26:01 +0100

    Adding SysAdmin manu SMS import capability.

--- plugins/SMS/uon_saturn.class.php
+++ plugins/SMS/uon_saturn.class.php
@@ -239,7 +239,7 @@ Class UON_SATURN extends SmsUtils {
 
     // Look up SMS
     $returned_data = @file_get_contents($sms_api . "&code=$replaced_module&year=" . $session_parts[0]);
-    var_dump($returned_data);
+    var_dump($session);
     $xml = false;
     if ($returned_data !== false) {
       $xml = new SimpleXMLElement($returned_data);
