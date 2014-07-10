bd16759f4cf990d0b81dc464b5de454bd594a7b5
Simon Wilkinson <simon.wilkinson@nottingham.ac.uk>
2014-07-10 10:30:00 +0100

    Take char map functionality off TinyMCE (does not work with Latin1 database).

--- tools/tinymce/jscripts/tiny_mce/tiny_config_externals_email.js
+++ tools/tinymce/jscripts/tiny_mce/tiny_config_externals_email.js
@@ -10,7 +10,7 @@ tinyMCE.init({
     skin : "default",
     plugins : "table,visualchars,nonbreaking,xhtmlxtras,paste",
     // Theme options
-    theme_advanced_buttons1 : "cut,copy,paste,|,undo,|,bold,italic,underline,|,sub,sup,|,justifyleft,justifycenter,justifyright,|,numlist,bullist,|,tablecontrols,|,code", 
+    theme_advanced_buttons1 : "cut,copy,paste,|,undo,|,bold,italic,underline,|,sub,sup,|,justifyleft,justifycenter,justifyright,justifyfull,|,numlist,bullist,|,tablecontrols,|,code", 
     theme_advanced_buttons2 : "", 
     theme_advanced_buttons3 : "",
     theme_advanced_toolbar_location : "top", 
