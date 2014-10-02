4076ff1fd58cebbd888667afd2ce56cc8ef4ec5d
Simon <simon.wilkinson@nottingham.ac.uk>
2014-10-02 23:06:18 +0100

    Fix for Solaris.

--- admin/system_info.php
+++ admin/system_info.php
@@ -288,21 +288,6 @@ if ($e6 == 'improved') {
 
   echo '<tr><td colspan="2" rowspan="18" valign="top" align="left"><table cellspacing="0" cellpadding="2" border="0" style="font-size:90%">';
 
-  function format_space($bytes) {
-    $space = '';
-    
-    $num = $bytes / 1073741824;
-    
-    if ($num > 1024) {
-      $num = $bytes / 1099511627776;
-      $space = round($num, 2) . ' TB';
-    } else {
-      $space = round($num) . ' GB';
-    }
-    
-    return $space;
-  }
-  
   if (php_uname('s') == 'Windows NT') {
     $disks = `fsutil fsinfo drives`;
     $disks = str_word_count($disks,1);
@@ -310,10 +295,8 @@ if ($e6 == 'improved') {
     foreach ($disks as $key=>$disk) {
       if ($disk != 'Drives') {
         $driveID = strtoupper($disk) . ':';
-        $master_array[$i][3] = format_space(@disk_free_space($driveID));
-        $master_array[$i][1] = format_space(@disk_total_space($driveID));
-        $master_array[$i]['free'] = round(@disk_free_space($driveID) / 1073741824);
-        $master_array[$i]['total'] = round(@disk_total_space($driveID) / 1073741824);
+        $master_array[$i][3] = round(@disk_free_space($driveID) / 1073741824) . 'G';
+        $master_array[$i][1] = round(@disk_total_space($driveID) / 1073741824) . 'G';
         $master_array[$i][5] = $disk . ':';
       }
       $i++;
@@ -327,7 +310,7 @@ if ($e6 == 'improved') {
     foreach ($lines as $individual_line) {
       if ($row_no > 0) {
         $cols = explode(' ', $individual_line);
-        foreach($cols as $individual_col) {
+        foreach ($cols as $individual_col) {
           if ($individual_col != '') {
             $master_array[$row_no][] = $individual_col;
           }
@@ -339,18 +322,16 @@ if ($e6 == 'improved') {
   for ($i=1; $i<($row_no-1);$i++) {
     if ($master_array[$i][5] != '' and $master_array[$i][1] != '0K') {
       echo '<tr><td><img src="../artwork/drive_icon.png" width="48" height="48" alt="' . $string['driveicon'] . '" /></td><td>' . $master_array[$i][5] . '<br />';
-      echo '<span style="border: 1px solid #808080; display:block; height:11px; width:150px">';
-      if (intval($master_array[$i]['free']) < intval($master_array[$i]['total'])) {
+      echo '<span style="border: 1px solid #808080; display:block; height:11px; background-color:#EAEAEA; width:150px">';
+      if (intval($master_array[$i][3]) < intval($master_array[$i][1])) {
           
-	$bar_width = round((1 - (intval($master_array[$i]['free']) / intval($master_array[$i]['total']))) * 148);
+	$bar_width = round((1 - (intval($master_array[$i][3]) / intval($master_array[$i][1]))) * 148);
 
-        if ((intval($master_array[$i]['total']) - intval($master_array[$i]['free'])) > (intval($master_array[$i]['total']) * 0.9)) {
+        if ((intval($master_array[$i][3]) - intval($master_array[$i][1])) > (intval($master_array[$i][3]) * 0.9)) {
           echo '<span style="display:block; height:11px; width:' . $bar_width . 'px; background-color:#DA2626"></span>';  // Red bar
         } else {
           echo '<span style="display:block; height:11px; width:' . $bar_width . 'px; background-color:#26A0DA"></span>';  // Blue bar
         }
-      } else {
-        echo '<img src="blank_bar.png" width="20" height="11" />';
       }
       echo '</span><span style="color:#808080">' . sprintf($string['freespace'], $master_array[$i][3], $master_array[$i][1]) . '</span></td></tr>';
     }
