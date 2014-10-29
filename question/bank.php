fe1a3b1718993e4fccefd0bbb84364ac2e28b87e
Simon Wilkinson <simon.wilkinson@nottingham.ac.uk>
2014-10-29 17:21:28 +0000

    Fix for unassigned questions in the Question Bank.

--- question/bank.php
+++ question/bank.php
@@ -46,13 +46,13 @@ $type = check_var('type', 'GET', true, false, true);
 $module = check_var('module', 'GET', true, false, true);
 
 if ($module == 0) {
-  $module_details['moduleid'] = 0;
+  $module_details['moduleid'] = 'Unassigned';
   $module_details['active'] = 1;
 } else {
   $module_details = module_utils::get_full_details_by_ID($module, $mysqli);
 }
 
-if (strpos($module_details['checklist'], 'mapping') === false and $_GET['type'] == 'objective') {
+if ($module != 0 and strpos($module_details['checklist'], 'mapping') === false and $_GET['type'] == 'objective') {
   $msg = sprintf($string['furtherassistance'], $configObject->get('support_email'), $configObject->get('support_email'));
   $notice->display_notice_and_exit($mysqli, $string['pagenotfound'], $msg, $string['pagenotfound'], '../artwork/page_not_found.png', '#C00000', true, true);
 }
