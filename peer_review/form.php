88e222299e2a132ffa27a83911626ef59f03d494
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2014-11-17 16:17:22 +0000

    rogo 6 bug fix: ROGO-435

--- peer_review/form.php
+++ peer_review/form.php
@@ -278,7 +278,7 @@ if (isset($_POST['submit'] )) {
     p {margin:0; padding:0}
     .paper {padding-left:5px; font-size:150%; color:white; font-weight:bold}
     .group {padding-left:5px; color:white}
-    .title {font-size:130%; font-weight:bold; color:<?php echo $themecolor; ?>; border-top:1px solid #C0C0C0}
+    .title {font-size:130%; font-weight:bold; color:<?php echo $paper_themecolor; ?>; border-top:1px solid #C0C0C0}
     .col {text-align:center; color:<?php echo $labelcolor; ?>}
     .phototd {vertical-align:top; border-top:1px solid #C0C0C0}
     .photo {background-color:white; border-left: 1px solid #F1F1F1; border-top: 1px solid #F1F1F1; box-shadow: 2px 2px 4px #808080; padding:10px; margin-right:10px}
@@ -329,7 +329,7 @@ if (isset($_POST['submit'] )) {
   td p {margin:0}
   .paper {padding-left:5px; font-size:150%; color:white; font-weight:bold}
   .group {padding-left:5px; color:white}
-  .title {font-size:130%; font-weight:bold; color:<?php echo $themecolor; ?>; border-top:1px solid #C0C0C0}
+  .title {font-size:130%; font-weight:bold; color:<?php echo $paper_themecolor; ?>; border-top:1px solid #C0C0C0}
   .col {text-align:center; color:<?php echo $labelcolor; ?>}
   .phototd {vertical-align:top; border-top:1px solid #C0C0C0}
   .photo {background-color:white; border-left: 1px solid #F1F1F1; border-top: 1px solid #F1F1F1; box-shadow: 2px 2px 4px #808080; padding:10px; margin-right:10px}
