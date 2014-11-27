852f1df04226b0b24124f1df40ed8d24c96f74d0
Simon Wilkinson <simon.wilkinson@nottingham.ac.uk>
2014-11-27 09:56:25 +0000

    Language fixes.

--- lang/cs/paper/user_index.php
+++ lang/cs/paper/user_index.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Rogō.  If not, see <http://www.gnu.org/licenses/>.
 
-require_once './lang/' . $language . '/include/paper_security.inc';
+require_once '../lang/' . $language . '/include/paper_security.inc';
 
 $string['startscreen'] = 'Úvodní obrazovka';
 $string['availability'] = 'Dostupnost';
