e0dd480df5c98ee0f7d408a949393004e014d59c
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2015-02-16 14:03:13 +0000

    ROGO-552 coding standards

--- updates/version5/201502161220_null_sms_imports.php
+++ updates/version5/201502161220_null_sms_imports.php
@@ -1,14 +1,14 @@
 <?php
 
 // Set deletions count to zero where null.
-if ($updater_utils->count_rows("SELECT * from sms_imports where deletions is NULL") > 0) {
-    $sql = "update sms_imports set deletions = 0 where deletions is NULL";
+if ($updater_utils->count_rows("SELECT * FROM sms_imports WHERE deletions IS NULL") > 0) {
+    $sql = "UPDATE sms_imports SET deletions = 0 WHERE deletions IS NULL";
     $updater_utils->execute_query($sql, true);
 }
 
 // Set enrolements count to zero where null.
-if ($updater_utils->count_rows("SELECT * from sms_imports where enrolements is NULL") > 0) {
-    $sql = "update sms_imports set enrolements = 0 where enrolements is NULL";
+if ($updater_utils->count_rows("SELECT * FROM sms_imports WHERE enrolements IS NULL") > 0) {
+    $sql = "UPDATE sms_imports SET enrolements = 0 WHERE enrolements IS NULL";
     $updater_utils->execute_query($sql, true);
 }
 ?>
