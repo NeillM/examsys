daaf030a400c550e89fedff35134dd7413487783
Joseph Baxter <joseph.lee.baxter@gmail.com>
2015-02-20 11:53:37 +0000

    ROGO-649 - simplify update script

--- updates/version5/201502201115_keyword_loop.php
+++ updates/version5/201502201115_keyword_loop.php
@@ -1,26 +1,11 @@
 <?php
 
-$mysqli->autocommit(false);
-
 // Delete any keywords associate with keyword based questions - ROGO-649
 if (!$updater_utils->has_updated('keyword_loop')) {
 
-	// Get keyword based questions that have an associated keyword.
-	$select = $mysqli->prepare("select keywords_question.q_id, keywordID from keywords_question, questions where q_type = 'keyword_based' and keywords_question.q_id = questions.q_id");
-	$select->execute();
-	$select->bind_result($qid, $keywordID);
-	
-	// Delete entries.
-	$delete = $mysqli->prepare("delete from keywords_question where q_id = ? and keywordID = ?)";
-	while ($select->fetch()) {
-		$delete->bind_param('ii', $qid, $keywordID);
-		$delete->execute();
-	}
-	
-	// Commit all deletes.
-	$mysqli->commit();
+	$delete = $mysqli->prepare("DELETE keywords_question FROM keywords_question, questions WHERE q_type = 'keyword_based' AND keywords_question.q_id = questions.q_id";
+	$delete->execute();
 	$delete->close();
-	$select->close();
 	
 	$updater_utils->record_update('keyword_loop');
 }
