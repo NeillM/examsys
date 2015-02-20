a7af4a69fb0bc80420b13c67b68f9a4cd9542dfd
Joseph Baxter <joseph.lee.baxter@gmail.com>
2015-02-20 14:06:40 +0000

    ROGO-649 missed end bracket

--- updates/version5/201502201115_keyword_loop.php
+++ updates/version5/201502201115_keyword_loop.php
@@ -3,7 +3,7 @@
 // Delete any keywords associate with keyword based questions - ROGO-649
 if (!$updater_utils->has_updated('keyword_loop')) {
 
-	$delete = $mysqli->prepare("DELETE keywords_question FROM keywords_question, questions WHERE q_type = 'keyword_based' AND keywords_question.q_id = questions.q_id";
+	$delete = $mysqli->prepare("DELETE keywords_question FROM keywords_question, questions WHERE q_type = 'keyword_based' AND keywords_question.q_id = questions.q_id");
 	$delete->execute();
 	$delete->close();
 	
