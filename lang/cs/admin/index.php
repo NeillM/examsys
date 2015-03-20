9ff56aa42eb323fe13504db83ad183903d779972
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2015-03-20 10:01:29 +0000

    ROGO-1485 lang files fixes

--- lang/cs/admin/index.php
+++ lang/cs/admin/index.php
@@ -41,6 +41,6 @@ $string['imslti'] = 'Klíče LTI';
 $string['questionstatuses'] = 'Statusy úloh';
 $string['savefailattempts'] = 'Uložit nezdařené pokusy';
 $string['testing'] = 'Testování systémů';
-$string['bitbucket'] = 'Hlášení chyb';
+$string['bug'] = 'Hlášení chyb';
 
 ?>
