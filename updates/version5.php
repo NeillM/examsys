ff65221c137f1d49b1bd5c8d130598cb8321fecc
Neill Magill <neill.magill@nottingham.ac.uk>
2015-07-21 14:09:17 +0100

    Autoload InstallUtils class
    Note it is not autoloaded in install/index.php as that does not include the load_config.php file

--- updates/version5.php
+++ updates/version5.php
@@ -24,7 +24,6 @@
  */
 
 require_once '../include/load_config.php';
-require_once '../classes/installutils.class.php';
 require_once '../classes/updaterutils.class.php';
 require_once '../include/auth.inc';
 require_once '../classes/lang.class.php';
