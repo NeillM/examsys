b576f2ea5ceccf21578292ab9ffa9676b629c847
Neill Magill <neill.magill@nottingham.ac.uk>
2015-07-29 15:25:49 +0100

    The Authentication class cannot be autoloaded as it's file contains defines.

--- testing/lookuptest.php
+++ testing/lookuptest.php
@@ -50,6 +50,7 @@ $cfg_web_root = $configObject->get('cfg_web_root');
 require_once $cfg_web_root . 'classes/configobject.class.php';
 require_once $cfg_web_root . 'lang/' . $language . '/include/common.inc'; // Include common language file that all scripts need
 require_once $cfg_web_root . 'include/custom_error_handler.inc';
+require_once $cfg_web_root . 'classes/authentication.class.php';
 
 require_once '../include/admin_auth.inc';
 
