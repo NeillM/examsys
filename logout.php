0fb408343c5b9c96ba4852f5d59249d16ab577ad
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2015-08-04 15:42:27 +0100

    ROGO-1498 only delete cookie if it exists

--- logout.php
+++ logout.php
@@ -35,7 +35,9 @@ $return = session_start();
 session_unset();
 session_destroy();
 session_write_close();
-setcookie('paperpwd', '', -1, '/');
+if (isset($_COOKIE['paperpwd'])) {
+    setcookie('paperpwd', '', -1, '/');
+}
 setcookie(session_name(), '', 0, '/');
 session_regenerate_id(true);
 
