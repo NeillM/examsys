dda2140fc957ea72b51d39cdd9c1d4c13f8a7a35
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2015-08-14 12:07:32 +0100

    encrypt existing passwords

--- updates/version5/201508041630_paperpasswords.php
+++ updates/version5/201508041630_paperpasswords.php
@@ -5,5 +5,21 @@ if (!$updater_utils->has_updated('rogo1477_paperpassword')) {
     $altersql = "ALTER TABLE properties CHANGE password password char(90) default NULL";
     $updater_utils->execute_query($altersql, true);
     
+    // Encrypt existing paper passwords.
+    $select = $mysqli->prepare("SELECT property_id, password FROM properties WHERE password != ''");
+    $select->execute();
+    $select->store_result();
+    $select->bind_result($property_id, $password);
+    
+    $salt = UserUtils::get_salt();
+    while ($select->fetch()) {
+        $encpassword = encpw($salt, $property_id, $property_id . $password);
+        $updatesql = $mysqli->prepare ("UPDATE properties set password = ? WHERE property_id = ?");
+        $updatesql->bind_param('si', $encpassword, $property_id);
+        $updatesql->execute();
+        $updatesql->close();
+    }
+    $select->close();
+    
     $updater_utils->record_update('rogo1477_paperpassword');
 }
