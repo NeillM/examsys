b4f096b186f1756719fef66ec032a3316e9e908f
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2015-10-21 15:08:42 +0100

    ROGO-1559 removed other gradebook related changes

--- classes/paperutils.class.php
+++ classes/paperutils.class.php
@@ -424,12 +424,7 @@ Class PaperUtils {
     $update = $db->prepare("UPDATE properties SET deleted = NOW(), paper_ownerID = -1 WHERE property_id = ?");
     $update->bind_param('i', $paperID);
     $update->execute();
-    $update->close();
-    if ($db->errno != 0) {
-      return false;
-    }
-    return true;
-    
+    $update->close(); 
   }
 
   public function type_to_name($type, $string) {
