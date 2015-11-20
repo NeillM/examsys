5fc7253ff9365a7a03221befb7285bc26c9c76bc
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2015-11-20 10:45:57 +0000

    ROGO-1582 added composite index

--- classes/databasetables.class.php
+++ classes/databasetables.class.php
@@ -905,7 +905,8 @@ QUERY;
           KEY `paper_owner` (`paper_ownerID`),
           KEY `question_type` (`paper_type`),
           KEY `crypt_name_idx` (`crypt_name`),
-          KEY `idx_owner_deleted` (`paper_ownerID`,`deleted`)
+          KEY `idx_owner_deleted` (`paper_ownerID`,`deleted`),
+          KEY `date_idx` (`start_date`, `end_date`)
         ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET={$charset}
 QUERY;
 
