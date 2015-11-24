084ea1d08f24d20fb153f65e56a1f183cc0d3b1d
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2015-11-24 10:31:10 +0000

    ROGO-1559 should also check on import submit that grades have not been published

--- import/osce_marks.php
+++ import/osce_marks.php
@@ -215,7 +215,9 @@ function marks_from_file($notice, $userObj, $paperID, $fileName, $db, $string) {
   echo "</ol>\n";
 }
 
-if (isset($_POST['submit'])) {
+$gradebook = new gradebook($mysqli);
+$graded = $gradebook->paper_graded($paperID);
+if (!$graded and isset($_POST['submit'])) {
   if ($_FILES['csvfile']['name'] != 'none' and $_FILES['csvfile']['name'] != '') {
     if (!move_uploaded_file($_FILES['csvfile']['tmp_name'],  $configObject->get('cfg_tmpdir') . $userObject->get_user_ID() . "_osce_marks.csv"))  {
       echo uploadError($_FILES['csvfile']['error']);
