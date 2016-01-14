044b8900f750ab7e288ec0ca64e36b56d15fa060
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2016-01-14 10:29:41 +0000

    ROGO-1607 handle html response

--- paper/start.php
+++ paper/start.php
@@ -707,7 +707,7 @@ if ($propertyObj->get_paper_type() != '5') { // Do not allow saving for offline 
               } else {
                 // Status is the response code and errorThrown is the HTTP response text.
                 if (errorThrown != '') {
-                    saveFail(textStatus + ': ' + xhr.status + ': ' + errorThrown, this.url, '');
+                    saveFail(textStatus + ': ' + xhr.status, this.url, errorThrown);
                     return;
                 }
               }
@@ -728,7 +728,17 @@ if ($propertyObj->get_paper_type() != '5') { // Do not allow saving for offline 
               return;
             } else {
               // marking_funcions.inc record_marks failed after retry.
-              saveFail('record_marks', this.url, ret_data);
+              if (ret_data == 'ERROR' || Number.isInteger(ret_data)) {
+                saveFail('record_marks', this.url, ret_data);
+              } else {
+                  htmlstart = ret_data.indexOf("<title>") + 7; 
+                  htmlend = ret_data.indexOf("</title>"); 
+                  htmltitle = ret_data.substring(htmlstart, htmlend);
+                  if (htmltitle == '') {
+                      htmltitle = 'html response';
+                  }
+                  saveFail('record_marks', this.url, htmltitle);
+              }
               return;
             }
           },
