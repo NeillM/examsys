24637c69298edfda4baf9894d097379e1c3e4dc8
Yijun xue <Yijun.Xue@nottingham,.ac.uk>
2016-01-14 10:01:02 +0000

    correct phpdoc for display_notice_and_exit()

--- classes/user_notices.class.php
+++ classes/user_notices.class.php
@@ -96,6 +96,7 @@ Class user_notices extends RogoStaticSingleton {
   /**
    * This function will output a message to the user and exit php;
    *
+   * @param mysqli $mysqli      - gloable database object
    * @param string $title       - string title to display
    * @param string $msg         - string the message displayed on screen
    * @param string $reason      - string the message displayed in the database
