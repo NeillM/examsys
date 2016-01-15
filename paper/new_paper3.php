032d33d7fd3768659fc40b59b126df72f7b3cc5b
Yijun xue <Yijun.Xue@nottingham,.ac.uk>
2016-01-15 10:09:52 +0000

    remove zzzz rubbish

--- paper/new_paper3.php
+++ paper/new_paper3.php
@@ -40,7 +40,7 @@ $papertype = $assessment->get_type_value($paper_type);
 if ($papertype === false) {
     $errorline = __LINE__ - 2;
     $msg = __FILE__ . " Line: " . $errorline . " Error:" . $string['papertypenotfound'];
-    $notice->display_notice_and_exit($mysqli, "zzzz $papertype " . $string['papertypenotfound'], $string['papertypenotfound'], $msg, '../artwork/page_not_found.png', '#C00000', true, true);
+    $notice->display_notice_and_exit($mysqli, " $paper_type " . $string['papertypenotfound'], $string['papertypenotfound'], $msg, '../artwork/page_not_found.png', '#C00000', true, true);
 }
 // Process the posted modules
 $modules = array();
