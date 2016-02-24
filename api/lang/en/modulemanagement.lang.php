fc04524de11840eddb497a1795b62da09dffb408
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2016-02-24 16:40:57 +0000

    improvement session should be a required parameter

--- api/lang/en/modulemanagement.lang.php
+++ api/lang/en/modulemanagement.lang.php
@@ -24,3 +24,4 @@ $string['module_not_created'] = 'Module not created';
 $string['module_not_deleted'] = 'Module not deleted';
 $string['module_not_deleted_inuse'] = 'Module not deleted, as linked to a paper or enrolement';
 $string['user_already_enrolled'] = 'User already enrolled';
+$string['session_not_supplied'] = 'Session not supplied';
