92e7bc61eb65736f143da65a6c504a686d0917dd
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2016-02-24 11:07:41 +0000

    refactor of unit tests into test per logical function

--- testing/unittest/tests/api/coursemanagementTest.php
+++ testing/unittest/tests/api/coursemanagementTest.php
@@ -26,6 +26,76 @@ use testing\unittest\unittestdatabase;
  */
 class coursemanagementtest extends unittestdatabase {
     /**
+     * Create a response array for creation
+     * @return array the resposne array  
+     */
+    private function create_response_array() {
+        return array(
+            "statuscode" => 100,
+            "status" => 'OK',
+            "id" => 3,
+            "error" => null,
+            "node" => 'create',
+            "nodeid" => 1);
+    }
+    /**
+     * Create a parameter array for creation
+     * @return array the param array  
+     */
+    private function create_param_array() {
+        return array(
+            "nodeid" => 1,
+            "name" => 'CREATE',
+            "description" => 'Create test',
+            "school" => 'School test',
+            "faculty" => 'Faculty test');
+    }
+    /**
+     * Create a parameter array for updates
+     * @return array the param array  
+     */
+    private function update_param_array() {
+        return array(
+            "nodeid" => 1,
+            "id" => 1,
+            "description" => 'Test course update');
+    }
+    /**
+     * Create a response array for updates
+     * @return array the resposne array  
+     */
+    private function update_response_array() {
+        return array(
+            "statuscode" => 100,
+            "status" => 'OK',
+            "id" => 1,
+            "error" => null,
+            "node" => 'create',
+            "nodeid" => 1);
+    }
+    /**
+     * Create a response array for deletion
+     * @return array the response array  
+     */
+    private function delete_response_array() {
+        return array(
+            "statuscode" => 100,
+            "status" => 'OK',
+            "id" => 1,
+            "error" => null,
+            "node" => 'delete',
+            "nodeid" => 1);
+    }
+    /**
+     * Create a parameter array for deletion
+     * @return array the param array  
+     */
+    private function delete_param_array() {
+        return array(
+            "nodeid" => 1,
+            "id" => 1);
+    }
+    /**
      * Get init data set from yml
      * @return dataset
      */
@@ -41,152 +111,132 @@ class coursemanagementtest extends unittestdatabase {
         return new PHPUnit_Extensions_Database_DataSet_YamlDataSet($this->get_base_fixture_directory() . "api" . DIRECTORY_SEPARATOR .  "coursemanagementTest" . DIRECTORY_SEPARATOR . $name . ".yml");
     }
     /**
-     * Test course create
+     * Test successful course create
      * @group api
      */
-    public function test_create() {
-        $course = new \api\coursemanagement($this->db);
-        $userid = 1;
+    public function test_create_success() {
         // Test course creation - SUCCESS
-        $responsearray = array(
-            "statuscode" => 100,
-            "status" => 'OK',
-            "id" => 3,
-            "error" => null,
-            "node" => 'create',
-            "nodeid" => 1);
-        $params = array(
-            "nodeid" => 1,
-            "name" => 'CREATE',
-            "description" => 'Create test',
-            "school" => 'School test',
-            "faculty" => 'Faculty test');
+        $responsearray = $this->create_response_array();
+        $params = $this->create_param_array();
+        $course = new \api\coursemanagement($this->db);
+        $userid = 1;
         $this->assertEquals($responsearray, $course->create($params, $userid));
+    }
+    /**
+     * Test successful course create - no faculty supplied
+     * @group api
+     */
+    public function test_create_nofaculty() {
         // Test course creation - SUCCESS (not supplying faculty)
-        $responsearray = array(
-            "statuscode" => 100,
-            "status" => 'OK',
-            "id" => 4,
-            "error" => null,
-            "node" => 'create',
-            "nodeid" => 2);
+        $responsearray = $this->create_response_array();
+        $course = new \api\coursemanagement($this->db);
+        $userid = 1;
         $params = array(
-            "nodeid" => 2,
-            "name" => 'CREATE2',
-            "description" => 'Create test 2',
-            "school" => 'School test');
+            "nodeid" => 1,
+            "name" => 'CREATE',
+            "description" => 'Create test',
+            "school" => 'Test school');
         $this->assertEquals($responsearray, $course->create($params, $userid));
+    }
+    /**
+     * Test course create exception course exists
+     * @group api
+     */
+    public function test_create_exception_course() {
         // Test course creation - ERROR course already exists
-        $responsearray = array(
-            "statuscode" => 306,
-            "status" => 'Course already exists',
-            "id" => 3,
-            "error" => null,
-            "node" => 'create',
-            "nodeid" => 3);
-        $params = array(
-            "nodeid" => 3,
-            "name" => 'CREATE',
-            "description" => 'Create test',
-            "school" => 'School test',
-            "faculty" => 'Faculty test');
-        $this->assertEquals($responsearray, $course->create($params, $userid));
-         // Test course creation - ERROR invalid faculty
-        $responsearray = array(
-            "statuscode" => 303,
-            "status" => 'Faculty not supplied',
-            "id" => null,
-            "error" => null,
-            "node" => 'create',
-            "nodeid" => 4);
-        $params = array(
-            "nodeid" => 4,
-            "name" => 'CREATE 3',
-            "description" => 'Create test 3',
-            "school" => 'School test 2');
+        $responsearray = $this->create_response_array();
+        $params = $this->create_param_array();
+        $course = new \api\coursemanagement($this->db);
+        $userid = 1;
+        $responsearray['statuscode'] = 306;
+        $responsearray['status'] = 'Course already exists';
+        $responsearray['id'] = 1;
+        $params['name'] = "TEST";
         $this->assertEquals($responsearray, $course->create($params, $userid));
     }
     /**
-     * Test course update
+     * Test course create exception invalid faculty
      * @group api
      */
-    public function test_update() {
-        // Init course.
+    public function test_create_exception_faculty() {
+        // Test course creation - ERROR invalid faculty
+        $responsearray = $this->create_response_array();
         $course = new \api\coursemanagement($this->db);
         $userid = 1;
+        $responsearray['statuscode'] = 303;
+        $responsearray['status'] = 'Faculty not supplied';
+        $responsearray['id'] = null;
         $params = array(
             "nodeid" => 1,
             "name" => 'CREATE',
             "description" => 'Create test',
-            "school" => 'School test',
-            "faculty" => 'Faculty test');
-        $course->create($params, $userid);
+            "school" => 'School test invalid');
+        $this->assertEquals($responsearray, $course->create($params, $userid));
+    }
+    /**
+     * Test successful course update
+     * @group api
+     */
+    public function test_update_success() {
         // Test course update - SUCCESS
-        $responsearray = array(
-            "statuscode" => 100,
-            "status" => 'OK',
-            "id" => 1,
-            "error" => null,
-            "node" => 'create',
-            "nodeid" => 1);
-        $params = array(
-            "nodeid" => 1,
-            "id" => 1,
-            "name" => 'CREATE2',
-            "description" => 'Create test 2');
-        $this->assertEquals($responsearray, $course->create($params, $userid));
-        // Test course uddate - ERROR course does not exist
-        $responsearray = array(
-            "statuscode" => 301,
-            "status" => 'Course does not exist',
-            "id" => null,
-            "error" => null,
-            "node" => 'create',
-            "nodeid" => 2);
-        $params = array(
-            "nodeid" => 2,
-            "id" => 100,
-            "name" => 'CREATE',
-            "description" => 'Create test');
+        $responsearray = $this->update_response_array();
+        $params = $this->update_param_array();
+        $course = new \api\coursemanagement($this->db);
+        $userid = 1;
+        $course->create($params, $userid);
         $this->assertEquals($responsearray, $course->create($params, $userid));
     }
     /**
-     * Test course deletion
+     * Test course update exception invalid course
      * @group api
      */
-    public function test_delete() {
+    public function test_update_exception_course() {
+        // Test course uddate - ERROR course does not exist
+        $responsearray = $this->update_response_array();
+        $params = $this->update_param_array();
         $course = new \api\coursemanagement($this->db);
         $userid = 1;
+        $responsearray['statuscode'] = 301;
+        $responsearray['status'] = 'Course does not exist';
+        $responsearray['id'] = null;
+        $params['id'] = 100;
+        $this->assertEquals($responsearray, $course->create($params, $userid));
+    }
+    /**
+     * Test successful course deletion
+     * @group api
+     */
+    public function test_delete_success() {
         // Test course deletion - SUCCESS.
-        $responsearray = array(
-            "statuscode" => 100,
-            "status" => 'OK',
-            "id" => 1,
-            "error" => null,
-            "node" => 'delete',
-            "nodeid" => 1);
-        $params = array(
-            "nodeid" => 1,
-            "id" => 1);
+        $responsearray = $this->delete_response_array();
+        $params = $this->delete_param_array();
+        $course = new \api\coursemanagement($this->db);
+        $userid = 1;
         $this->assertEquals($responsearray, $course->delete($params, $userid));
         // Check that the remaining courses are correct, when we delete a course we actually just add a timestamp to the table
         // which makes creating a fixture to check against difficult so doing this instead
         $querytable = $this->getConnection()->createQueryTable('courses', 'SELECT id, name, description, schoolid FROM courses WHERE deleted is NULL');
         $expectedtable = $this->get_expected_data_set('deletecourse')->getTable("courses");  
         $this->assertTablesEqual($expectedtable, $querytable);
-        // Test deleting a non existance cuoprse.
+    }
+    /**
+     * Test successful course deletion
+     * @group api
+     */
+    public function test_delete_exception_course() {
+        // Test deleting a non existance course.
+        $responsearray = $this->delete_response_array();
+        $params = $this->delete_param_array();
+        $course = new \api\coursemanagement($this->db);
+        $userid = 1;
         $responsearray['statuscode'] = 301;
         $responsearray['status'] = 'Course does not exist';
         $responsearray['id'] = null;
-        $responsearray['nodeid'] = 2;
-        $params['nodeid'] = 2;
         $params['id'] = 99;
         $this->assertEquals($responsearray, $course->delete($params, $userid));
         // Test deleting a course in use.
         $responsearray['statuscode'] = 302;
         $responsearray['status'] = 'Course not deleted, as users enrolled';
-        $responsearray['nodeid'] = 3;
-        $params['nodeid'] = 3;
         $params['id'] = 2;
         $this->assertEquals($responsearray, $course->delete($params, $userid)); 
     }
