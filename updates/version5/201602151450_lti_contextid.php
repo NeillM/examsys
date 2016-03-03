61d7d7a58b9df77edb2c6ed3ff89fe5f09149c7b
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2016-03-03 14:57:15 +0000

    ROGO-1642 default lti integration upgrade data

--- updates/version5/201602151450_lti_contextid.php
+++ updates/version5/201602151450_lti_contextid.php
@@ -4,25 +4,27 @@ if ($updater_utils->check_version("6.1.0")) {
     if (!$updater_utils->has_updated('rogo1642_lticontextid')) {
         $configObj = Config::get_instance();
         $lti_integration = $configObj->get('lti_integration');
+        // Update context ids from short codes to module ids.
         if ($lti_integration == 'UoN') {
-            // Update context ids from short codes to module ids.
             $select_sql = "SELECT c.c_internal_id, m.id FROM lti_context c, modules m WHERE
                 (m.moduleid LIKE '%\_UNNC' and SUBSTRING_INDEX(c.c_internal_id, '-CN', 1) = SUBSTRING_INDEX (m.moduleid, '_', 1))
                 OR (m.moduleid LIKE '%\_UNMC' and SUBSTRING_INDEX(c.c_internal_id, '-MY', 1) = SUBSTRING_INDEX (m.moduleid, '_', 1))
                 OR (SUBSTRING_INDEX(c.c_internal_id, '-UK', 1) = m.moduleid)";
-            $modules = $mysqli->prepare($select_sql);
-            $modules->execute();
-            $modules->store_result();
-            $modules->bind_result($lti, $id);
-            while ($modules->fetch()) {
-                $update_sql = "UPDATE lti_context SET c_internal_id = ? WHERE c_internal_id = ?";
-                $update = $mysqli->prepare($update_sql);
-                $update->bind_param('ss', $id, $lti);
-                $update->execute();
-                $update->close();
-            }
-            $modules->close();
+        } else {
+            $select_sql = "SELECT c.c_internal_id, m.id FROM lti_context c, modules m WHERE c.c_internal_id = m.moduleid";
         }
+        $modules = $mysqli->prepare($select_sql);
+        $modules->execute();
+        $modules->store_result();
+        $modules->bind_result($lti, $id);
+        while ($modules->fetch()) {
+            $update_sql = "UPDATE lti_context SET c_internal_id = ? WHERE c_internal_id = ?";
+            $update = $mysqli->prepare($update_sql);
+            $update->bind_param('ss', $id, $lti);
+            $update->execute();
+            $update->close();
+        }
+        $modules->close();
         // Add alter context id to be integer
         $altersql = "ALTER TABLE lti_context MODIFY COLUMN `c_internal_id` int(11) NOT NULL";
         $updater_utils->execute_query($altersql, true);
