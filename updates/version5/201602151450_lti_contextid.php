70262e9ffb68605169b6b787e2fac1e435c39c43
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2016-03-04 14:07:21 +0000

    ROGO-1642 updated upgrade script to delete metamodule context links as unrequired

--- updates/version5/201602151450_lti_contextid.php
+++ updates/version5/201602151450_lti_contextid.php
@@ -6,14 +6,14 @@ if ($updater_utils->check_version("6.1.0")) {
         $lti_integration = $configObj->get('lti_integration');
         // Update context ids from short codes to module ids.
         if ($lti_integration == 'UoN') {
-            // Saturn modules and fake modules accounted for.
-            $select_sql = "SELECT c.c_internal_id, m.id FROM lti_context c, modules m WHERE
-                (m.moduleid LIKE '%\_UNNC' and SUBSTRING_INDEX(c.c_internal_id, '-CN', 1) = SUBSTRING_INDEX (m.moduleid, '_', 1))
+            // Saturn modules and fake modules accounted for. Ignore meta modules.
+            $select_sql = "SELECT c.c_internal_id, m.id FROM lti_context c, modules m WHERE ((LENGTH(c.c_internal_id) - LENGTH(REPLACE(c.c_internal_id, 'N', ''))) = 1
+                AND (m.moduleid LIKE '%\_UNNC' and SUBSTRING_INDEX(c.c_internal_id, '-CN', 1) = SUBSTRING_INDEX (m.moduleid, '_', 1))
                 OR (m.moduleid LIKE '%\_UNMC' and SUBSTRING_INDEX(c.c_internal_id, '-MY', 1) = SUBSTRING_INDEX (m.moduleid, '_', 1))
-                OR (SUBSTRING_INDEX(c.c_internal_id, '-UK', 1) = m.moduleid)
+                OR (SUBSTRING_INDEX(c.c_internal_id, '-UK', 1) = m.moduleid))
                 OR ((m.moduleid LIKE '%\_UNNMC' and SUBSTRING_INDEX (m.moduleid, '_', 1) = SUBSTRING_INDEX(SUBSTRING(c_internal_id, 4), '-MY', 1))
                 OR (m.moduleid LIKE '%\_UNNC' and SUBSTRING_INDEX (m.moduleid, '_', 1) = SUBSTRING_INDEX(SUBSTRING(c_internal_id, 4), '-CN', 1))
-                OR (m.moduleid = SUBSTRING_INDEX(SUBSTRING(c_internal_id, 4), '-UK', 1)) AND c_internal_id LIKE 'ZZ-%'))";
+                OR (m.moduleid = SUBSTRING_INDEX(SUBSTRING(c_internal_id, 4), '-UK', 1)) AND c_internal_id LIKE 'ZZ-%')";
         } else {
             $select_sql = "SELECT c.c_internal_id, m.id FROM lti_context c, modules m WHERE c.c_internal_id = m.moduleid";
         }
@@ -29,6 +29,23 @@ if ($updater_utils->check_version("6.1.0")) {
             $update->close();
         }
         $modules->close();
+        // Delete metamodule contet links as they make no sense.
+        if ($lti_integration == 'UoN') {
+            $select_sql = "SELECT c.c_internal_id FROM lti_context c WHERE (LENGTH(c.c_internal_id) - LENGTH(REPLACE(c.c_internal_id, '-', ''))) > 1
+                AND c.c_internal_id NOT LIKE 'ZZ-%'";
+            $modules = $mysqli->prepare($select_sql);
+            $modules->execute();
+            $modules->store_result();
+            $modules->bind_result($id);
+            while ($modules->fetch()) {
+                $delete_sql = "DELETE FROM lti_context WHERE c_internal_id = ?";
+                $delete = $mysqli->prepare($delete_sql);
+                $delete->bind_param('s', $id);
+                $delete->execute();
+                $delete->close();
+            }
+            $modules->close();
+        }
         // Add alter context id to be integer
         $altersql = "ALTER TABLE lti_context MODIFY COLUMN `c_internal_id` int(11) NOT NULL";
         $updater_utils->execute_query($altersql, true);
