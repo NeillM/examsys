e829aff47abd2513365e60660b3d894f8dbbe69f
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2016-03-10 10:22:39 +0000

    ROGO-1658 end user friendly warning

--- lang/cs/paper/new_paper2.php
+++ lang/cs/paper/new_paper2.php
@@ -41,7 +41,7 @@ $string['hrs'] = 'hodin';
 $string['msg7'] = 'Varování: Je potřeba vyplnit datum konání zkoušky.';
 $string['msg8'] = 'Varování: Je potřeba vyplnit délku trvání zkoušky v minutách.';
 $string['msg9'] = 'Varování: Je potřeba vyplnit velikost skupiny studentů.';
-$string['msg10'] = 'WARNING: End datetime must be after Start datetime';
+$string['msg10'] = 'WARNING: A paper cannot finish before it starts';
 $string['duration'] = 'Trvání';
 $string['na'] = 'N/A';
 ?>
