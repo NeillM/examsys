29e1d1f7a5060a808da13c3a7ba449e02a313b13
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2016-05-09 15:49:55 +0100

    ROGO-1808 added extra info to denied delete school message

--- lang/cs/delete/check_delete_school.php
+++ lang/cs/delete/check_delete_school.php
@@ -17,5 +17,5 @@
 require '../lang/' . $language . '/delete/delete.inc';
 
 $string['msg'] = 'Jste si skutečně jisti, že chcete tuto školu odstranit?';
-$string['modulesattached'] = 'All modules must be removed from the school.';
+$string['modulesattached'] = 'All modules and courses must be removed from the school.';
 ?>
