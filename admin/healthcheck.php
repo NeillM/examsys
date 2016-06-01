9e6a65d85b8faca541642e2f0b84a1375bc7b758
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2016-06-01 10:58:02 +0100

    added \n to format multiple errors better. also noted that we need to directly connect to the db and not use the dbutils class in order to avoid the db eror default handling

--- admin/healthcheck.php
+++ admin/healthcheck.php
@@ -26,10 +26,9 @@
   require "../include/load_config.php";
   $error = false;
   // Check access to database.
-  $notice = UserNotices::get_instance();
-  $mysqli = DBUtils::get_mysqli_link($configObject->get('cfg_db_host') , $configObject->get('cfg_db_username'), $configObject->get('cfg_db_passwd'), $configObject->get('cfg_db_database'), $configObject->get('cfg_db_charset'), $notice, $configObject->get('dbclass'));
-  if (mysqli_connect_error()) {
-    echo "ERROR::Can not Connect to MySQL on " . $configObject->get('cfg_db_host');
+  $mysqli = new mysqli($configObject->get('cfg_db_host') , $configObject->get('cfg_db_username'), $configObject->get('cfg_db_passwd'), $configObject->get('cfg_db_database'), 3306);
+  if ($mysqli->connect_error) {
+    echo "ERROR::Can not Connect to MySQL on " . $configObject->get('cfg_db_host') . "\n";
     $error = true;
   }
 
@@ -46,7 +45,7 @@
   if ($ldapserver != '') {
     $ldap = ldap_connect($ldapserver);
     if (!ldap_bind($ldap, $ldaprdn, $ldappass)) {
-      echo "ERROR::Can not Connect to LDAP @ ". $ldapserver;
+      echo "ERROR::Can not Connect to LDAP @ ". $ldapserver . "\n";
       $error = true;
     }
   }
@@ -54,77 +53,77 @@
   try {
     $mediadir = rogo_directory::get_directory('media');
     if (!$mediadir->check_permissions()) {
-      echo "ERROR::Invalid permissions on media directory: " . $mediadir->location();
+      echo "ERROR::Invalid permissions on media directory: " . $mediadir->location() . "\n";
       $error = true;
     }
   } catch (directory_not_found $e) {
-    echo "ERROR::Can not Connect to media directory: " . $e->getMessage();
+    echo "ERROR::Can not Connect to media directory: " . $e->getMessage() . "\n";
     $error = true;
   }
   // Check access to email_templates directory.
   try {
     $mediadir = rogo_directory::get_directory('email_templates');
     if (!$mediadir->check_permissions()) {
-      echo "ERROR::Invalid permissions on email_templates directory: " . $mediadir->location();
+      echo "ERROR::Invalid permissions on email_templates directory: " . $mediadir->location() . "\n";
       $error = true;
     }
   } catch (directory_not_found $e) {
-    echo "ERROR::Can not Connect to email_templates directory: " . $e->getMessage();
+    echo "ERROR::Can not Connect to email_templates directory: " . $e->getMessage() . "\n";
     $error = true;
   }
   // Check access to qti_export directory.
   try {
     $mediadir = rogo_directory::get_directory('qti_export');
     if (!$mediadir->check_permissions()) {
-      echo "ERROR::Invalid permissions on qti_export directory: " . $mediadir->location();
+      echo "ERROR::Invalid permissions on qti_export directory: " . $mediadir->location() . "\n";
       $error = true;
     }
   } catch (directory_not_found $e) {
-    echo "ERROR::Can not Connect to qti_export directory: " . $e->getMessage();
+    echo "ERROR::Can not Connect to qti_export directory: " . $e->getMessage() . "\n";
     $error = true;
   }
   // Check access to qti_import directory.
   try {
     $mediadir = rogo_directory::get_directory('qti_import');
     if (!$mediadir->check_permissions()) {
-      echo "ERROR::Invalid permissions on qti_import directory: " . $mediadir->location();
+      echo "ERROR::Invalid permissions on qti_import directory: " . $mediadir->location() . "\n";
       $error = true;
     }
   } catch (directory_not_found $e) {
-    echo "ERROR::Can not Connect to qti_import directory: " . $e->getMessage();
+    echo "ERROR::Can not Connect to qti_import directory: " . $e->getMessage() . "\n";
     $error = true;
   }
   // Check access to user_photo directory.
   try {
     $mediadir = rogo_directory::get_directory('user_photo');
     if (!$mediadir->check_permissions()) {
-      echo "ERROR::Invalid permissions on user_photo directory: " . $mediadir->location();
+      echo "ERROR::Invalid permissions on user_photo directory: " . $mediadir->location() . "\n";
       $error = true;
     }
   } catch (directory_not_found $e) {
-    echo "ERROR::Can not Connect to user_photo directory: " . $e->getMessage();
+    echo "ERROR::Can not Connect to user_photo directory: " . $e->getMessage() . "\n";
     $error = true;
   }
   // Check access to help_student directory.
   try {
     $mediadir = rogo_directory::get_directory('help_student');
     if (!$mediadir->check_permissions()) {
-      echo "ERROR::Invalid permissions on help_student directory: " . $mediadir->location();
+      echo "ERROR::Invalid permissions on help_student directory: " . $mediadir->location() . "\n";
       $error = true;
     }
   } catch (directory_not_found $e) {
-    echo "ERROR::Can not Connect to help_student directory: " . $e->getMessage();
+    echo "ERROR::Can not Connect to help_student directory: " . $e->getMessage() . "\n";
     $error = true;
   }
   // Check access to help_staff directory.
   try {
     $mediadir = rogo_directory::get_directory('help_staff');
     if (!$mediadir->check_permissions()) {
-      echo "ERROR::Invalid permissions on help_staff directory: " . $mediadir->location();
+      echo "ERROR::Invalid permissions on help_staff directory: " . $mediadir->location() . "\n";
       $error = true;
     }
   } catch (directory_not_found $e) {
-    echo "ERROR::Can not Connect to help_staff directory: " . $e->getMessage();
+    echo "ERROR::Can not Connect to help_staff directory: " . $e->getMessage() . "\n";
     $error = true;
   }
   if (!$error) {
