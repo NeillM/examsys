e157cabcd00f610ba79abf313538976ed0db45b6
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2016-06-07 11:40:52 +0100

    missing space (reverted from commit 3f6ad4dd32b58f7f5e7d7411d9d08d3cb319413d)

--- classes/installutils.class.php
+++ classes/installutils.class.php
@@ -1086,7 +1086,7 @@ $php_date_url = 'http://www.php.net/manual/en/function.date.php';
     $priv_SQL[] = "GRANT SELECT, INSERT ON " . $dbname . ".save_fail_log TO '". self::$cfg_db_staff_user . "'@'". self::$cfg_web_host . "'";
     $priv_SQL[] = "GRANT SELECT, UPDATE ON " . $dbname . ".toilet_breaks TO '". self::$cfg_db_staff_user . "'@'". self::$cfg_web_host . "'";
     $priv_SQL[] = "GRANT SELECT ON " . $dbname . ".academic_year TO '". self::$cfg_db_staff_user . "'@'". self::$cfg_web_host . "'";
-    $priv_SQL[] = "REVOKE SELECT ON " . $dbname . ".plugins FROM '". self::$cfg_db_staff_user . "'@'". self::$cfg_web_host . "'";
+    $priv_SQL[] = "REVOKE SELECT ON" . $dbname . ".plugins FROM '". self::$cfg_db_staff_user . "'@'". self::$cfg_web_host . "'";
 
     $priv_SQL[] = "FLUSH PRIVILEGES";
     foreach ($priv_SQL as $sql) {
@@ -1196,7 +1196,7 @@ $php_date_url = 'http://www.php.net/manual/en/function.date.php';
     $priv_SQL[] = "GRANT SELECT, INSERT, UPDATE, DELETE ON " . $dbname . ".properties_modules TO '". self::$cfg_db_webservice_user . "'@'". self::$cfg_web_host . "'";
     $priv_SQL[] = "GRANT SELECT, INSERT, UPDATE, DELETE ON " . $dbname . ".scheduling TO '". self::$cfg_db_webservice_user . "'@'". self::$cfg_web_host . "'";
     $priv_SQL[] = "GRANT INSERT ON " . $dbname . ".track_changes TO '". self::$cfg_db_webservice_user . "'@'". self::$cfg_web_host . "'";
-    $priv_SQL[] = "REVOKE SELECT ON " . $dbname . ".plugins FROM '". self::$cfg_db_webservice_user . "'@'". self::$cfg_web_host . "'";
+    $priv_SQL[] = "REVOKE SELECT ON" . $dbname . ".plugins FROM '". self::$cfg_db_webservice_user . "'@'". self::$cfg_web_host . "'";
     
     $priv_SQL[] = "FLUSH PRIVILEGES";
     foreach ($priv_SQL as $sql) {
