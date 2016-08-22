0ea65c4809db146709b9021ec9679fd1e1d819f4
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2016-08-22 13:02:21 +0100

    removed double encoding

--- updates/version5/201608101619_marks_config.php
+++ updates/version5/201608101619_marks_config.php
@@ -5,9 +5,9 @@ if ($updater_utils->check_version("6.2.0")) {
         // New configs.
         $configObject = Config::get_instance();
         $configObject->set_db_object($mysqli);
-        $configObject->set_setting('paper_marks_postive', json_encode(range(1, 20)), 'csv');
-        $configObject->set_setting('paper_marks_negative', json_encode(array(0, -0.25, -0.5, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10)), 'csv');
-        $configObject->set_setting('paper_marks_partial', json_encode(array_merge(range(0, 1, 0.1), range(2, 5))), 'csv');
+        $configObject->set_setting('paper_marks_postive', range(1, 20), 'csv');
+        $configObject->set_setting('paper_marks_negative', array(0, -0.25, -0.5, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10), 'csv');
+        $configObject->set_setting('paper_marks_partial', array_merge(range(0, 1, 0.1), range(2, 5)), 'csv');
         
         // Update config type.
         $updatesql = "UPDATE config set type = 'csv' WHERE setting = 'summative_cohort_sizes'";
