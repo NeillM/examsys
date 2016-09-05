37807dc505d31ca749ae80c7a995d29d865ff27f
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2016-09-05 10:38:53 +0100

    refacored version checking into version.class.php

--- classes/updaterutils.class.php
+++ classes/updaterutils.class.php
@@ -356,14 +356,16 @@ Class UpdaterUtils {
     $code_version = $configObject->getxml('version');
     $dev_system = $configObject->get('cfg_dev_system');
 
+    $checkcode = version::is_version_higher($code_version, $version) or $code_version == $version;
     if ($dev_system) {
       // A dev system may need to upgrade even if the config file has been updated.
-      $run_update = ($cfg_version <= $version and $code_version >= $version);
+      $checkcfg = version::is_version_higher($version, $cfg_version) or $cfg_version == $version;
     } else {
       // Production systems should not run update files for versions that were built if the config file is
       // for that version of Rogo, as they should have already been run.
-      $run_update = ($cfg_version < $version and $code_version >= $version);
+      $checkcfg = version::is_version_higher($version, $cfg_version);
     }
+    $run_update = ($checkcfg and $checkcode);
     return $run_update;
   }
 
