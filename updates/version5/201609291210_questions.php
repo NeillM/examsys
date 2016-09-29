d855b7daa02d54719e720ce9fc6f12e119ac835f
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2016-09-29 16:51:05 +0100

    added comments

--- updates/version5/201609291210_questions.php
+++ updates/version5/201609291210_questions.php
@@ -2,10 +2,12 @@
 
 if ($updater_utils->check_version("6.2.0") and !$updater_utils->has_updated('rogo1971_questions')) {
 
+    // Make reference column same type as referred column.
     $altersql = "ALTER TABLE `questions`
         ALTER `status` DROP DEFAULT";
     $updater_utils->execute_query($altersql, true);
 
+    // Add fk.
     $altersql = "ALTER TABLE `questions`
         CHANGE COLUMN `status` `status` INT(11) NOT NULL AFTER `std`,
         ADD CONSTRAINT `questions_fk1` FOREIGN KEY (`status`) REFERENCES `question_statuses` (`id`)";
