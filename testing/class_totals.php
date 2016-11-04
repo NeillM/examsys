3a926e094744d8051a4be4392217a72ec30cbc56
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2016-11-04 15:01:53 +0000

    update other function as well in case it is used

--- testing/class_totals.php
+++ testing/class_totals.php
@@ -39,7 +39,6 @@ function getData($url) {
   curl_setopt($ch, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
   curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-  curl_setopt($ch, CURLOPT_SSLVERSION, 3);
   curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept-Language: en-us,en;q=0.5'));
 
   $output = curl_exec($ch);
