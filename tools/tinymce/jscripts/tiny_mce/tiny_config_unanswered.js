bfd5a05c4b8a445b947ea249d8b883898cfade71
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2017-01-13 14:32:08 +0000

    preview tab on tinymce editor

--- tools/tinymce/jscripts/tiny_mce/tiny_config_unanswered.js
+++ tools/tinymce/jscripts/tiny_mce/tiny_config_unanswered.js
@@ -3,9 +3,9 @@
     editor_selector : "mceEditor",
     theme : "advanced", 
     skin : "default",
-    plugins : "table,visualchars,nonbreaking", 
+    plugins : "preview, table,visualchars,nonbreaking", 
     // Theme options
-    theme_advanced_buttons1 : "cut,copy,paste,|,undo,|,bold,italic,underline,|,sub,sup,|,justifyleft,justifycenter,justifyright,|,numlist,bullist,|,tablecontrols", 
+    theme_advanced_buttons1 : "cut,copy,paste,|,undo,|,bold,italic,underline,|,sub,sup,|,justifyleft,justifycenter,justifyright,|,numlist,bullist,|,tablecontrols,|,preview,", 
     theme_advanced_buttons2 : "", 
     theme_advanced_buttons3 : "",
     theme_advanced_toolbar_location : "top", 
