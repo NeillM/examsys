e0a5334e494fc19a7e5023ac10860cf1c4885c21
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2017-05-02 15:32:05 +0100

    use cleanup and removeformat instead

--- tools/tinymce/jscripts/tiny_mce/tiny_config_question_editor.js
+++ tools/tinymce/jscripts/tiny_mce/tiny_config_question_editor.js
@@ -120,7 +120,7 @@ tinyMCE.init({
     skin : "default",
     plugins : "paste",
     // Theme options
-    theme_advanced_buttons1 : "cut,copy,paste,|,undo|,code",
+    theme_advanced_buttons1 : "cut,copy,paste,|,undo|,cleanup,removeformat",
     theme_advanced_buttons2 : "",
     theme_advanced_buttons3 : "",
     theme_advanced_toolbar_location : "top",
