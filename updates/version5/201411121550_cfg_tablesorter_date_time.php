9cdf794762ba5cce1977b18b3e45872e1608401a
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2017-05-09 14:22:12 +0100

    check version before running update

--- updates/version5/201411121550_cfg_tablesorter_date_time.php
+++ updates/version5/201411121550_cfg_tablesorter_date_time.php
@@ -1,5 +1,9 @@
 <?php
 
-$new_lines = array("\$cfg_tablesorter_date_time = 'uk';\r\n");
-$target_line = '$cfg_long_date_time ';
-$updater_utils->add_line($string, '$cfg_tablesorter_date_time', $new_lines, 69, $cfg_web_root, $target_line, 1);
+// Config no longer required in 6.4.0 onwards so do not run this update script on that version or version higher.
+$version = $configObject->getxml('version');
+if (version::is_version_higher($version, '6.4.0') === false and $version !== '6.4.0') {
+  $new_lines = array("\$cfg_tablesorter_date_time = 'uk';\r\n");
+  $target_line = '$cfg_long_date_time ';
+  $updater_utils->add_line($string, '$cfg_tablesorter_date_time', $new_lines, 69, $cfg_web_root, $target_line, 1);
+}
--- updates/version5/201605171159_full-question-text-in-search.php
+++ updates/version5/201605171159_full-question-text-in-search.php
@@ -1,5 +1,9 @@
 <?php
 
-$new_lines = array("\$cfg_search_leadin_length = 160;\r\n");
-$target_line = '$cfg_timezone ';
-$updater_utils->add_line($string, '$cfg_search_leadin_length', $new_lines, 64, $cfg_web_root, $target_line, 1);
+// Config no longer required in 6.4.0 onwards so do not run this update script on that version or version higher.
+$version = $configObject->getxml('version');
+if (version::is_version_higher($version, '6.4.0') === false and $version !== '6.4.0') {
+  $new_lines = array("\$cfg_search_leadin_length = 160;\r\n");
+  $target_line = '$cfg_timezone ';
+  $updater_utils->add_line($string, '$cfg_search_leadin_length', $new_lines, 64, $cfg_web_root, $target_line, 1);
+}
