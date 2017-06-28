645c64408705471d0a2a9a77694e68700aea9c60
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2017-06-28 11:11:50 +0100

    whitespace

--- lang/en/admin/oauth/add_oauthclient.php
+++ lang/en/admin/oauth/add_oauthclient.php
@@ -45,3 +45,4 @@ $string['facultymanagement/create'] = 'Create a faculty';
 $string['facultymanagement/delete'] = 'Delete a faculty';
 $string['facultymanagement/update'] = 'Update a faculty';
 $string['extsys'] = 'External System';
+
