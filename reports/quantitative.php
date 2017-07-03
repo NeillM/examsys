e1a3038b609b00e0d440053ce21c0ffb676b13bc
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2017-07-03 12:02:40 +0100

    ROGO-2182 refactor so logic is in get_quantitative_log_data

--- reports/quantitative.php
+++ reports/quantitative.php
@@ -422,34 +422,10 @@
   $result->fetch();
   $result->close();
 
-  $exclude = '';
-  if ($_GET['complete'] == 1) {
-    $result = $mysqli->prepare("SELECT 
-      lm.userID,
-      COUNT(l.id) AS answer_no
-    FROM
-      log3 l,
-      log_metadata lm
-    WHERE
-      l.metadataID = lm.id
-      AND lm.paperID = ?
-      AND lm.started >= ?
-      AND lm.started <= ?
-      AND l.user_answer NOT IN ('u', '')
-    GROUP BY lm.userID");
-    $result->bind_param('iss', $paperID, $startdate, $enddate);
-    $result->execute();
-    $result->bind_result($tmp_userid, $answer_no);
-    while ($result->fetch()) {
-      if ($answer_no < $number_of_questions or $answer_no > $number_of_questions) {
-        $exclude .= ' AND lm.userID != "' . $tmp_userid . '"';
-      }
-    }
-    $result->close();
-  }
+  $exclude = param::optional('complete', false, param::BOOLEAN, param::FETCH_GET);
 
   $log_array = array();
-  $hits = get_quantitative_log_data($_GET['paperID'], $_GET['repcourse'], $_GET['startdate'], $_GET['enddate'], $exclude, $log_array, $mysqli);
+  $hits = get_quantitative_log_data($_GET['paperID'], $_GET['repcourse'], $_GET['startdate'], $_GET['enddate'], $exclude, $log_array, $mysqli, $number_of_questions);
 
   $module_code = '';
   $module = (isset($_GET['module']) and $_GET['module'] != '') ? $_GET['module'] : '';
