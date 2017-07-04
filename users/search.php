51647c9caa95d7b2c32ba40c0f66688ac15862b6
Naseem Sarwar <naseem.sarwar@nottingham.ac.uk>
2017-07-04 14:38:59 +0100

    Now searching the following string patterns. 1- Exact surname and first name search (e.g.' Ian Slater') 2- Partial surname and first name search (e.g. 'Ian Sla') 3- Different positions of the firstname and surname (e.g 'slater Ian')

--- users/search.php
+++ users/search.php
@@ -129,10 +129,22 @@ if (isset($_GET['submit'])) {
       $initials_param_types = 's';
       $initials_params = array($tmp_initials . '%');
     }
-    $tmp_surname = $mysqli->real_escape_string(str_replace('*', '%', $tmp_surname));
-    $surname_sql = " AND ( surname LIKE ? or first_names LIKE ? ) ";
-    $surname_param_types = 'ss';
-    $surname_params = array($tmp_surname, $tmp_surname);
+    $tmp_surname = explode(' ',$tmp_surname);
+    $i=0;
+    $surname_sql = " AND ( ";
+    $surname_param_types = '';
+    foreach($tmp_surname as $name){
+        if($i > 0){
+            $surname_sql .= " OR ";
+        }
+        $i++;
+        $name = $mysqli->real_escape_string(str_replace('*', '%', $name));
+        $surname_sql .= " surname LIKE ? OR first_names LIKE ?";
+        $surname_param_types .= 'ss';
+        $name = '%'.$name.'%';
+        array_push($surname_params,$name,$name);
+    }
+    $surname_sql .= " ) ";
   }
 
   if (!is_null($search_username) and $search_username !== '') {
