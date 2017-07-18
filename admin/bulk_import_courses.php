a35b867606b735d84eb917c74b8f4f046c892d21
Baxter Joseph <Joseph.Baxter@nottingham.ac.uk>
2017-07-18 10:18:33 +0100

    use template header / footer

--- admin/bulk_import_courses.php
+++ admin/bulk_import_courses.php
@@ -31,42 +31,29 @@ require '../include/toprightmenu.inc';
 $render = new render($configObject);
 
 ini_set("auto_detect_line_endings", true);
+$lang['title'] = $string['bulkcourseimport'];
+$additionaljs = "<script type=\"text/javascript\" src=\"../js/jquery.validate.min.js\"></script>
+                <script>
+                    $(function () {
+                      $('#import_form').validate();
+                      
+                      $('#cancel').click(function() {
+                        history.back();
+                      });
+                    });
+                </script>";
+$addtionalcss = "<link rel=\"stylesheet\" type=\"text/css\" href=\"../css/dialog.css\" />
+                <style type=\"text/css\">
+                    p {margin:0; padding:0}
+                    h1 {font-size:120%; font-weight:bold}
+                    label.error {display:block; color:#f00}
+                    .existing {color:#808080}
+                    .added {color:black}
+                    .failed {color:#C00000}
+                </style>";
+$render->render_admin_header($lang, $additionaljs, $addtionalcss);
 ?>
-<!DOCTYPE html>
-<html>
-  <head>
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta http-equiv="content-type" content="text/html;charset=<?php echo $configObject->get('cfg_page_charset') ?>" />
-  
-  <title><?php echo $string['bulkcourseimport'] . ' ' . $configObject->get('cfg_install_type'); ?></title>
-  
-  <link rel="stylesheet" type="text/css" href="../css/body.css" />
-  <link rel="stylesheet" type="text/css" href="../css/header.css" />
-  <link rel="stylesheet" type="text/css" href="../css/dialog.css" />
-  <link rel="stylesheet" type="text/css" href="../css/submenu.css" />
-  <style type="text/css">
-    p {margin:0; padding:0}
-    h1 {font-size:120%; font-weight:bold}
-    label.error {display:block; color:#f00}
-    .existing {color:#808080}
-    .added {color:black}
-    .failed {color:#C00000}
-  </style>
-  <script type="text/javascript" src="../js/jquery-1.11.1.min.js"></script>
-  <script type="text/javascript" src="../js/jquery.validate.min.js"></script>
-  <script type="text/javascript" src="../js/toprightmenu.js"></script>
-  <script>
-    $(function () {
-      $('#import_form').validate();
-      
-      $('#cancel').click(function() {
-        history.back();
-      });
-    });
-  </script>
-  </head>
-
-  <body>
+<body>
 <?php
   require '../include/course_options.inc';
   echo draw_toprightmenu();
@@ -208,9 +195,7 @@ ini_set("auto_detect_line_endings", true);
 </tr>
 </table>
 
-</div>
 <?php
   }
+  $render->render_admin_footer();
 ?>
-</body>
-</html>
