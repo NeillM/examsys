b2eae8e1d978b05078374459ec729ba0b6b7b780
uaann <naseem.sarwar@nottingham.ac.uk>
2017-07-24 13:06:15 +0100

    ROGO-1970 space removed.

--- admin/add_module.php
+++ admin/add_module.php
@@ -223,7 +223,7 @@ if (isset($_POST['submit']) and $unique_moduleid == true) {
 ?>
     <table cellpadding="0" cellspacing="1" border="0" style="text-align:left; margin-left:auto; margin-right:auto">
     <tr><td class="field"><?php echo $string['moduleid'] ?></td><td><input type="text" size="10" maxlength="25" id="modulecode" name="modulecode" value="<?php echo $tmp_modulecode ?>" required autofocus /></td></tr>
-    <tr><td class="field"><?php echo $string['name'] ?></td><td><input type="text" size="70" id="fullname" name="fullname" value="<?php if (isset($_POST['fullname'])) echo $_POST['fullname'] ?>" required  /></td></tr>
+    <tr><td class="field"><?php echo $string['name'] ?></td><td><input type="text" size="70" id="fullname" name="fullname" value="<?php if (isset($_POST['fullname'])) echo $_POST['fullname'] ?>" required /></td></tr>
 
 <?php
   $old_faculty = '';
