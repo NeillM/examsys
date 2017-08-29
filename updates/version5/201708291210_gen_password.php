dc0dd8eca40687ceb0b41e46d2818fcc40067fcc
Baxter Joseph <Joseph.Baxter@nottingham.ac.uk>
2017-08-29 16:18:45 +0100

    should default update to no dictionary file as users need to install a pre-requisite dictionary in order for this to work

--- updates/version5/201708291210_gen_password.php
+++ updates/version5/201708291210_gen_password.php
@@ -3,7 +3,7 @@ if ($updater_utils->check_version("6.5.0")) {
     if (!$updater_utils->has_updated('rogo_1843')) {
         $configObject = Config::get_instance();
         $configObject->set_db_object($mysqli);
-        $configObject->set_setting('misc_dictionary_file', '/usr/share/dict/words', 'string');
+        $configObject->set_setting('misc_dictionary_file', '', 'string');
         $updater_utils->record_update('rogo_1843');
     }
 }
