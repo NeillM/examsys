4ddcf63bf5e572a05a3d3cc198f6050b3b7263f0
Baxter Joseph <Joseph.Baxter@nottingham.ac.uk>
2017-09-15 12:10:44 +0100

    update refactor. pre 6.4 null enhancedcalc_type means Rrserve.

--- updates/version5/201708170930_calc_cfgs.php
+++ updates/version5/201708170930_calc_cfgs.php
@@ -3,11 +3,12 @@
 if ($updater_utils->check_version("6.5.0")) {
     if (!$updater_utils->has_updated('rogo2080')) {
         $configObject = Config::get_instance();
-        $configObject->set_setting('cfg_calc_type', $configObject->get('enhancedcalc_type'), Config::STRING);
-        if ($configObject->get('enhancedcalc_type') == 'phpEval') {
-          $configObject->set_setting('cfg_calc_settings', array('host' => '', 'port' => '', 'timeout' => ''), Config::ASSOC);
+        if ($configObject->get('enhancedcalc_type') === 'phpEval') {
+            $configObject->set_setting('cfg_calc_type', 'phpEval', Config::STRING);
+            $configObject->set_setting('cfg_calc_settings', array('host' => '', 'port' => '', 'timeout' => ''), Config::ASSOC);
         } else {
-          $configObject->set_setting('cfg_calc_settings', $configObject->get('enhancedcalculation'), Config::ASSOC);
+            $configObject->set_setting('cfg_calc_type', 'Rrserve', Config::STRING);
+            $configObject->set_setting('cfg_calc_settings', $configObject->get('enhancedcalculation'), Config::ASSOC);
         }
         $updater_utils->record_update('rogo2080');
     }
