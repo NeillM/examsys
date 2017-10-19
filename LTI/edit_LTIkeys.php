ad75f727417b0947605c6a7da4898c3c7642a222
Baxter Joseph <Joseph.Baxter@nottingham.ac.uk>
2017-10-19 12:18:52 +0100

    Missed one

--- LTI/edit_LTIkeys.php
+++ LTI/edit_LTIkeys.php
@@ -31,6 +31,7 @@ $lti->init_lti0($mysqli);
 $LTIkeysid = check_var('LTIkeysid', 'GET', true, false, true);
 
 if (!$lti->lti_key_exists($LTIkeysid)) {
+  $contactemail = support::get_email();
   $msg = sprintf($string['furtherassistance'], $contactemail, $contactemail);
   $notice->display_notice_and_exit($mysqli, $string['pagenotfound'], $msg, $string['pagenotfound'], '../artwork/page_not_found.png', '#C00000', true, true);
 }
