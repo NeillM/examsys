49f5a7daddd7726351d5c4528d0fc530d98c4a92
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2018-02-13 13:11:48 +0000

    ROGO-2309 info unittest

---
+++ testing/unittest/tests/questions/infoTest.php
@@ -0,0 +1,47 @@
+<?php
+// This file is part of Rogō
+//
+// Rogō is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Rogō is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Rogō.  If not, see <http://www.gnu.org/licenses/>.
+
+use testing\unittest\unittest;
+
+/**
+ * Test info question class
+ * 
+ * @author Dr Joseph Baxter <joseph.baxter@nottingham.ac.uk>
+ * @version 1.0
+ * @copyright Copyright (c) 2018 onwards The University of Nottingham
+ * @package tests
+ */
+class infotest extends unittest{
+
+  /**
+    * Test area question setter
+    * @group question
+    */
+  public function test_set_question() {
+    $pluginns = 'plugins\questions\info\render';
+    $render = new $pluginns();
+    $render->set_question(1, '', '');
+    $this->assertFalse($render->get('displaymedia'));
+    $this->assertTrue($render->get('displayleadin'));
+    $render->set('qmedia', 'test');
+    $render->set('questionno', 2);
+    $render->set_question(1, '', '');
+    $this->assertTrue($render->get('displaymedia'));
+    $this->assertEquals(1, $render->get('questionno'));
+  }
+ 
+
+}
