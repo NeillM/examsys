f42d8fcb3669012a42051f879817aaf33eeeb9ec
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2018-05-21 15:49:24 +0100

    dialog css should be plugin specific

--- plugins/texteditor/plugin_tinymce3_texteditor/tinymce/jscripts/tiny_mce/plugins/insertimagestudent/dialog.php
+++ plugins/texteditor/plugin_tinymce3_texteditor/tinymce/jscripts/tiny_mce/plugins/insertimagestudent/dialog.php
@@ -34,7 +34,7 @@
   <title>Add New Image</title>
 
   <link rel="stylesheet" type="text/css" href="../../../../../../../../css/body.css" />
-  <link rel="stylesheet" type="text/css" href="../../../../../../../../css/dialog.css" />
+  <link rel="stylesheet" type="text/css" href="css/dialog.css" />
   <style type="text/css">
     body {font-size:90%}
     .field {text-align:right}
