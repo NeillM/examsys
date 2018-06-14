e4d75712ff5dd99288124e4048e977ff3ad4f86f
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2018-06-14 13:34:32 +0100

    header needs to be set before session delete called.

--- logout.php
+++ logout.php
@@ -36,9 +36,8 @@ session_unset();
 session_destroy();
 session_write_close();
 setcookie(session_name(), '', 0, '/');
-session_regenerate_id(true);
-
 header('Location: ./');
+session_regenerate_id(true);
 ?>
 <html>
 <body>
