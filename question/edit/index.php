0f17c3aedda66bf4aa4077316d126327d1653f4f
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2018-08-06 17:37:50 +0100

    only apply new logic to enhanced calc

--- question/edit/index.php
+++ question/edit/index.php
@@ -77,10 +77,20 @@ function save_options($question, $userObject, $db) {
 
   for ($option_no = 1; $option_no <= $question->max_options; $option_no++) {
     $option = null;
-
-    if (isset($question->options[$option_no])) {
+    $check = false;
+    if ($question->get_type() === 'enhancedcalc') {
+      if (isset($question->options[$option_no])) {
+        $check = true;
+        $option = $question->options[$option_no];
+      }
+    } else {
+      if (isset($_POST["optionid$option_no"]) and $_POST["optionid$option_no"] != -1) {
+        $check = true;
+        $option = $question->options[$_POST["optionid$option_no"]];
+      }
+    }
+    if ($check) {
       // Editing existing option
-      $option = $question->options[$option_no];
       $part_names = $option->get_editable_fields();
       try {
         $postparams = get_post_params($part_names, $option, $option_no);
