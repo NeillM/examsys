4436e408a08c822e6421777ef49ce15c51a77ddb
Richard Aspden (GetJohn) <richard@getjohn.co.uk>
2019-04-30 12:33:35 +0000

    Wrap link to force this to only be viewable to staff accounts

--- reviews/start.php
+++ reviews/start.php
@@ -657,8 +657,10 @@ echo '" onsubmit="return confirmSubmit()" autocomplete="off">';   // Warning mes
     if ($q_displayed == 0 and $current_screen == 1 and $paper_prologue != '') echo '<tr><td colspan="2" style="padding:20px; text-align:justify">' . $paper_prologue . '</td></tr>';
     if ($q_displayed == 0 and $question['theme'] == '') echo "<tr><td colspan=\"2\">&nbsp;</td></tr>\n";
 
-    echo "<tr><td colspan=\"2\"><a href=\"/question/edit/index.php?q_id=" . $question['q_id'] . "&qNo=$question_no&paperID=$paperID\" target=\"_blank\" style=\"padding-left: 40px;\">{$string['editquestion']}</a></td></tr>";
-    
+    if (!$userObject->has_role('External Examiner') and !$userObject->has_role('Internal Reviewer')) {
+      echo "<tr><td colspan=\"2\"><a href=\"/question/edit/index.php?q_id=" . $question['q_id'] . "&qNo=$question_no&paperID=$paperID\" target=\"_blank\" style=\"padding-left: 40px;\">{$string['editquestion']}</a></td></tr>";
+    }
+
     display_question($configObject, $question, $propertyObj->get_paper_type(), $propertyObj->get_calculator(), $current_screen, $previous_q_type, $question_no, $question_offset, $start_of_day_ts, $texteditorplugin);
     $previous_q_type = $question['q_type'];
     $q_displayed++;
