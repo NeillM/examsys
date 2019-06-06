c08b10c5b936315767b688e44f95a06d7d4760a7
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2019-06-06 15:01:52 +0000

    Merged in improvement/ROGO-2313_Part6of32 (pull request #1075)
    ROGO-2313 mathjax implementation modularised (6 of 32)
    
    * ROGO-2313 mathjax implementation modularised
    * init now done in main.js
    
    Approved-by: Neill Magill
    Approved-by: yijun xue

--- js/src/mathjax-config.js
+++
@@ -1,9 +0,0 @@
-var MathJax = {
-    extensions: ["tex2jax.js"],
-    jax: ["input/TeX", "output/HTML-CSS"],
-    messageStyle: "none",
-    showMathMenu: false,
-    showMathMenuMSIE: false,
-    tex2jax: {inlineMath: [['$$','$$'], ['[tex]', '[/tex]'], ['[texi]', '[/texi]']], displayMath: [['$$$','$$$']]},
-    "HTML-CSS": {fonts: ["TeX"]}
-};
