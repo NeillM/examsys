e6a3b5e2328bf6d54eff149892c3a4b9f394e0b2
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2019-06-06 15:01:05 +0000

    Merged in improvement/ROGO-2313_part5of32 (pull request #1074)
    ROGO-2313 jcalc partially modularised (5 of 32)
    
    * ROGO-2313 jcalc partially modularised
    * jcalc98.js is used with permission
    * jcalc98uon.js is our implementation so this has been modularised
    
    Approved-by: Neill Magill
    Approved-by: yijun xue

--- js/src/jcalc98uon.js
+++ js/src/jcalc98uon.js
@@ -1,11 +1,33 @@
-$(function () {
-    $('.calc').draggable();
-    $('#lab_off').click(function() {
-        $('.calc').css('display', 'none');
-    });
-    $('.sciCalculator').click(function() {
-        $('.calc').css('display', 'block');
-        $('.calc').css('top',10);
-        $('.calc').css('left',$(window).width() - 280);
+// This file is part of Rogo
+//
+// Rogo is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Rogo is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Rogo.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Jcalc.
+//
+// @author Dr Joseph Baxter <joseph.baxter@nottingham.ac.uk>
+// @copyright Copyright (c) 2018 The University of Nottingham
+//
+requirejs(['jquery', 'jqueryui'], function($) {
+    $(function () {
+        $('.calc').draggable();
+        $('#lab_off').click(function () {
+            $('.calc').css('display', 'none');
+        });
+        $('.sciCalculator').click(function () {
+            $('.calc').css('display', 'block');
+            $('.calc').css('top', 10);
+            $('.calc').css('left', $(window).width() - 280);
+        });
     });
 });
