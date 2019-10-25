e9e79a9cf1238f3690887ad987b4f28492c7db13
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2019-10-25 08:46:58 +0000

    Merged in drjosephbaxter/rog/hotfix/ROGO-2599 (pull request #1184)
    hotfix/ROGO-2599
    
    Approved-by: Neill Magill <neill.magill@nottingham.ac.uk>
    Approved-by: yijun xue <xuey@hotmail.com>

--- updates/scripts/201905300930_textterms.php
+++ updates/scripts/201905300930_textterms.php
@@ -8,13 +8,15 @@ if ($updater_utils->check_version("7.1.0")) {
     $updatesql = $mysqli->prepare("UPDATE questions SET settings = ? WHERE q_id = ?");
     while ($selectsql->fetch()) {
       $oldsettings = json_decode($settings, true);
-      $oldterms = explode(';', $oldsettings['terms']);
-      if (is_array($oldterms)) {
-        // JSON encode instead of ; seperated.
-        $oldsettings['terms'] = json_encode($oldterms);
-        $newsettings = json_encode($oldsettings, true);
-        $updatesql->bind_param('si', $newsettings, $q_id);
-        $updatesql->execute();
+      if (isset($oldsettings['terms'])) {
+        $oldterms = explode(';', $oldsettings['terms']);
+        if (is_array($oldterms)) {
+          // JSON encode instead of ; seperated.
+          $oldsettings['terms'] = json_encode($oldterms);
+          $newsettings = json_encode($oldsettings, true);
+          $updatesql->bind_param('si', $newsettings, $q_id);
+          $updatesql->execute();
+        }
       }
     }
     $selectsql->close();
