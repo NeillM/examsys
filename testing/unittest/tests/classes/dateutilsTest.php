a99ba40e026d7106838a93f0b8a5356f31db80a8
Neill Magill <neill.magill@nottingham.ac.uk>
2020-05-14 10:49:44 +0100

    ROGO-2783 Add more unit tests for checking timestamp generation

--- testing/unittest/tests/classes/dateutilsTest.php
+++ testing/unittest/tests/classes/dateutilsTest.php
@@ -53,15 +53,46 @@ class DateUtilsTest extends unittest
 
     /**
      * Test getting timestamp from time
+     *
+     * @param string $timezone
+     * @param int $hour
+     * @param int $minute
      * @group date
+     * @dataProvider dataGetTimestampFromTime
      */
-    public function testGetTimestampFromTime(): void
+    public function testGetTimestampFromTime(string $timezone, int $hour, int $minute): void
     {
-        $timezone = new DateTimeZone('Europe/London');
-        $min = 30;
-        $hr = 1;
-        $tmp_datetime = new DateTime(date('Y-m-d') . $hr . ':' . $min . ':00', $timezone);
-        $expected = $tmp_datetime->getTimestamp();
-        $this->assertEquals($expected, date_utils::getTimestampFromTime($hr, $min, $timezone));
+        $tz = new DateTimeZone($timezone);
+        // The date right now for the timezone.
+        $now = new DateTime('now', $tz);
+
+        $result = date_utils::getTimestampFromTime($hour, $minute, $tz);
+        // Convert the timestamp to a DateTime using the server timezone.
+        $resultdate = new DateTime(date('Y-m-d H:i:s', $result));
+        $resultdate->setTimezone($tz);
+
+        // Assert that the local day is the same and that the hour and minutes are correct.
+        $this->assertEquals($now->format('Y-m-d'), $resultdate->format('Y-m-d'));
+        $this->assertEquals($hour, $resultdate->format('G'));
+        $this->assertEquals($minute, $resultdate->format('i'));
+    }
+
+    /**
+     * Data used to test that timestamps are calculated correctly.
+     *
+     * @return array
+     */
+    public function dataGetTimestampFromTime(): array {
+        return [
+            'london-early' => ['Europe/London', 1, 30],
+            'london-noon' => ['Europe/London', 12, 0],
+            'london-late' => ['Europe/London', 23, 5],
+            'china-early' => ['Asia/Shanghai', 0, 1],
+            'china-noon' => ['Asia/Shanghai', 12, 0],
+            'china-late' => ['Asia/Shanghai', 23, 29],
+            'alaska-early' => ['America/Anchorage', 1, 15],
+            'alaska-noon' => ['America/Anchorage', 12, 0],
+            'alaska-late' => ['America/Anchorage', 23, 59],
+        ];
     }
 }
