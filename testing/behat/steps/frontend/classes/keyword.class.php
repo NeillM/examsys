57937d17c83f4ec69e4f529d2c62451ed8e8c677
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2020-09-09 13:21:53 +0000

    Merged in drjosephbaxter/rog/improvement/ROGO-2818_autotest_performance (pull request #1313)
    ROGO-2818 wait for page to load
    
    Approved-by: Yijun Xue <yijun.xue@nottingham.ac.uk>
    Approved-by: Neill Magill <neill.magill@nottingham.ac.uk>

--- testing/behat/steps/frontend/classes/keyword.class.php
+++ testing/behat/steps/frontend/classes/keyword.class.php
@@ -43,5 +43,6 @@ trait Keyword
         $this->fillField('new_keyword', $keyword);
         $this->i_click('OK', 'button');
         $this->i_focus_main_window();
+        $this->i_wait_for_page_to_load();
     }
 }
