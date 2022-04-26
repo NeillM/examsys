891cf94e8091d3cb5bb04c45d3657011ded6c88e
cczyx <yijun.xue@nottingham.ac.uk>
2022-04-26 17:50:31 +0100

    ROGO-3233 Add  update file to fix the bug in other versons

---
+++ updates/scripts/20220426_questionsmodules.php
@@ -0,0 +1,24 @@
+<?php
+
+// This file is part of Rogō
+//
+// Rogō is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Rogō is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Rogō.  If not, see <http://www.gnu.org/licenses/>.
+
+if (!$updater_utils->has_updated('ROGO-3233')) {
+    $sql1 = 'ALTER TABLE questions_modules ADD CONSTRAINT `questions_modules_fk1` FOREIGN KEY (`q_id`) REFERENCES `questions` (`q_id`)';
+    $sql2 = 'ALTER TABLE questions_modules ADD CONSTRAINT `questions_modules_fk2` FOREIGN KEY (`idMod`) REFERENCES `modules` (`id`)';
+    $updater_utils->execute_query($sql1, false);
+    $updater_utils->execute_query($sql2, false);
+    $updater_utils->record_update('ROGO-3233');
+}
