c3b6d93bf9a3181aeb1c99bcdf0d2e8a432367b9
cczyx <yijun.xue@nottingham.ac.uk>
2022-08-12 17:04:33 +0100

    ROGO-2981 Remove unnecessary included file

--- reviews/sct_review.php
+++ reviews/sct_review.php
@@ -30,7 +30,7 @@ $language = LangUtils::getLang($cfg_web_root);
 LangUtils::loadlangfile(str_replace($cfg_web_root, '', str_replace('\\', '/', ($_SERVER['SCRIPT_FILENAME']))));
 
 require_once '../include/errors.php';
-require_once $cfg_web_root . 'classes/authentication.inc.php';
+require_once '../include/sct_review.inc';
 
 // Connect to the database as the SCT user.
 $mysqli = DBUtils::get_mysqli_link(
