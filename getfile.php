5fe590acad47e1367654d1edfa8f4d54e3b10c18
cczyx <yijun.xue@nottingham.ac.uk>
2022-08-31 15:22:40 +0100

    ROGO-2981 Use an empty array $newauth for this auth setting, so it wiil use alreadyloggedin class  to do auth in gertfile.php

--- getfile.php
+++ getfile.php
@@ -23,13 +23,9 @@
  * @package core
  */
 
-session_start(); // Need this to check if user's login status $_SESSION['authenticationObj']['loggedin']
 require_once './include/load_config.php';
+require_once './include/get_session.php';
 require_once './include/errors.php';
-
-if(isset($_SESSION['authenticationObj']['loggedin'])) {
-    define('SCT_AUTH_SUCCESS', 1);
-}
 
 // End the session now so that it no longer blocks other files being loaded.
 session_write_close();
@@ -45,8 +41,10 @@ try {
 } catch (directory_not_found $e) {
     send_404();
 }
-if($directory->authentication_required() and !defined('SCT_AUTH_SUCCESS')){
-    require_once $cfg_web_root . './classes/authentication.inc.php';
+if($directory->authentication_required()){
+    $newauth = array();
+    $configObject->set('authentication', $newauth);
+    require_once $cfg_web_root . 'classes/authentication.inc.php';
 }
 
 try {
