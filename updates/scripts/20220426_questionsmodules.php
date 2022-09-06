c51291d462a8c7a0bfb25c130dec3c92ee22fe6d
cczyx <yijun.xue@nottingham.ac.uk>
2022-09-06 10:19:32 +0100

    ROGO-3233 Change 2 steps names

--- updates/scripts/20220426_questionsmodules.php
+++ updates/scripts/20220426_questionsmodules.php
@@ -19,11 +19,11 @@ if ($updater_utils->check_version('7.5.0') and !$updater_utils->has_updated('ROG
     if ($updater_utils->does_foreign_key_exist('questions_modules','q_id','questions', 'q_id', 'questions_modules_fk1') === 0 ) {
         $sql1 = 'ALTER TABLE questions_modules ADD CONSTRAINT `questions_modules_fk1` FOREIGN KEY (`q_id`) REFERENCES `questions` (`q_id`)';
         $updater_utils->execute_query($sql1, false);
-        $updater_utils->record_update('ROGO-3233');
+        $updater_utils->record_update('ROGO-3233_1');
     }
     if ($updater_utils->does_foreign_key_exist('questions_modules','idMod','modules', 'id', 'questions_modules_fk2') === 0 ) {
         $sql2 = 'ALTER TABLE questions_modules ADD CONSTRAINT `questions_modules_fk2` FOREIGN KEY (`idMod`) REFERENCES `modules` (`id`)';
         $updater_utils->execute_query($sql2, false);
-        $updater_utils->record_update('ROGO-3233');
+        $updater_utils->record_update('ROGO-3233_2');
     }
 }
