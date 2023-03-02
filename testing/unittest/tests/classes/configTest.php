f899fa524191d8e64c0dd4e2b31d94ef656c178f
cczyx <yijun.xue@nottingham.ac.uk>
2023-03-02 14:58:18 +0000

    EXAMSYS-3330 Add a phpunit test for check_type function

---
+++ testing/unittest/tests/classes/configTest.php
@@ -0,0 +1,52 @@
+<?php
+
+// This file is part of ExamSys
+//
+// ExamSys is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// ExamSys is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with ExamSys.  If not, see <http://www.gnu.org/licenses/>.
+
+use testing\unittest\unittest;
+
+/**
+ * Unit tests the for  config class.
+ *
+ * @author Yijun Xue <yijun.xue@nottingham.ac.uk>
+ * @copyright Copyright (c) 2020 onwards The University of Nottingham
+ * @package tests
+ * @group core
+ */
+class configTest extends unittest {
+    /**
+     * Tests check_type function in config class
+     *
+     * @dataProvider dataToChecktype
+     */
+    public function testRogoToTimestamp(string $input, bool $expected) {
+        $datatype =Config::check_type($input, "boolean");
+        $this->assertEquals($expected, Config::check_type($input, $datatype));
+    }
+
+    /**
+     * Data used to test check_type function checking BOOLEAN.
+     *
+     * @return array
+     */
+    public function dataToChecktype(): array {
+        return [
+                'default' => ['',  true],
+                'default' => ['somevalue', true],
+                'default' => ['1', true],
+                'default' => ['0', true]
+        ];
+    }
+}
