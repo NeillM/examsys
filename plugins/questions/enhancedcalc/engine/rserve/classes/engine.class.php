4150da27c183aea8b4f70b098a0c1c7abac91bb2
Neill Magill <neill.magill@nottingham.ac.uk>
2023-05-03 11:33:09 +0100

    EXAMSYS-3303 Add a Rserve engine with a better name
    The double r in the main name is annoying.

---
+++ plugins/questions/enhancedcalc/engine/rserve/classes/engine.class.php
@@ -0,0 +1,33 @@
+<?php
+
+// This file is part of ExamSys
+//
+// ExamSys is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// ExamSys is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with ExamSys.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace plugins\questions\enhancedcalc\engine\rserve;
+
+/**
+ * R based maths functions for Calculation questions.
+ *
+ * This is an alias of the main class, it has a nicer spelling.
+ *
+ * @author Neill Magill <neill.magill@nottingham.ac.uk>
+ * @version 1.0
+ * @copyright Copyright (c) 2023 The University of Nottingham
+ * @package
+ */
+class Engine extends \plugins\questions\enhancedcalc\engine\rrserve\Engine
+{
+    // This class uses a better spelling of RServe.
+}
