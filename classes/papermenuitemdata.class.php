69e38e01bd12486463086f75db59582a2b9e6e94
trishxnd <iyudtrish@gmail.com>
2024-10-04 13:32:05 +0100

    EXAMSYS-3510 Added data for rendering Import/Export menu item

--- classes/papermenuitemdata.class.php
+++ classes/papermenuitemdata.class.php
@@ -23,6 +23,23 @@ class MenuItemData {
         ];
     }
 
+    public function createSubmenuItem($text, $icon, $id, $dataAttributes, $submenuItems) {
+        return [
+            'text' => $text,
+            'icon' => Config::get_instance()->get('cfg_root_path') . '/artwork/' . $icon,
+            'href' => '#',
+            'classes' => 'menuitem cascade showmenu',
+            'disabled' => false,
+            'id' => $id,
+            'data_attributes' => $dataAttributes,
+            'hasPopup' => true,
+            'tabindex' => 0,
+            'submenu' => $submenuItems,
+            'template' => 'sidebar/submenuitem.html'
+        ];
+    }
+
+
     public function getTestPreviewItem($properties) {
         if ($properties->get_paper_type() == '5' || $properties->get_item_no() == 0) {
             return [
@@ -49,5 +66,42 @@ class MenuItemData {
                 ]
             ];
         }
+    }
+
+    public function getImportExportItem($properties, $paperID, $module) {
+        $submenuItems = [];
+        
+        if (!$properties->get_summative_lock()) {
+            $submenuItems[] = [
+                'text' => $this->string['import'],
+                'href' => Config::get_instance()->get('cfg_root_path') . "/qti/import.php?paperID=$paperID&module=$module"
+            ];
+            $submenuItems[] = [
+                'text' => $this->string['importraf'],
+                'href' => Config::get_instance()->get('cfg_root_path') . "/import/rogo_assessment_format.php?paperID=$paperID&module=$module"
+            ];
+            if ($properties->get_question_no() > 0) {
+                $submenuItems[] = ['type' => 'separator'];
+            }
+        }
+        
+        if ($properties->get_question_no() > 0) {
+            $submenuItems[] = [
+                'text' => $this->string['export12'],
+                'href' => Config::get_instance()->get('cfg_root_path') . "/qti/export.php?dest=qti12&paperID=$paperID&module=$module"
+            ];
+            $submenuItems[] = [
+                'text' => $this->string['exportraf'],
+                'href' => Config::get_instance()->get('cfg_root_path') . "/export/rogo_assessment_format.php?paperID=$paperID"
+            ];
+        }
+
+        return $this->createSubmenuItem(
+            $this->string['importexport'],
+            'ims_16.png',
+            'qti',
+            ['popupid' => '2', 'popuptype' => 'papertasks', 'popupname' => 'qti'],
+            $submenuItems
+        );
     }
 }
