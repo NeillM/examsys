9320cb0f091789754fa51feaf05dbfa2a1e567be
trishxnd <iyudtrish@gmail.com>
2025-01-23 14:45:32 +0000

    EXAMSYS-3510 Removing unused code

--- js/src/modules/sidebar.js
+++ js/src/modules/sidebar.js
@@ -122,16 +122,6 @@ define(['jsxls', 'rogoconfig', 'jquery', 'jqueryui'], function(jsxls, config, $)
                 scope.scrollDownEnd();
             });
 
-            // $('.showmenu').click(function(e) {
-            //     var options = JSON.parse($("#popupmenu" + $(this).attr('data-popupid')).attr('data-myOptions'));
-            //     var urls = JSON.parse($("#popupmenu" + $(this).attr('data-popupid')).attr('data-myURLs'));
-            //     var id = 'popup' + $(this).attr('data-popupid');
-            //     var type = $(this).attr('data-popuptype');
-            //     var name = $(this).attr('data-popupname');
-            //     scope.lastFocusedTrigger = $(this).closest('.menuitem');  // Store the parent menuitem
-            //     scope.showMenu(id, type, name, options, urls, e);
-            // });
-
             $('.popup').mouseleave(function(e) {
                 // FF/IE trigger mouseleave incorrectly on dropdown use so ignore.
                 if (e.target.tagName !== 'SELECT') {
