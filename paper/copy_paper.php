540938db70ac279d507d5a8c301fa546a57e49d1
trishxnd <iyudtrish@gmail.com>
2025-02-20 10:41:10 +0000

    EXAMSYS-3510 Added Copyright and updated parameter assertions

--- paper/copy_paper.php
+++ paper/copy_paper.php
@@ -1,11 +1,33 @@
 <?php
+// This file is part of ExamSys
+//
+// ExamSys is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// ExamSys is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with ExamSys.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Copy Paper page - allows copying of exam papers
+ *
+ * @author Iyud Dissanayake
+ * @copyright Copyright (c) 2025 The University of Nottingham
+ */
+
 require_once '../include/staff_auth.inc';
 require_once '../include/errors.php';
 
 // Get variables from paper_options.php
-$paperID = $_GET['paperID'] ?? '';
-$module = $_GET['module'] ?? '';
-$folder = $_GET['folder'] ?? '';
+$paperID = check_var('paperID', 'GET', true, false, true, param::INT);
+$module = param::optional('module', null, param::INT, param::FETCH_GET);
+$folder = param::optional('folder', null, param::INT, param::FETCH_GET);
 
 // Get paper properties
 $properties = PaperProperties::get_paper_properties_by_id($paperID, $mysqli, $string);
