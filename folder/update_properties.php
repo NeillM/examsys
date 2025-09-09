1d879f9836451a50058d3d0308c715ddc6153277
Liam Bye <liamcjbye@gmail.com>
2025-09-09 14:26:35 +0100

    GJT-1767 folders_modules_staff table is now edited if the module array is empty

--- folder/update_properties.php
+++ folder/update_properties.php
@@ -113,18 +113,17 @@ if (mb_strtolower((string) $new_folder) != mb_strtolower((string) $oldfolder)) {
     $editProperties->close();
 }
 
-if (count($module_array) > 0) {
-    //set the folder staff_modules
-    $editProperties = $mysqli->prepare('DELETE FROM folders_modules_staff WHERE folders_id = ?');
-    $editProperties->bind_param('i', $folderID);
-    $editProperties->execute();
-    $editProperties->close();
+//set the folder staff_modules
+$editProperties = $mysqli->prepare('DELETE FROM folders_modules_staff WHERE folders_id = ?');
+$editProperties->bind_param('i', $folderID);
+$editProperties->execute();
+$editProperties->close();
 
-    $editProperties = $mysqli->prepare('INSERT INTO folders_modules_staff VALUES(?, ?)');
-    foreach ($module_array as $idMod) {
-        $editProperties->bind_param('ii', $folderID, $idMod);
-        $editProperties->execute();
-    }
-    $editProperties->close();
+$editProperties = $mysqli->prepare('INSERT INTO folders_modules_staff VALUES(?, ?)');
+foreach ($module_array as $idMod) {
+    $editProperties->bind_param('ii', $folderID, $idMod);
+    $editProperties->execute();
 }
+$editProperties->close();
+
 echo json_encode('SUCCESS');
