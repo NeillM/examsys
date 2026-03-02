98e34074fe3043950bf49ec984b133c257c2c249
Neill Magill <neill.magill@nottingham.ac.uk>
2026-03-02 11:26:19 +0000

    EXAMSYS-3357 Fix spelling of method

--- component/form/classes/generalgroup.class.php
+++ component/form/classes/generalgroup.class.php
@@ -61,7 +61,7 @@ class GeneralGroup extends Fieldset
      * @param FormElement $option
      * @return void
      */
-    public function adOption(FormElement $option): void
+    public function addOption(FormElement $option): void
     {
         $this->options[] = $option;
     }
@@ -74,8 +74,8 @@ class GeneralGroup extends Fieldset
             name: 'general-fieldset',
             label: 'Fieldset',
         );
-        $example->adOption(Text::getExample());
-        $example->adOption(Select::getExample());
+        $example->addOption(Text::getExample());
+        $example->addOption(Select::getExample());
         return $example;
     }
 }
